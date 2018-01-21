566c9c9d9443f58e9ee20a0b97920ec2f2e8727a
rick.towler <rick.towler@noaa.gov>
2018-01-20 22:42:57 -0800

    added shift pings method to processed data. added new heave compensation example.

--- util/nmea_data.py
+++ util/nmea_data.py
@@ -24,7 +24,8 @@ import ConfigParser
 from functools import reduce
 from datetime import datetime
 from collections import defaultdict
-from instruments.util.pynmea2 import NMEASentence
+#from instruments.util.pynmea2 import NMEASentence
+from .pynmea2 import NMEASentence
 
 
 log = logging.getLogger(__name__)
@@ -42,7 +43,7 @@ class NMEAData(object):
         out there.
     '''
 
-    #TODO Add functions that allow user to get all raw nmea data based on 
+    #TODO Add functions that allow user to get all raw nmea data based on
     #TODO time and based on types return nmea data object. all by default.
     #TODO Add interpolate_pings
 
@@ -93,21 +94,21 @@ class NMEAData(object):
         if header.isalpha() and len(header) == 5:
 
             #  add the raw NMEA datagram
-            self.raw_datagrams = np.append(self.raw_datagrams, {'time':np.datetime64(time), 'text':str(text)}) 
+            self.raw_datagrams = np.append(self.raw_datagrams, {'time':np.datetime64(time), 'text':str(text)})
             cur_index = len(self.raw_datagrams) - 1
-    
+
             #self.time_index[np.datetime64(time)].append(cur_index)
             time_in_seconds = (time - datetime.fromtimestamp(0)).total_seconds()
             self.time_index[time_in_seconds].append(cur_index)
-    
+
             nmea_talker = str(text[1:3].upper())
             self.nmea_talker_index[nmea_talker].append(cur_index)
             self.nmea_talkers = self.nmea_talker_index.keys()
-    
+
             nmea_type = str(text[3:6].upper())
             self.nmea_type_index[nmea_type].append(cur_index)
             self.nmea_types = self.nmea_type_index.keys()
-    
+
             header = np.dtype([(str(header), np.string_, 'S3')])
             self.type_index[header].append(cur_index)
             self.types = self.type_index.keys()
@@ -154,17 +155,17 @@ class NMEAData(object):
 
 
     def get_interpolate(self, data_object, nmea_data_type, nmea_talker_idx_name=None, nmea_type_idx_name=None, start_time=None, end_time=None):
-        ''' 
+        '''
         params:
-            data_object: data object that inherits data container, i.e., raw_data, processed_data 
+            data_object: data object that inherits data container, i.e., raw_data, processed_data
             nmea_data_type: nmea data type to be interpolated. i.e., lat, lon
-            nmea_talker_idx_name_idx_name: nmea talker index name 
-            nmea_type_idx_name_idx_name: nmea type index name 
+            nmea_talker_idx_name_idx_name: nmea talker index name
+            nmea_type_idx_name_idx_name: nmea type index name
             start_time: start of data to interpolate, i.e., start_time=numpy.datetime64('2010-01-10T09:00:00.000000-0700')
             end_time: end of data to interpolate, i.e., end_time=numpy.datetime64('2020-01-10T09:00:00.000000-0700')
 
-             
-        ''' 
+
+        '''
 
         #TODO Add prioritization of location data based on type.
         #TODO Get this from Chuck
@@ -173,7 +174,7 @@ class NMEAData(object):
         #DONE? Add code to handle outliers in lat/lon values.  Use max/min values from conf file? Add percent threshold?
         #DONE make this work with both raw and processedata objects.
         #TODO Create an array with success or fail for each.
-        #TODO Add a flag to the output.  
+        #TODO Add a flag to the output.
         #TODO Add an alert based on threshold based on data type, lat, lon. Add values to config file.
         #TODO if this data was munged, what gets returned?  Do we want to generated a warning? something else?
         #     Generate a warning.  If over 60%.
@@ -188,7 +189,7 @@ class NMEAData(object):
 #            time_index_values = self.time_index[time_index_keys].values()
 #            index = reduce(np.intersect1d(time_index_values))
 #
-            
+
 
         if nmea_talker_idx_name is not None and nmea_type_idx_name is not None:
             index = np.intersect1d(self.nmea_talker_index[nmea_talker_idx_name], \
@@ -211,7 +212,7 @@ class NMEAData(object):
         for record in self.raw_datagrams[index]:
             if 'text' in record and isinstance(record['text'], str):
                 sentence_data = NMEASentence.parse(record['text'])
-                if 'time' in record: 
+                if 'time' in record:
 
                     if start_time is not None and record['time'] < start_time:
                         continue
@@ -234,7 +235,7 @@ class NMEAData(object):
                             update = 0
                         elif max_threshold is not None and nmea_data_val > max_threshold:
                             update = 0
-    
+
                         if update:
                             nmea_data = np.append(nmea_data, nmea_data_val)
                             nmea_time.append(record['time'])
@@ -254,7 +255,7 @@ class NMEAData(object):
         if len(nmea_data) > 0:
             interpolated_nmea_data = np.interp(ping_time_seconds, nmea_time_seconds, nmea_data)
 
-            #Add data to input data object. 
+            #Add data to input data object.
             setattr(data_object, nmea_data_type, interpolated_nmea_data)
         else:
             log.warning("No nmea data was found in the specified parameters.")
@@ -268,7 +269,7 @@ class NMEAData(object):
         config = ConfigParser.ConfigParser()
         for dirpath, dirs, files in os.walk(os.curdir, os.path.expanduser("~")):
             if self.config_file in files:
-                try: 
+                try:
                     with open(os.path.join(dirpath,self.config_file)) as source:
                         if (sys.version_info.major > 2):
                             config.read_file( source )
@@ -278,7 +279,7 @@ class NMEAData(object):
                     config = None
                     log.warning("Could not read config file.  No nmea data thresholds have been set.")
         return config
-            
+
 
     def get_threshold_values(self, nmea_data_type):
         min_threshold = None
@@ -289,13 +290,13 @@ class NMEAData(object):
             try:
                 min_threshold = config.get('nmea', 'min_' + nmea_data_type)
                 min_threshold = np.float32(min_threshold)
-            except:  
+            except:
                 min_threshold = None
-                            
+
             try:
                 max_threshold = config.get('nmea', 'max_' + nmea_data_type)
                 max_threshold = np.float32(max_threshold)
-            except: 
+            except:
                 max_threshold = None
 
         return min_threshold, max_threshold
