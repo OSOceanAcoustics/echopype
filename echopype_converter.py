011536dd66ce15b73c08c8e43568a0ac9dbf7a86
leewujung <leewujung@gmail.com>
2018-02-17 21:58:31 -0800

    add primitive command line converter to call raw2hdf5_* functions

---
+++ echopype_converter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+Convert echosounder files to HDF5 files
+"""
+
+import argparse
+import h5py
+import os.path
+from unpack_ek60 import raw2hdf5_initiate, raw2hdf5_concat
+
+def main():
+    parser = argparse.ArgumentParser(description='Convert EK60 *.raw to HDF5')
+
+    parser.add_argument('-o','--output-file',action='store', nargs=1, help='output HDF5 filename')
+    parser.add_argument('-i','--input-file',action='store', nargs='+', help='input *.raw filename')
+
+    args = parser.parse_args()
+
+    for cnt,f in zip(range(len(args.input_file)),args.input_file):
+        if not(os.path.isfile(f)) or cnt==0:
+            raw2hdf5_initiate(f,args.output_file[0])
+        else:
+            raw2hdf5_concat(f,args.output_file[0])
+    
+if __name__ == '__main__':
+    main()
