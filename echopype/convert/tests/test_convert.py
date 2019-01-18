5705e3c9990ab32804086f4a87164c32b147dc09
leewujung <leewujung@gmail.com>
2019-01-18 18:10:20 -0500

    updated SetGroups.set_beam() to check EK60-specific params before saving

--- echopype/convert/tests/test_convert.py
+++ echopype/convert/tests/test_convert.py
@@ -1,12 +1,16 @@
+import os
 from echopype.convert import ek60
 
 raw_filename = '../data/DY1801_EK60-D20180211-T164025.raw'
 
-# first_ping_metadata, data_times, motion, \
-#     power_data_dict, angle_data_dict, tr_data_dict, \
-#     config_header, config_transducer = ek60.load_ek60_raw(raw_filename)
+def test_convert_ek60():
+
+first_ping_metadata, data_times, motion, \
+    power_data_dict, angle_data_dict, tr_data_dict, \
+    config_header, config_transducer = ek60.load_ek60_raw(raw_filename)
 
 ek60.save_raw_to_nc(raw_filename)
+
 
 
 # OceanStarr 2 channel EK60
