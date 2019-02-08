4628620c1d4471420eece105b38ff571c24f2d92
leewujung <leewujung@gmail.com>
2019-02-07 22:57:59 -0500

    revised command line converter, need to figure out where it should go

--- echopype/data_converter.py
+++ echopype/data_converter.py
@@ -8,25 +8,25 @@ This will be changed to use the functions under /convert and support multiple so
 
 import argparse
 import os.path
-from echopype.unpack_ek60 import raw2hdf5_initiate, raw2hdf5_concat
-from datetime import datetime as dt
+from echopype.convert.ek60 import ConvertEK60
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Convert EK60 *.raw to HDF5')
+    parser = argparse.ArgumentParser(description='Convert EK60 .raw to a netCDF (.nc) file')
 
-    parser.add_argument('-o','--output-file',action='store', nargs=1, help='output HDF5 filename')
-    parser.add_argument('-i','--input-file',action='store', nargs='+', help='input *.raw filename')
+    parser.add_argument('-i', '--input-file', action='store', nargs='+', help='input *.raw filename')
 
     args = parser.parse_args()
 
-    for cnt,f in zip(range(len(args.input_file)),args.input_file):
-        if not(os.path.isfile(args.output_file[0])) and cnt==0:
-            raw2hdf5_initiate(f,args.output_file[0])
-            print('%s  creating and saving data to : %s' % (dt.now().strftime('%H:%M:%S'), args.output_file[0]))
+    for p in args.input_file:
+        ff = os.path.basename(p)
+        f, ext = os.path.splitext(ff)
+        if not os.path.isfile(f+'.nc'):  # if file has not been converted
+            tmp = ConvertEK60(p)  # create an instance of EK60 converter
+            tmp.raw2nc()   # convert .raw to .nc
         else:
-            raw2hdf5_concat(f,args.output_file[0])
-            print('%s  appending data to: %s' % (dt.now().strftime('%H:%M:%S'), args.output_file[0]))
+            print('This .raw file has been converted before. Delete if you want to convert again.')
+
 
 if __name__ == '__main__':
     main()
