6024009109fa2ed84dcd3f29f5929770f91cf106
leewujung <leewujung@gmail.com>
2019-02-10 15:04:40 -0500

    started EK60 model class and added method calibrate()

--- echopype/model/ek60.py
+++ echopype/model/ek60.py
@@ -4,6 +4,8 @@ its connection to data files.
 """
 
 import os
+import datetime as dt
+import numpy as np
 import xarray as xr
 
 
@@ -25,20 +27,99 @@ class EchoData(object):
         pp = os.path.basename(p)
         _, ext = os.path.splitext(pp)
 
-        if ext is '.raw':
+        if ext == '.raw':
             print('Data file in manufacturer format, please convert to .nc first.')
-        elif ext is '.nc':
+        elif ext == '.nc':
             self.toplevel = xr.open_dataset(self.file_path)
-            if self.toplevel['sonar_convention_version'] == 'SONAR-netCDF4':
+            if self.toplevel.sonar_convention_name == 'SONAR-netCDF4':
+                # Point to various groups
                 self.provenance = xr.open_dataset(self.file_path, group="Provenance")
                 self.environment = xr.open_dataset(self.file_path, group="Environment")
                 self.platform = xr.open_dataset(self.file_path, group="Platform")
                 self.sonar = xr.open_dataset(self.file_path, group="Sonar")
                 self.beam = xr.open_dataset(self.file_path, group="Beam")
+
+                # Get a separate .nc filename for storing processed data if computation is performed
+                self.proc_path = os.path.join(os.path.dirname(self.file_path),
+                                              os.path.splitext(os.path.basename(self.file_path))[0]+'_proc.nc')
             else:
                 print('netCDF file convention not recognized.')
+                self.proc_path = ''
         else:
             print('Data file format not recognized.')
+
+    def calibrate(self, tvg_correction_factor=2):
+        """Perform echo-integration to get volume backscattering strength (Sv) from EK60 power data.
+
+        Parameters
+        -----------
+        tvg_correction_factor : range bin offset factor for calculating time-varying gain in EK60.
+        """
+
+        # Loop through each frequency for calibration
+        Sv = np.zeros(self.beam.backscatter_r.shape)
+        for f_seq, freq in enumerate(self.beam.frequency.values):
+            # Params from env group
+            c = self.environment.sound_speed_indicative.sel(frequency=freq).values
+            alpha = self.environment.absorption_indicative.sel(frequency=freq).values
+
+            # Params from beam group
+            t = self.beam.sample_interval.sel(frequency=freq).values
+            gain = self.beam.gain_correction.sel(frequency=freq).values
+            phi = self.beam.equivalent_beam_angle.sel(frequency=freq).values
+            pt = self.beam.transmit_power.sel(frequency=freq).values
+            tau = self.beam.transmit_duration_nominal.sel(frequency=freq).values
+            Sac = 2 * self.beam.sa_correction.sel(frequency=freq).values
+
+            # Derived params
+            dR = c*t/2  # sample thickness
+            wvlen = c/freq  # wavelength
+
+            # Calc gain
+            CSv = 10 * np.log10((pt * (10 ** (gain / 10))**2 *
+                                 wvlen**2 * c * tau * 10**(phi / 10)) /
+                                (32 * np.pi ** 2))
+
+            # Get TVG
+            range_vec = np.arange(self.beam.range_bin.size) * dR
+            range_vec = range_vec - (tvg_correction_factor * dR)
+            range_vec[range_vec < 0] = 0
+
+            TVG = np.empty(range_vec.shape)
+            TVG[range_vec != 0] = np.real(20 * np.log10(range_vec[range_vec != 0]))
+            TVG[range_vec == 0] = 0
+
+            # Get absorption
+            ABS = 2 * alpha * range_vec
+
+            # Compute Sv
+            Sv[f_seq, :, :] = self.beam.backscatter_r.sel(frequency=freq).values + TVG + ABS - CSv - Sac
+
+        # Assemble an xarray DataArray
+        ping_time = (self.beam.ping_time.data - np.datetime64('1900-01-01T00:00:00')) / np.timedelta64(1, 's')
+        ds = xr.Dataset(
+            {'Sv': (['frequency', 'ping_time', 'range_bin'], Sv,
+                    {'long_name': 'Platform pitch',
+                     'standard_name': 'platform_pitch_angle',
+                     'units': 'arc_degree',
+                     'valid_range': (-90.0, 90.0)}),
+             },
+            coords={'frequency': (['frequency'], self.beam.frequency,
+                                  {'units': 'Hz',
+                                   'valid_min': 0.0}),
+                    'ping_time': (['ping_time'], ping_time,
+                                  {'axis': 'T',
+                                   'calendar': 'gregorian',
+                                   'long_name': 'Timestamp of each ping',
+                                   'standard_name': 'time',
+                                   'units': 'seconds since 1900-01-01'}),
+                    'range_bin': (['range_bin'], self.beam.range_bin)},
+            attrs={'tvg_correction_factor': tvg_correction_factor})
+
+        # Save calibrated data to a separate .nc file in the same directory as the data file
+        print('Saving calibrated Sv into %s' % self.proc_path)
+        ds.to_netcdf(path=self.proc_path, mode="w")
+        ds.close()
 
     # @file_path.setter
     # def file_path(self, p):
@@ -62,44 +143,6 @@ class EchoData(object):
     #         raise ValueError('Not sure what file this is?? try to find a .nc file??')
     #     self._file_path = p
 
-
-    # def load(self, data_path):
-    #     data_file = os.path.basename(data_path)
-    #     data_ext = os.path.splitext(data_file)
-    #     """Load echo data from file"""
-    #     if data_ext is '.raw':
-    #         print('Data file in manufacturer format, please convert first.')
-    #         print('To convert data, use the following... ')
-    #     elif data_ext is '.nc':
-    #         self.nc_path = data_path
-    #         self.nc_file = data_file
-    #         self.toplevel = xr.open_dataset(self.data_path)
-    #         if self.toplevel['sonar_convention_version'] == 'SONAR-netCDF4':
-    #             # load groups as xarray objects
-    #             self.provenance = xr.open_dataset(self.data_path, group="Provenance")
-    #             self.environment = xr.open_dataset(self.data_path, group="Environment")
-    #             self.platform = xr.open_dataset(self.data_path, group="Platform")
-    #             self.sonar = xr.open_dataset(self.data_path, group="Sonar")
-    #             self.beam = xr.open_dataset(self.data_path, group="Beam")
-    #         else:
-    #             print('netCDF file convention not recognized.')
-    #     else:
-    #         print('Data file format not recognized.')
-    #
-    # def set_nc_path(self, nc_path):
-    #     path = os.path.dirname(nc_path)
-    #     if path != '':
-    #         self.nc_path = nc_path
-    #     self.nc_file = os.path.basename(nc_path)
-    #
-    # def get_nc_path(self):
-    #     return self.nc_path
-    #
-    # def set_nc_file(self, nc_file):
-    #     self.nc_file = os.path.basename(nc_file)
-    #
-    # def get_nc_file(self):
-    #     return self.nc_file
 
 
 
