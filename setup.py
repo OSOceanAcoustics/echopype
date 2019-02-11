e66cc2bc17728720d2bcc6e0e293f0cd4b09b389
leewujung <leewujung@gmail.com>
2019-02-11 16:07:04 -0500

    moved reading README.rst part to setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,14 @@ import os
 from setuptools import setup, find_packages
 PACKAGES = find_packages()
 
-# Get version and release info, which is all stored in shablona/version.py
+# Get version and release info, which is all stored in echopype/version.py
 ver_file = os.path.join('echopype', 'version.py')
 with open(ver_file) as f:
     exec(f.read())
+
+# Long description will go up on the pypi page
+with open('README.rst') as file:
+    LONG_DESCRIPTION = file.read()
 
 opts = dict(name=NAME,
             maintainer=MAINTAINER,
