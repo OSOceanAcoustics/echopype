0701c30c66ca1e7b26c4f864113ec2f1040b9d0f
leewujung <leewujung@gmail.com>
2019-02-14 11:06:41 -0500

    first working version of remove_noise() using xarray

--- echopype/model/ek60.py
+++ echopype/model/ek60.py
@@ -15,8 +15,8 @@ class EchoData(object):
     def __init__(self, file_path=""):
         self.file_path = file_path         # this passes the input through file name test
         self.tvg_correction_factor = 2     # range bin offset factor for calculating time-varying gain in EK60
-        self.noise_est_range_bin_size = 10  # meters per tile for noise estimation
-        self.noise_est_ping_size = 40       # number of pings per tile for noise estimation
+        self.noise_est_range_bin_size = 5  # meters per tile for noise estimation
+        self.noise_est_ping_size = 30      # number of pings per tile for noise estimation
 
     @property
     def file_path(self):
@@ -35,26 +35,31 @@ class EchoData(object):
             print('Data file in manufacturer format, please convert to .nc first.')
         elif ext == '.nc':
             self.toplevel = xr.open_dataset(self.file_path)
+
+            # Get a separate .nc filename for storing processed data if computation is performed
+            self.proc_path = os.path.join(os.path.dirname(self.file_path),
+                                          os.path.splitext(os.path.basename(self.file_path))[0] + '_proc.nc')
+
             if self.toplevel.sonar_convention_name == 'SONAR-netCDF4':
-                # Point to various groups to .nc file converted from .raw
-                self.provenance = xr.open_dataset(self.file_path, group="Provenance")
-                self.environment = xr.open_dataset(self.file_path, group="Environment")
-                self.platform = xr.open_dataset(self.file_path, group="Platform")
-                self.sonar = xr.open_dataset(self.file_path, group="Sonar")
-                self.beam = xr.open_dataset(self.file_path, group="Beam")
+                # # Point to various groups to .nc file converted from .raw
+                # self.provenance = xr.open_dataset(self.file_path, group="Provenance")
+                # self.environment = xr.open_dataset(self.file_path, group="Environment")
+                # self.platform = xr.open_dataset(self.file_path, group="Platform")
+                # self.sonar = xr.open_dataset(self.file_path, group="Sonar")
+                # self.beam = xr.open_dataset(self.file_path, group="Beam")
 
                 # Get a separate .nc filename for storing processed data if computation is performed
                 self.proc_path = os.path.join(os.path.dirname(self.file_path),
                                               os.path.splitext(os.path.basename(self.file_path))[0]+'_proc.nc')
             else:
                 print('netCDF file convention not recognized.')
-                # Groups for .nc file converted from .raw
-                self.toplevel = ''
-                self.provenance = ''
-                self.environment = ''
-                self.platform = ''
-                self.sonar = ''
-                self.beam = ''
+                # # Groups for .nc file converted from .raw
+                # self.toplevel = ''
+                # self.provenance = ''
+                # self.environment = ''
+                # self.platform = ''
+                # self.sonar = ''
+                # self.beam = ''
 
                 # Processed data
                 self.proc_path = ''
@@ -65,20 +70,25 @@ class EchoData(object):
         """Perform echo-integration to get volume backscattering strength (Sv) from EK60 power data.
         """
 
+        # Open data set for Environment and Beam groups
+        ds_env = xr.open_dataset(self.file_path, group="Environment")
+        ds_beam = xr.open_dataset(self.file_path, group="Beam")
+
+        # TODO: change the code below to use xarray func
         # Loop through each frequency for calibration
-        Sv = np.zeros(self.beam.backscatter_r.shape)
-        for f_seq, freq in enumerate(self.beam.frequency.values):
+        Sv = np.zeros(ds_beam.backscatter_r.shape)
+        for f_seq, freq in enumerate(ds_beam.frequency.values):
             # Params from env group
-            c = self.environment.sound_speed_indicative.sel(frequency=freq).values
-            alpha = self.environment.absorption_indicative.sel(frequency=freq).values
+            c = ds_env.sound_speed_indicative.sel(frequency=freq).values
+            alpha = ds_env.absorption_indicative.sel(frequency=freq).values
 
             # Params from beam group
-            t = self.beam.sample_interval.sel(frequency=freq).values
-            gain = self.beam.gain_correction.sel(frequency=freq).values
-            phi = self.beam.equivalent_beam_angle.sel(frequency=freq).values
-            pt = self.beam.transmit_power.sel(frequency=freq).values
-            tau = self.beam.transmit_duration_nominal.sel(frequency=freq).values
-            Sac = 2 * self.beam.sa_correction.sel(frequency=freq).values
+            t = ds_beam.sample_interval.sel(frequency=freq).values
+            gain = ds_beam.gain_correction.sel(frequency=freq).values
+            phi = ds_beam.equivalent_beam_angle.sel(frequency=freq).values
+            pt = ds_beam.transmit_power.sel(frequency=freq).values
+            tau = ds_beam.transmit_duration_nominal.sel(frequency=freq).values
+            Sac = 2 * ds_beam.sa_correction.sel(frequency=freq).values
 
             # Derived params
             dR = c*t/2  # sample thickness
@@ -90,7 +100,7 @@ class EchoData(object):
                                 (32 * np.pi ** 2))
 
             # Get TVG
-            range_vec = np.arange(self.beam.range_bin.size) * dR
+            range_vec = np.arange(ds_beam.range_bin.size) * dR
             range_vec = range_vec - (self.tvg_correction_factor * dR)
             range_vec[range_vec < 0] = 0
 
@@ -102,18 +112,13 @@ class EchoData(object):
             ABS = 2 * alpha * range_vec
 
             # Compute Sv
-            Sv[f_seq, :, :] = self.beam.backscatter_r.sel(frequency=freq).values + TVG + ABS - CSv - Sac
+            Sv[f_seq, :, :] = ds_beam.backscatter_r.sel(frequency=freq).values + TVG + ABS - CSv - Sac
 
-        # Assemble an xarray DataArray
-        ping_time = (self.beam.ping_time.data - np.datetime64('1900-01-01T00:00:00')) / np.timedelta64(1, 's')
+        # Assemble an xarray DataSet
+        ping_time = (ds_beam.ping_time.data - np.datetime64('1900-01-01T00:00:00')) / np.timedelta64(1, 's')
         ds = xr.Dataset(
-            {'Sv': (['frequency', 'ping_time', 'range_bin'], Sv,
-                    {'long_name': 'Platform pitch',
-                     'standard_name': 'platform_pitch_angle',
-                     'units': 'arc_degree',
-                     'valid_range': (-90.0, 90.0)}),
-             },
-            coords={'frequency': (['frequency'], self.beam.frequency,
+            {'Sv': (['frequency', 'ping_time', 'range_bin'], Sv)},
+            coords={'frequency': (['frequency'], ds_beam.frequency,
                                   {'units': 'Hz',
                                    'valid_min': 0.0}),
                     'ping_time': (['ping_time'], ping_time,
@@ -122,7 +127,7 @@ class EchoData(object):
                                    'long_name': 'Timestamp of each ping',
                                    'standard_name': 'time',
                                    'units': 'seconds since 1900-01-01'}),
-                    'range_bin': (['range_bin'], self.beam.range_bin)},
+                    'range_bin': (['range_bin'], ds_beam.range_bin)},
             attrs={'tvg_correction_factor': self.tvg_correction_factor})
 
         # Save calibrated data to a separate .nc file in the same directory as the data file
@@ -130,11 +135,12 @@ class EchoData(object):
         ds.to_netcdf(path=self.proc_path, mode="w")
         ds.close()
 
-    def get_noise_est(self, noise_est_range_bin_size=None, noise_est_ping_size=None):
+    def remove_noise(self, noise_est_range_bin_size=None, noise_est_ping_size=None):
         """
-        Estimate noise level by getting the minimum value for bins of averaged ping.
+        Remove noise by using noise estimates obtained from the minimum mean calibrated power level
+        along each column of tiles.
 
-        This function is called internally by remove_noise()
+        See method noise_estimates() for details of noise estimation.
         Reference: De Robertis & Higginbottom, 2017, ICES Journal of Marine Sciences
 
         Parameters
@@ -149,18 +155,22 @@ class EchoData(object):
         if (noise_est_ping_size is not None) and (self.noise_est_ping_size != noise_est_ping_size):
             self.noise_est_ping_size = noise_est_ping_size
 
+        # Open data set for Environment and Beam groups
+        ds_env = xr.open_dataset(self.file_path, group="Environment")
+        ds_beam = xr.open_dataset(self.file_path, group="Beam")
+
         # Get TVG and absorption
-        c = self.environment.sound_speed_indicative
-        t = self.beam.sample_interval
+        c = ds_env.sound_speed_indicative
+        t = ds_beam.sample_interval
         dR = c * t / 2  # sample thickness
-        range_idx = xr.DataArray(np.array([np.arange(self.beam.range_bin.size)] * 5).squeeze(),
+        range_idx = xr.DataArray(np.array([np.arange(ds_beam.range_bin.size)] * 5).squeeze(),
                                  dims=['frequency', 'range_bin'],
-                                 coords={'frequency': self.beam.frequency, 'range_bin': self.beam.range_bin})
-        range_meter = range_idx * dR - self.tvg_correction_factor * dR  # return DataArray with dim frequency x i1 (1:1386)
+                                 coords={'frequency': ds_beam.frequency, 'range_bin': ds_beam.range_bin})
+        range_meter = range_idx * dR - self.tvg_correction_factor * dR  # return DataArray [frequency x range_bin]
         range_meter = range_meter.where(range_meter > 0, other=0)  # set all negative elements to 0
         TVG = np.real(20 * np.log10(range_meter.where(range_meter != 0, other=1)))
 
-        alpha = self.environment.absorption_indicative
+        alpha = ds_env.absorption_indicative
         ABS = 2 * alpha * range_meter
 
         # Get calibrated power
@@ -173,37 +183,40 @@ class EchoData(object):
         self.noise_est_range_bin_size = (num_range_bin_per_tile * dR).values
 
         # Number of tiles along range_bin
-        if np.mod(self.beam.range_bin.size, num_range_bin_per_tile) == 0:
-            N_range_bin = np.ceil(self.beam.range_bin.size / num_range_bin_per_tile).astype(int) + 1
+        if np.mod(ds_beam.range_bin.size, num_range_bin_per_tile) == 0:
+            num_tile_range_bin = np.ceil(ds_beam.range_bin.size / num_range_bin_per_tile).astype(int) + 1
         else:
-            N_range_bin = np.ceil(self.beam.range_bin.size / num_range_bin_per_tile).astype(int)
+            num_tile_range_bin = np.ceil(ds_beam.range_bin.size / num_range_bin_per_tile).astype(int)
 
         # Produce a new coordinate for groupby operation
-        if np.mod(self.beam.ping_time.size, self.noise_est_ping_size) == 0:
-            N_ping = np.ceil(self.beam.ping_time.size / self.noise_est_ping_size).astype(int) + 1
-            z = np.array([np.arange(N_ping - 1)] * self.noise_est_ping_size).squeeze().T.ravel()
+        if np.mod(ds_beam.ping_time.size, self.noise_est_ping_size) == 0:
+            num_tile_ping = np.ceil(ds_beam.ping_time.size / self.noise_est_ping_size).astype(int) + 1
+            z = np.array([np.arange(num_tile_ping - 1)] * self.noise_est_ping_size).squeeze().T.ravel()
         else:
-            N_ping = np.ceil(self.beam.ping_time.size / self.noise_est_ping_size).astype(int)
-            pad = np.ones(N_ping*self.noise_est_ping_size - self.beam.ping_time.size, dtype=int)*(N_ping-1)
-            z = np.hstack((np.array([np.arange(N_ping - 1)] * self.noise_est_ping_size).squeeze().T.ravel(), pad))
+            num_tile_ping = np.ceil(ds_beam.ping_time.size / self.noise_est_ping_size).astype(int)
+            pad = np.ones(ds_beam.ping_time.size - (num_tile_ping - 1) * self.noise_est_ping_size, dtype=int) \
+                  * (num_tile_ping - 1)
+            z = np.hstack((np.array([np.arange(num_tile_ping - 1)] * self.noise_est_ping_size).squeeze().T.ravel(), pad))
 
-        # Use groupby_bins to compute noise estimates over N_range_bin x N_ping tile
-        range_bin_bin = np.arange(N_range_bin)*num_range_bin_per_tile
+        # Tile bin edges along range
+        range_bin_tile_bin = np.arange(num_tile_range_bin) * num_range_bin_per_tile
 
         # Function for use with apply
         def remove_n(x):
             p_c_lin = 10 ** ((x - ABS - TVG) / 10)
-            nn = 10 * np.log10(p_c_lin.groupby_bins('range_bin', range_bin_bin).mean('range_bin').
+            nn = 10 * np.log10(p_c_lin.groupby_bins('range_bin', range_bin_tile_bin).mean('range_bin').
                                groupby('frequency').mean('ping_time').min(dim='range_bin_bins'))
             return x.where(x > nn, other=np.nan)
 
         # Groupby noise removal operation
-        Sv = proc_data.Sv
-        Sv.coords['add_idx'] = ('ping_time', z)
-        Sv_clean = Sv.groupby('add_idx').apply(remove_n)
+        proc_data.coords['add_idx'] = ('ping_time', z)
+        Sv_clean = proc_data.Sv.groupby('add_idx').apply(remove_n)
         Sv_clean.name = 'Sv_clean'
-        Sv_clean = Sv_clean.drop('add_idx')
-        proc_data['Sv_clean'] = Sv_clean
+
+        # Save as a netCDF file
+        Sv_clean.to_dataset().to_netcdf(self.proc_path)
+        proc_data.close()
+
 
         # if (N_ping-1)*self.noise_est_ping_size >= self.beam.ping_time.size:
         #     ping_bin = self.beam.ping_time.isel(ping_time=np.hstack((np.arange(N_ping-1) * self.noise_est_ping_size,
@@ -216,15 +229,6 @@ class EchoData(object):
         # noise_est = noise_est_rp.groupby('ping_time_bins').min('range_bin_bins')
 
 
-        # nn = 10 * np.log10(p_c_lin.groupby_bins('range_bin', range_bin_bin).mean('range_bin').
-        #                    groupby('frequency').mean('ping_time').min(dim='range_bin_bins'))
-
-        # Assemble an xarray DataArray
-        # TODO: convert noise_est to a DataSet,
-        #  and attach the right coordinates ping_time_noise_est and range_bin_noise_est
-
-
-        #
         # # Get noise estimates over N_range_bin x N_ping tile
         # idx_r_base = np.arange(num_range_bin_per_tile, dtype=int)
         # idx_p_base = np.arange(self.noise_est_ping_size, dtype=int)
--- echopype/version.py
+++ echopype/version.py
@@ -1,10 +1,11 @@
 from __future__ import absolute_import, division, print_function
 from os.path import join as pjoin
 
+
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
 _version_major = 0
 _version_minor = 1
-_version_micro = 0  # use '' for first of series, number for 1 and above
+_version_micro = 1  # use '' for first of series, number for 1 and above
 _version_extra = ''
 # _version_extra = ''  # Uncomment this for full releases
 
@@ -26,16 +27,13 @@ CLASSIFIERS = ["Development Status :: 3 - Alpha",
                "Topic :: Scientific/Engineering"]
 
 # Description should be a one-liner:
-description = "echopype is an open-source toolkit for analyzing active sonar data for biological information in the ocean."
-# Long description will go up on the pypi page
-with open('README.rst') as file:
-    long_description = file.read()
+description = "Enhancing interoperability and scalability in analyzing water column sonar data."
 
 NAME = "echopype"
 MAINTAINER = "Wu-Jung Lee"
 MAINTAINER_EMAIL = "leewujung@gmail.com"
 DESCRIPTION = description
-LONG_DESCRIPTION = long_description
+# LONG_DESCRIPTION = long_description
 URL = "https://github.com/OSOceanAcoustics/echopype"
 DOWNLOAD_URL = ""
 LICENSE = 'Apache License, Version 2.0'
