49274a672283c26f2096709079d9436b2a5a8896
leewujung <leewujung@gmail.com>
2019-02-14 14:25:23 -0500

    save each type of processed data into different .nc files

--- echopype/model/ek60.py
+++ echopype/model/ek60.py
@@ -13,15 +13,18 @@ class EchoData(object):
     """Base class for echo data."""
 
     def __init__(self, file_path=""):
-        self.file_path = file_path         # this passes the input through file name test
-        self.tvg_correction_factor = 2     # range bin offset factor for calculating time-varying gain in EK60
-        self.TVG = None                    # time varying gain along range
-        self.ABS = None                    # absorption along range
-        self.sample_thickness = None       # sample thickness for each frequency
+        self.file_path = file_path  # this passes the input through file name test
+        self.tvg_correction_factor = 2  # range bin offset factor for calculating time-varying gain in EK60
+        self.TVG = None  # time varying gain along range
+        self.ABS = None  # absorption along range
+        self.sample_thickness = None  # sample thickness for each frequency
         self.noise_est_range_bin_size = 5  # meters per tile for noise estimation
-        self.noise_est_ping_size = 30      # number of pings per tile for noise estimation
-        self.MVBS_range_bin_size = 5       # meters per tile for MVBS
-        self.MVBS_ping_size = 30           # number of pings per tile for MVBS
+        self.noise_est_ping_size = 30  # number of pings per tile for noise estimation
+        self.MVBS_range_bin_size = 5  # meters per tile for MVBS
+        self.MVBS_ping_size = 30  # number of pings per tile for MVBS
+        self.Sv = None  # calibrated volume backscattering strength
+        self.Sv_clean = None  # denoised volume backscattering strength
+        self.MVBS = None  # mean volume backscattering strength
 
     @property
     def file_path(self):
@@ -40,33 +43,16 @@ class EchoData(object):
         elif ext == '.nc':
             self.toplevel = xr.open_dataset(self.file_path)
 
-            # Get a separate .nc filename for storing processed data if computation is performed
-            self.proc_path = os.path.join(os.path.dirname(self.file_path),
-                                          os.path.splitext(os.path.basename(self.file_path))[0] + '_proc.nc')
+            # Get .nc filenames for storing processed data if computation is performed
+            self.Sv_path = os.path.join(os.path.dirname(self.file_path),
+                                        os.path.splitext(os.path.basename(self.file_path))[0] + '_Sv.nc')
+            self.Sv_clean_path = os.path.join(os.path.dirname(self.file_path), '_Sv_clean.nc')
+            self.MVBS_path = os.path.join(os.path.dirname(self.file_path),
+                                          os.path.splitext(os.path.basename(self.file_path))[0] + '_MVBS.nc')
 
-            if self.toplevel.sonar_convention_name == 'SONAR-netCDF4':
-                # # Point to various groups to .nc file converted from .raw
-                # self.provenance = xr.open_dataset(self.file_path, group="Provenance")
-                # self.environment = xr.open_dataset(self.file_path, group="Environment")
-                # self.platform = xr.open_dataset(self.file_path, group="Platform")
-                # self.sonar = xr.open_dataset(self.file_path, group="Sonar")
-                # self.beam = xr.open_dataset(self.file_path, group="Beam")
-
-                # Get a separate .nc filename for storing processed data if computation is performed
-                self.proc_path = os.path.join(os.path.dirname(self.file_path),
-                                              os.path.splitext(os.path.basename(self.file_path))[0]+'_proc.nc')
-            else:
-                print('netCDF file convention not recognized.')
-                # # Groups for .nc file converted from .raw
-                # self.toplevel = ''
-                # self.provenance = ''
-                # self.environment = ''
-                # self.platform = ''
-                # self.sonar = ''
-                # self.beam = ''
-
-                # Processed data
-                self.proc_path = ''
+            # Raise error if the file format convention does not match
+            if self.toplevel.sonar_convention_name != 'SONAR-netCDF4':
+                raise ValueError('netCDF file convention not recognized.')
         else:
             raise ValueError('Data file format not recognized.')
 
@@ -105,14 +91,13 @@ class EchoData(object):
         Sv.name = 'Sv'
 
         # Save calibrated data to a separate .nc file in the same directory as the data file
-        print('%s  saving calibrated Sv to %s' % (dt.datetime.now().strftime('%H:%M:%S'), self.proc_path))
-        Sv.to_netcdf(path=self.proc_path, mode="w")
+        print('%s  saving calibrated Sv to %s' % (dt.datetime.now().strftime('%H:%M:%S'), self.Sv_path))
+        Sv.to_netcdf(path=self.Sv_path, mode="w")
         ds_env.close()
         ds_beam.close()
 
     def remove_noise(self, noise_est_range_bin_size=None, noise_est_ping_size=None):
-        """
-        Remove noise by using noise estimates obtained from the minimum mean calibrated power level
+        """Remove noise by using noise estimates obtained from the minimum mean calibrated power level
         along each column of tiles.
 
         See method noise_estimates() for details of noise estimation.
@@ -130,11 +115,8 @@ class EchoData(object):
         if (noise_est_ping_size is not None) and (self.noise_est_ping_size != noise_est_ping_size):
             self.noise_est_ping_size = noise_est_ping_size
 
-        # Open data set for Beam groups
-        ds_beam = xr.open_dataset(self.file_path, group="Beam")
-
         # Get calibrated power
-        proc_data = xr.open_dataset(self.proc_path)
+        proc_data = xr.open_dataset(self.Sv_path)
 
         # Adjust noise_est_range_bin_size because range_bin_size may be an inconvenient value
         num_range_bin_per_tile = (np.round(self.noise_est_range_bin_size /
@@ -142,18 +124,18 @@ class EchoData(object):
         self.noise_est_range_bin_size = (num_range_bin_per_tile * self.sample_thickness).values
 
         # Number of tiles along range_bin
-        if np.mod(ds_beam.range_bin.size, num_range_bin_per_tile) == 0:
-            num_tile_range_bin = np.ceil(ds_beam.range_bin.size / num_range_bin_per_tile).astype(int) + 1
+        if np.mod(proc_data.range_bin.size, num_range_bin_per_tile) == 0:
+            num_tile_range_bin = np.ceil(proc_data.range_bin.size / num_range_bin_per_tile).astype(int) + 1
         else:
-            num_tile_range_bin = np.ceil(ds_beam.range_bin.size / num_range_bin_per_tile).astype(int)
+            num_tile_range_bin = np.ceil(proc_data.range_bin.size / num_range_bin_per_tile).astype(int)
 
         # Produce a new coordinate for groupby operation
-        if np.mod(ds_beam.ping_time.size, self.noise_est_ping_size) == 0:
-            num_tile_ping = np.ceil(ds_beam.ping_time.size / self.noise_est_ping_size).astype(int) + 1
+        if np.mod(proc_data.ping_time.size, self.noise_est_ping_size) == 0:
+            num_tile_ping = np.ceil(proc_data.ping_time.size / self.noise_est_ping_size).astype(int) + 1
             z = np.array([np.arange(num_tile_ping - 1)] * self.noise_est_ping_size).squeeze().T.ravel()
         else:
-            num_tile_ping = np.ceil(ds_beam.ping_time.size / self.noise_est_ping_size).astype(int)
-            pad = np.ones(ds_beam.ping_time.size - (num_tile_ping - 1) * self.noise_est_ping_size, dtype=int) \
+            num_tile_ping = np.ceil(proc_data.ping_time.size / self.noise_est_ping_size).astype(int)
+            pad = np.ones(proc_data.ping_time.size - (num_tile_ping - 1) * self.noise_est_ping_size, dtype=int) \
                   * (num_tile_ping - 1)
             z = np.hstack((np.array([np.arange(num_tile_ping - 1)]
                                     * self.noise_est_ping_size).squeeze().T.ravel(), pad))
@@ -175,9 +157,9 @@ class EchoData(object):
         Sv_clean.name = 'Sv_clean'
 
         # Save as a netCDF file
-        Sv_clean.to_dataset().to_netcdf(self.proc_path)
+        print('%s  saving denoised Sv to %s' % (dt.datetime.now().strftime('%H:%M:%S'), self.Sv_clean_path))
+        Sv_clean.to_netcdf(self.Sv_clean_path)
         proc_data.close()
-        ds_beam.close()
 
     def noise_estimates(self, noise_est_range_bin_size=None, noise_est_ping_size=None):
         """
@@ -212,7 +194,7 @@ class EchoData(object):
         ds_beam = xr.open_dataset(self.file_path, group="Beam")
 
         # Use calibrated data to calculate noise removal
-        proc_data = xr.open_dataset(self.proc_path)
+        proc_data = xr.open_dataset(self.Sv_path)
 
         # Adjust noise_est_range_bin_size because range_bin_size may be an inconvenient value
         num_range_bin_per_tile = (np.round(self.noise_est_range_bin_size /
@@ -262,3 +244,89 @@ class EchoData(object):
         ds_beam.close()
 
         return noise_est
+
+    def get_MVBS(self, source='Sv', MVBS_range_bin_size=None, MVBS_ping_size=None):
+        """Calculate Mean Volume Backscattering Strength (MVBS).
+
+        The calculation uses class attributes MVBS_ping_size and MVBS_range_bin_size.
+
+        Parameters
+        ------------
+        source : str
+            source used to calculate MVBS, can be ``Sv`` or ``Sv_clean``,
+            where ``Sv`` and ``Sv_clean`` are the original and denoised volume
+            backscattering strengths, respectively. Both are calibrated.
+        MVBS_range_bin_size : float
+            meters per tile for calculating MVBS [m]
+        MVBS_ping_size : int
+            number of pings per tile for calculating MVBS
+
+        Returns
+        ---------
+        MVBS : xarray DataArray
+            MVBS has dimensions [ping_bin and range_bin_bin]
+            range_bin_bin is the number of tiles along range_bin calculated from attributes MVBS_range_bin_size
+            ping_bin is the number of tiles along ping_time calculated from attributes MVBS_ping_size
+        """
+        # Check params
+        if (MVBS_range_bin_size is not None) and (self.MVBS_range_bin_size != MVBS_range_bin_size):
+            self.MVBS_range_bin_size = MVBS_range_bin_size
+        if (MVBS_ping_size is not None) and (self.MVBS_ping_size != MVBS_ping_size):
+            self.MVBS_ping_size = MVBS_ping_size
+
+        # Use calibrated data to calculate noise removal
+        if source == 'Sv':
+            proc_data = xr.open_dataset(self.Sv_path)
+        elif source == 'Sv_clean':
+            if self.Sv_clean is not None:
+                proc_data = xr.open_dataset(self.Sv_clean_path)
+            else:
+                raise ValueError('Need to obtain Sv_clean first by calling remove_noise()')
+        else:
+            raise ValueError('Unknown source, cannot calculate MVBS')
+
+        # Adjust noise_est_range_bin_size because range_bin_size may be an inconvenient value
+        num_range_bin_per_tile = (np.round(self.MVBS_range_bin_size /
+                                           self.sample_thickness).astype(int)).values.max()  # num of range_bin per tile
+        self.MVBS_range_bin_size = (num_range_bin_per_tile * self.sample_thickness).values
+
+        # Number of tiles along range_bin
+        if np.mod(proc_data.range_bin.size, num_range_bin_per_tile) == 0:
+            num_tile_range_bin = np.ceil(proc_data.range_bin.size / num_range_bin_per_tile).astype(int) + 1
+        else:
+            num_tile_range_bin = np.ceil(proc_data.range_bin.size / num_range_bin_per_tile).astype(int)
+
+        # Produce a new coordinate for groupby operation
+        if np.mod(proc_data.ping_time.size, self.MVBS_ping_size) == 0:
+            num_tile_ping = np.ceil(proc_data.ping_time.size / self.MVBS_ping_size).astype(int) + 1
+            z = np.array([np.arange(num_tile_ping - 1)] * self.MVBS_ping_size).squeeze().T.ravel()
+        else:
+            num_tile_ping = np.ceil(proc_data.ping_time.size / self.MVBS_ping_size).astype(int)
+            pad = np.ones(proc_data.ping_time.size - (num_tile_ping - 1) * self.MVBS_ping_size, dtype=int) \
+                  * (num_tile_ping - 1)
+            z = np.hstack(
+                (np.array([np.arange(num_tile_ping - 1)] * self.MVBS_ping_size).squeeze().T.ravel(), pad))
+
+        # Tile bin edges along range
+        # ... -1 to make sure each bin has the same size because of the right-inclusive and left-exclusive bins
+        range_bin_tile_bin = np.arange(num_tile_range_bin+1) * num_range_bin_per_tile - 1
+
+        # Calculate MVBS
+        proc_data.coords['add_idx'] = ('ping_time', z)
+        MVBS = proc_data.Sv.groupby('add_idx').mean('ping_time').\
+            groupby_bins('range_bin', range_bin_tile_bin).mean(['range_bin'])
+
+        # Set MVBS coordinates
+        ping_time = proc_data.ping_time[list(map(lambda x: x[0], list(proc_data.ping_time.groupby('add_idx').groups.values())))]
+        MVBS.coords['ping_time'] = ('add_idx', ping_time)
+        MVBS.coords['ping_time'] = ('add_idx', ping_time)
+        range_bin = list(map(lambda x: x[0], list(proc_data.range_bin.
+                                                       groupby_bins('range_bin', range_bin_tile_bin).groups.values())))
+        MVBS.coords['range_bin'] = ('range_bin_bins', range_bin)
+        MVBS.swap_dims({'range_bin_bins': 'range_bin'})
+
+        # Save results in object and as a netCDF file
+        print('%s  saving MVBS to %s' % (dt.datetime.now().strftime('%H:%M:%S'), self.MVBS_path))
+        self.MVBS = MVBS
+        MVBS.to_netcdf(self.MVBS_path)
+        proc_data.close()
