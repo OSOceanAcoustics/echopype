c4fa5f7c9ba77882d2392b37c8a7e89c0a1ad787
Wu-Jung Lee <leewujung@gmail.comm>
2019-10-17 02:17:48 -0400

    update docs

--- echopype/convert/__init__.py
+++ echopype/convert/__init__.py
@@ -1,5 +1,10 @@
 """
 Include code to unpack manufacturer-specific data files into an interoperable netCDF format.
+
+The current version supports:
+
+- Simrad EK60 echosounder ``.raw`` data
+- ASL Environmental Sciences AZFP echosounder ``.01A`` data
 """
 from .convert import Convert
 from .ek60 import ConvertEK60
