cb097b8fc222b63fb767875e74cb676795a6bcf7
Kavin <ng.kavin97@gmail.com>
2019-10-19 23:47:37 -0700

    create a base class for similar attributes among different echosounders

---
+++ echopype/convert/convertbase.py
@@ -0,0 +1,26 @@
+class ConvertBase:
+    # Class for assigning attributes common to all echosounders
+
+    @property
+    def platform_name(self):
+        return self.parameters['platform_name']
+
+    @platform_name.setter
+    def platform_name(self, platform_name):
+        self.parameters['platform_name'] = platform_name
+
+    @property
+    def platform_type(self):
+        return self.parameters['platform_type']
+
+    @platform_type.setter
+    def platform_type(self, platform_type):
+        self.parameters['platform_type'] = platform_type
+
+    @property
+    def platform_code_ICES(self):
+        return self.parameters['platform_code_ICES']
+
+    @platform_code_ICES.setter
+    def platform_code_ICES(self, platform_code_ICES):
+        self.parameters['platform_code_ICES'] = platform_code_ICES
