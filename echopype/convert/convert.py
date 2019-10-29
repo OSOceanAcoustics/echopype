28221f949c76c4d5817608772b7e17049ba378c1
Emilio Mayorga <emiliomayorga@gmail.com>
2019-10-29 16:33:08 -0700

    Suggested simplification of convert.py
    @leewujung I was thinking back at the flow in convert.py, and realized there was a good chunk of rendundancy and some tests happening before more logical ones (ie, testing for the file extension type before testing for whether the file path exists). So, I went ahead and refactored the code to implement this simplification. 
    
    I didn't want to bother forking, cloning, editing locally (for just one module), then submitting a PR. So I've edited it inline here in Github. I have done no testing whatsoever! My intent is not necessarily for you to try to merge this PR, but just to communicate my suggestions
    
    FYI, in this version, `echosounder_type` is no longer used. But I can see that maybe the redundant structure in your code was intended to support, in the future, an expansion of functionality and echotypes handled. In that case, my single-if-else-block approach may not be a good way to go. Use as you see fit!

--- echopype/convert/convert.py
+++ echopype/convert/convert.py
@@ -25,36 +25,30 @@ class Convert:
             Specialized convert object that will be used to produce a .nc file
         """
 
-        # Gets the type of echosounder from the extension of the raw file
         file_name = os.path.basename(path)
-        ext = os.path.splitext(file_name)[1]
-
-        if ext == '.raw':
-            echosounder_type = 'EK60'  # TODO: EK80 also produced .raw files so need something else later
-        elif ext == '.01A':
-            echosounder_type = 'AZFP'
-        else:
-            raise ValueError("'{}' is not a supported file type".format(ext))
 
         if path:
             if not os.path.isfile(path):
                 raise FileNotFoundError(f"There is no file named {os.path.basename(path)}")
+
+            # Gets the type of echosounder from the extension of the raw file
+            # return a Convert object depending on the type of echosounder used to create the raw file
+            ext = os.path.splitext(file_name)[1]
+            if ext == '.raw':
+                echosounder_type = 'EK60'  # TODO: EK80 also produced .raw files so need something else later
+                return ConvertEK60(path)
+            elif ext == '.01A':
+                echosounder_type = 'AZFP'
+                if xml_path:
+                    if '.XML' in xml_path.upper():
+                        if not os.path.isfile(xml_path):
+                            raise FileNotFoundError(f"There is no file named {os.path.basename(xml_path)}")
+                        return ConvertAZFP(path, xml_path)
+                    else:
+                        raise ValueError(f"{os.path.basename(xml_path)} is not an XML file")
+                else:
+                    raise ValueError("XML file is required for AZFP raw data")
+            else:
+                raise ValueError(f"'{ext}' is not a supported file type")
         else:
             raise ValueError("Convert requires the path to a raw file")
-
-        # return a Convert object depending on the type of echosounder used to create the raw file
-        if echosounder_type == 'AZFP':
-            if xml_path:
-                if '.XML' in xml_path.upper():
-                    if not os.path.isfile(xml_path):
-                        raise FileNotFoundError(f"There is no file named {os.path.basename(xml_path)}")
-                else:
-                    raise ValueError(f"{os.path.basename(xml_path)} is not an XML file")
-            else:
-                raise ValueError("XML file is required for AZFP raw data")
-
-            return ConvertAZFP(path, xml_path)
-        elif echosounder_type == 'EK60':
-            return ConvertEK60(path)
-        else:
-            raise ValueError("Unknown file extension")
