ef63326d68947e465d82d6c34d7bf9fc106d351d
Kavin <ng.kavin97@gmail.com>
2019-11-25 15:29:05 -0800

    uncomment initial test code

--- echopype/tests/test_ek60_model.py
+++ echopype/tests/test_ek60_model.py
@@ -34,70 +34,72 @@ def test_noise_estimates_removal():
     assert np.allclose(ds_Sv.values, e_data.Sv.Sv.values, atol=1e-10)
     # assert np.allclose(ds_TS.values, e_data.TS.TS.values, atol=1e-10)
     # Noise estimation via numpy brute force =======
-    # proc_data = xr.open_dataset(Sv_path)
+    proc_data = xr.open_dataset(Sv_path)
 
-    # # Get tile indexing parameters
-    # e_data.noise_est_range_bin_size, add_idx, range_bin_tile_bin_edge = \
-    #     e_data.get_tile_params(r_data_sz=proc_data.range_bin.size,
-    #                            p_data_sz=proc_data.ping_time.size,
-    #                            r_tile_sz=e_data.noise_est_range_bin_size,
-    #                            p_tile_sz=e_data.noise_est_ping_size,
-    #                            sample_thickness=e_data.sample_thickness)
+    # Get tile indexing parameters
+    e_data.noise_est_range_bin_size, add_idx, range_bin_tile_bin_edge = \
+        e_data.get_tile_params(r_data_sz=proc_data.range_bin.size,
+                               p_data_sz=proc_data.ping_time.size,
+                               r_tile_sz=e_data.noise_est_range_bin_size,
+                               p_tile_sz=e_data.noise_est_ping_size,
+                               sample_thickness=e_data.sample_thickness)
 
-    # range_meter = e_data.range
-    # TVG = np.real(20 * np.log10(range_meter.where(range_meter != 0, other=1)))
-    # ABS = 2 * e_data.seawater_absorption * range_meter
-    # power_cal_test = (10 ** ((proc_data.Sv - ABS - TVG) / 10)).values
+    range_meter = e_data.range
+    TVG = np.real(20 * np.log10(range_meter.where(range_meter >= 1, other=1)))
+    ABS = 2 * e_data.seawater_absorption * range_meter
+    power_cal_test = (10 ** ((proc_data.Sv - ABS - TVG) / 10)).values
 
-    # num_ping_bins = np.unique(add_idx).size
-    # num_range_bins = range_bin_tile_bin_edge.size - 1
-    # noise_est_tmp = np.empty((proc_data.frequency.size, num_range_bins, num_ping_bins))  # all tiles
-    # noise_est_test = np.empty((proc_data.frequency.size, num_ping_bins))  # all columns
-    # p_sz = e_data.noise_est_ping_size
-    # p_idx = np.arange(p_sz, dtype=int)
-    # r_sz = (e_data.noise_est_range_bin_size.max() / e_data.sample_thickness[0].values).astype(int)
-    # r_idx = np.arange(r_sz, dtype=int)
+    num_ping_bins = np.unique(add_idx).size
+    num_range_bins = range_bin_tile_bin_edge.size - 1
+    noise_est_tmp = np.empty((proc_data.frequency.size, num_range_bins, num_ping_bins))  # all tiles
+    noise_est_test = np.empty((proc_data.frequency.size, num_ping_bins))  # all columns
+    p_sz = e_data.noise_est_ping_size
+    p_idx = np.arange(p_sz, dtype=int)
+    r_sz = (e_data.noise_est_range_bin_size.max() / e_data.sample_thickness[0].values).astype(int)
+    r_idx = np.arange(r_sz, dtype=int)
 
-    # # Get noise estimates manually
-    # for f, f_seq in enumerate(np.arange(proc_data.frequency.size)):
-    #     for p, p_seq in enumerate(np.arange(num_ping_bins)):
-    #         for r, r_seq in enumerate(np.arange(num_range_bins)):
-    #             if p_idx[-1] + p_sz * p_seq < power_cal_test.shape[1]:
-    #                 pp_idx = p_idx + p_sz * p_seq
-    #             else:
-    #                 pp_idx = np.arange(p_sz * p_seq, power_cal_test.shape[1])
-    #             if r_idx[-1] + r_sz * r_seq < power_cal_test.shape[2]:
-    #                 rr_idx = r_idx + r_sz * r_seq
-    #             else:
-    #                 rr_idx = np.arange(r_sz * r_seq, power_cal_test.shape[2])
-    #             nn = power_cal_test[f_seq, :, :][np.ix_(pp_idx, rr_idx)]
-    #             noise_est_tmp[f_seq, r_seq, p_seq] = 10 * np.log10(nn.mean())
-    #         noise_est_test[f_seq, p_seq] = noise_est_tmp[f_seq, :, p_seq].min()
+    # Get noise estimates manually
+    for f, f_seq in enumerate(np.arange(proc_data.frequency.size)):
+        for p, p_seq in enumerate(np.arange(num_ping_bins)):
+            for r, r_seq in enumerate(np.arange(num_range_bins)):
+                if p_idx[-1] + p_sz * p_seq < power_cal_test.shape[1]:
+                    pp_idx = p_idx + p_sz * p_seq
+                else:
+                    pp_idx = np.arange(p_sz * p_seq, power_cal_test.shape[1])
+                if r_idx[-1] + r_sz * r_seq < power_cal_test.shape[2]:
+                    rr_idx = r_idx + r_sz * r_seq
+                else:
+                    rr_idx = np.arange(r_sz * r_seq, power_cal_test.shape[2])
+                nn = power_cal_test[f_seq, :, :][np.ix_(pp_idx, rr_idx)]
+                noise_est_tmp[f_seq, r_seq, p_seq] = 10 * np.log10(nn.mean())
+            noise_est_test[f_seq, p_seq] = noise_est_tmp[f_seq, :, p_seq].min()
 
-    # # Check xarray and numpy noise estimates
-    # assert np.all(np.isclose(noise_est_test, noise_est.noise_est.values))
+    # Check xarray and numpy noise estimates
+    assert np.all(np.isclose(noise_est_test, noise_est.noise_est.values))
 
-    # # Remove noise manually
-    # Sv_clean_test = np.empty(proc_data.Sv.shape)
-    # for f, f_seq in enumerate(np.arange(proc_data.frequency.size)):
-    #     for p, p_seq in enumerate(np.arange(num_ping_bins)):
-    #         if p_idx[-1] + p_sz * p_seq < power_cal_test.shape[1]:
-    #             pp_idx = p_idx + p_sz * p_seq
-    #         else:
-    #             pp_idx = np.arange(p_sz * p_seq, power_cal_test.shape[1])
-    #         ss_tmp = proc_data.Sv.values[f_seq, pp_idx, :]
-    #         nn_tmp = (noise_est_test[f_seq, p_seq] +
-    #                   ABS.isel(frequency=f_seq) + TVG.isel(frequency=f_seq)).values
-    #         Sv_clean_tmp = ss_tmp.copy()
-    #         Sv_clean_tmp[Sv_clean_tmp < nn_tmp] = np.nan
-    #         Sv_clean_test[f_seq, pp_idx, :] = Sv_clean_tmp
+    # Remove noise manually
+    Sv_clean_test = np.empty(proc_data.Sv.shape)
+    for f, f_seq in enumerate(np.arange(proc_data.frequency.size)):
+        for p, p_seq in enumerate(np.arange(num_ping_bins)):
+            if p_idx[-1] + p_sz * p_seq < power_cal_test.shape[1]:
+                pp_idx = p_idx + p_sz * p_seq
+            else:
+                pp_idx = np.arange(p_sz * p_seq, power_cal_test.shape[1])
+            ss_tmp = proc_data.Sv.values[f_seq, pp_idx, :]
+            nn_tmp = (noise_est_test[f_seq, p_seq] +
+                      ABS.isel(frequency=f_seq) + TVG.isel(frequency=f_seq)).values
+            Sv_clean_tmp = ss_tmp.copy()
+            Sv_clean_tmp[Sv_clean_tmp < nn_tmp] = np.nan
+            Sv_clean_test[f_seq, pp_idx, :] = Sv_clean_tmp
 
-    # # Check xarray and numpy noise removal
-    # assert ~np.any(e_data.Sv_clean.Sv_clean.values[~np.isnan(e_data.Sv_clean.Sv_clean.values)]
-    #                != Sv_clean_test[~np.isnan(Sv_clean_test)])
+    # Check xarray and numpy noise removal
+    assert ~np.any(e_data.Sv_clean.Sv_clean.values[~np.isnan(e_data.Sv_clean.Sv_clean.values)]
+                   != Sv_clean_test[~np.isnan(Sv_clean_test)])
 
-    # proc_data.close()
+    proc_data.close()
     del tmp
     del e_data
     os.remove(nc_path)
     os.remove(Sv_path)
+
+test_noise_estimates_removal()
