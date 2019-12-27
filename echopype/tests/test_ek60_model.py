7a1595932c8eec686f2033b9542976fb4bca10c1
Wu-Jung Lee <leewujung@gmail.comm>
2019-12-26 21:43:31 -0800

    fixed bug in modelbase/remove_noise, code before didn't average over ping_time within each group

--- echopype/tests/test_ek60_model.py
+++ echopype/tests/test_ek60_model.py
@@ -93,8 +93,8 @@ def test_noise_estimates_removal():
             Sv_clean_test[ff, pp_idx, :] = Sv_clean_tmp
 
     # Check xarray and numpy noise removal
-    # assert ~np.any(e_data.Sv_clean['Sv'].values[~np.isnan(e_data.Sv_clean['Sv'].values)]
-    #                != Sv_clean_test[~np.isnan(Sv_clean_test)])
+    assert ~np.any(e_data.Sv_clean['Sv'].values[~np.isnan(e_data.Sv_clean['Sv'].values)]
+                   != Sv_clean_test[~np.isnan(Sv_clean_test)])
 
     proc_data.close()
     del tmp
