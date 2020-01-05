b97ea3c06f065a37a0be6237d5dd56bda4fb085f
Kavin <ng.kavin97@gmail.com>
2020-01-05 13:13:28 -0800

    add sample thickness and range calculation

--- echopype/model/ek80.py
+++ echopype/model/ek80.py
@@ -43,13 +43,29 @@ class ModelEK80(ModelBase):
 
     def calc_seawater_absorption(self, src='FG'):
         with xr.open_dataset(self.file_path, group='Beam') as ds_beam:
-            freq = ds_beam.frequency.astype(np.int64)  # should already be in unit [Hz]
-        sea_abs = uwa.calc_seawater_absorption(freq,
+            f0 = ds_beam.frequency_start
+            f1 = ds_beam.frequency_end
+            f_center = (f0 + f1) / 2
+        sea_abs = uwa.calc_seawater_absorption(f_center,
                                                temperature=self.temperature,
                                                salinity=self.salinity,
                                                pressure=self.pressure,
                                                formula_source='FG')
         return sea_abs
+
+    def calc_sample_thickness(self):
+        with xr.open_dataset(self.file_path, group="Beam") as ds_beam:
+            sth = self.sound_speed * ds_beam.sample_interval / 2  # sample thickness
+            return sth
+
+    def calc_range(self):
+        """Calculates range in meters using parameters stored in the .nc file.
+        """
+        with xr.open_dataset(self.file_path, group="Beam") as ds_beam:
+            range_meter = ds_beam.range_bin * self.sample_thickness - \
+                ds_beam.transmit_duration_nominal * self.sound_speed / 2  # DataArray [frequency x range_bin]
+            range_meter = range_meter.where(range_meter > 0, other=0)
+            return range_meter
 
     def calibrate(self, save=False):
         # Hard-coded values supplied by Simrad
@@ -68,10 +84,14 @@ class ModelEK80(ModelBase):
         # Average accross quadrants and take the absolute value of complex backscatter
         prx = np.sqrt(np.mean(backscatter_r, 1) ** 2 + np.mean(backscatter_i, 1) ** 2)
         prx = prx * prx / 2 * (np.abs(Rwbtrx + Ztrd) / Rwbtrx) ** 2 / np.abs(Ztrd)
-        sea_abs = uwa.calc_seawater_absorption(f_center,
-                                               temperature=self.temperature,
-                                               salinity=self.salinity,
-                                               pressure=self.pressure,
-                                               formula_source='FG')
+        sea_abs = self.seawater_absorption
+
+        la2 = (c / f_center) ** 2
+        f = np.moveaxis(np.array((ds_beam.frequency_start, ds_beam.frequency_end)), 0, 2)
+        # TODO Gfc should be gain interpolated at the center frequency
+        Gfc = np.mean(ds_beam.gain_correction.values)
+        ptx = ds_beam.transmit_power
+        r = self.range
+        r = r.where(r >= 1, other=1)
 
         pass
