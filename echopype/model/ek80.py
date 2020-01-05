58bfc1f68eba58bba14753c6db5a5efc21a33642
Kavin <ng.kavin97@gmail.com>
2020-01-05 00:39:56 -0800

    start calibrate

--- echopype/model/ek80.py
+++ echopype/model/ek80.py
@@ -5,6 +5,7 @@ echopype data model inherited from based class EchoData for EK80 data.
 import datetime as dt
 import numpy as np
 import xarray as xr
+from echopype.utils import uwa
 from .modelbase import ModelBase
 
 
@@ -18,11 +19,10 @@ class ModelEK80(ModelBase):
         self._temperature = None
         self._pressure = None
 
-
     def get_salinity(self):
         if self._salinity is None:
             with xr.open_dataset(self.file_path, group="Environment") as ds_env:
-                self._salinity = ds_env._salinity
+                self._salinity = ds_env.salinity
         return self._salinity
 
     def get_temperature(self):
@@ -40,6 +40,38 @@ class ModelEK80(ModelBase):
     def get_sound_speed(self):
         with xr.open_dataset(self.file_path, group="Environment") as ds_env:
             return ds_env.sound_speed_indicative
-    
+
+    def calc_seawater_absorption(self, src='FG'):
+        with xr.open_dataset(self.file_path, group='Beam') as ds_beam:
+            freq = ds_beam.frequency.astype(np.int64)  # should already be in unit [Hz]
+        sea_abs = uwa.calc_seawater_absorption(freq,
+                                               temperature=self.temperature,
+                                               salinity=self.salinity,
+                                               pressure=self.pressure,
+                                               formula_source='FG')
+        return sea_abs
+
     def calibrate(self, save=False):
+        # Hard-coded values supplied by Simrad
+        Rwbtrx = 1000
+        Ztrd = 75
+
+        ds_beam = xr.open_dataset(self.file_path, group="Beam")
+        f0 = ds_beam.frequency_start
+        f1 = ds_beam.frequency_end
+        f_center = (f0 + f1) / 2
+        f_nominal = ds_beam.frequency
+        c = self.sound_speed
+
+        backscatter_r = ds_beam.backscatter_r
+        backscatter_i = ds_beam.backscatter_i
+        # Average accross quadrants and take the absolute value of complex backscatter
+        prx = np.sqrt(np.mean(backscatter_r, 1) ** 2 + np.mean(backscatter_i, 1) ** 2)
+        prx = prx * prx / 2 * (np.abs(Rwbtrx + Ztrd) / Rwbtrx) ** 2 / np.abs(Ztrd)
+        sea_abs = uwa.calc_seawater_absorption(f_center,
+                                               temperature=self.temperature,
+                                               salinity=self.salinity,
+                                               pressure=self.pressure,
+                                               formula_source='FG')
+
         pass
