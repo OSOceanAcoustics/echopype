f5f095781acf612b85360549435c01eec7a82c52
Kavin <ng.kavin97@gmail.com>
2020-01-19 13:39:01 -0800

    calculate ytx

--- echopype/model/ek80.py
+++ echopype/model/ek80.py
@@ -18,6 +18,14 @@ class ModelEK80(ModelBase):
         self._salinity = None
         self._temperature = None
         self._pressure = None
+        self._ch_ids = None
+
+    @property
+    def ch_ids(self):
+        if self._ch_ids is None:
+            with xr.open_dataset(self.file_path, group="Beam") as ds_beam:
+                self._ch_ids = ds_beam.channel_id.data
+        return self._ch_ids
 
     def get_salinity(self):
         if self._salinity is None:
@@ -68,13 +76,57 @@ class ModelEK80(ModelBase):
             return range_meter
 
     def calibrate(self, save=False):
-        # Hard-coded values supplied by Simrad
-        Rwbtrx = 1000
-        Ztrd = 75
+        def calc_sent_signal():
+            def chirp_linear(t, f0, f1, tau):
+                beta = (f1 - f0) * (tau ** -1)
+                return np.cos(2 * np.pi * (beta / 2 * (t ** 2) + f0 * t))
+            # Retrieve filter coefficients
+            ds_fil = xr.open_dataset(self.file_path, group="Vendor")
+            # WBT signal generator
+            delta = 1 / 1.5e6   # Hard-coded EK80 sample interval
+            Ztrd = 75           # Transducer quadrant nominal impedance [Ohms]
+            a = np.sqrt((txpower / 4) * (2 * Ztrd))
 
+            # Create transmit signal
+            ytx = []
+            for i in range(len(f0)):
+                t = np.arange(0, tau[i], delta)
+                nt = len(t)
+                nwtx = (int(2 * np.floor(slope[i].data * nt)))
+                wtx_tmp = np.hanning(nwtx)
+                nwtxh = (int(np.round(nwtx / 2)))
+                wtx = np.concatenate([wtx_tmp[0:nwtxh], np.ones((nt - nwtx)), wtx_tmp[nwtxh:]])
+                y_tmp = a[i] * chirp_linear(t, f0[i], f1[i], tau[i]) * wtx
+                # The transmit signal must have a max amplitude of 1
+                y = (y_tmp / np.max(np.abs(y_tmp)))
+
+                # filter and decimation
+                wbt_fil = ds_fil[self.ch_ids[i] + "_WBT_filter"].data
+                pc_fil = ds_fil[self.ch_ids[i] + "_PC_filter"].data
+                # if saved as netCDF4, convert compound complex datatype to complex64
+                if wbt_fil.ndim == 1:
+                    wbt_fil = np.array([complex(n[0], n[1]) for n in wbt_fil], dtype='complex64')
+                    pc_fil = np.array([complex(n[0], n[1]) for n in pc_fil], dtype='complex64')
+                # Apply WBT filter and downsample
+                ytx_tmp = np.convolve(y, wbt_fil)
+                ytx_tmp = ytx_tmp[0::ds_fil.attrs[self.ch_ids[i] + "_WBT_decimation"]]
+                # Apply PC filter and downsample
+                ytx_tmp = np.convolve(ytx_tmp, pc_fil)
+                ytx_tmp = ytx_tmp[0::ds_fil.attrs[self.ch_ids[i] + "_PC_decimation"]]
+                ytx.append(ytx_tmp)
+                del nwtx, wtx_tmp, nwtxh, wtx, y_tmp, y, ytx_tmp
+            return np.array(ytx)
+
+        # Hard-coded EK80 values supplied by Simrad
+        Rwbtrx = 1000       # Wideband transceiver impedance [Ohms]
         ds_beam = xr.open_dataset(self.file_path, group="Beam")
-        f0 = ds_beam.frequency_start
-        f1 = ds_beam.frequency_end
+        tau = ds_beam.transmit_duration_nominal.data
+        txpower = ds_beam.transmit_power.data
+        f0 = ds_beam.frequency_start[:, 0].data     # Use start frequency of first ping
+        f1 = ds_beam.frequency_end[:, 0].data       # Use end frequency of first ping
+        slope = ds_beam.slope[:, 0]                 # Use slope frequency of first ping
+        ytx = calc_sent_signal()
+
         f_center = (f0 + f1) / 2
         f_nominal = ds_beam.frequency
         c = self.sound_speed
@@ -93,5 +145,4 @@ class ModelEK80(ModelBase):
         ptx = ds_beam.transmit_power
         r = self.range
         r = r.where(r >= 1, other=1)
-
         pass
