966183cbfb01bcf278999d30fa6225b12e24ed81
Wu-Jung Lee <leewujung@gmail.comm>
2020-02-20 23:53:01 -0800

    minor printout changes

--- echopype/model/ek60.py
+++ echopype/model/ek60.py
@@ -76,6 +76,9 @@ class ModelEK60(ModelBase):
         save_path : str
             Full filename to save to, overwriting the RAWFILENAME_Sv.nc default
         """
+        # Print raw data nc file
+        print('%s  calibrating data in %s' % (dt.datetime.now().strftime('%H:%M:%S'), self.file_path))
+
         # Open data set for Environment and Beam groups
         ds_beam = xr.open_dataset(self.file_path, group="Beam")
 
