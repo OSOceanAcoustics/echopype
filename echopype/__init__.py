717aa711652566d92d4fc055c6fe244588fc37aa
leewujung <leewujung@gmail.com>
2020-06-19 15:08:51 -0400

    change root __init__.py to allow less verbose access to Convert and Process

--- echopype/__init__.py
+++ echopype/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function
-from . import convert
-from . import process
+from .convert import Convert
+from .process import Process
 from . import model     # Delete in later patch
 
 from ._version import get_versions
