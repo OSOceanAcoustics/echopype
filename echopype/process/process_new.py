908308919fc62eaec774b9651995df8bc7e2c0e3
Kavin <ng.kavin97@gmail.com>
2020-11-24 18:16:21 -0800

    remove unused functions

--- echopype/process/process_new.py
+++ echopype/process/process_new.py
@@ -6,6 +6,8 @@ Some operations are instrument-dependent, such as calibration to obtain Sv.
 Some operations are instrument-agnostic, such as obtaining MVBS or detecting bottom.
 """
 import warnings
+from datetime import datetime as dt
+import xarray as xr
 from ..utils import uwa
 from . import process_classes
 from .echodata import EchoDataBase
@@ -34,14 +36,6 @@ class Process:
         self.sonar_model = model   # type of echosounder
         self.process_obj = self.PROCESS_SONAR[model]   # process object to use
 
-        # TODO: we need something to restrict the type of parameters users
-        #  can put in to these dictionaries,
-        #  for example, for env_params we allow only:
-        #     'sea_water_salinity'            [psu]
-        #     'sea_water_temperature'         [degC]
-        #     'sea_water_pressure'            [dbars] (~depth in meters)
-        #     'speed_of_sound_in_sea_water'   [m/s]
-        #     'seawater_absorption'           [dB/m]
         self._env_params = {}   # env parameters
         self._cal_params = {}   # cal parameters, eg: equivalent beam width sa_correction for EK60
         self._proc_params = {}  # proc parameters, eg: MVBS bin size
@@ -157,21 +151,13 @@ class Process:
     def _check_model_echodata_match(self, ed):
         """Check if sonar model corresponds with the type of data in EchoData object.
         """
-        # Check is self.sonar_model and ed.sonar_model are the same
-        # Raise error if they do not match
-
-    def _autofill_save_path(self, save_type):
-        """
-        Autofill the paths to save the processing results if not already set.
-        The default paths will be to the same folder as the raw data files.
-
-        Use case is something like:
-            proc._autofill_save_path(save_type='Sv')
-
-        Parameters
-        ----------
-        save_type
-        """
+        if ed.raw_path is None:
+            raise ValueError("EchoData object does not have raw files to calibrate")
+        else:
+            with xr.open_dataset(ed.raw_path) as ds:
+                model = ds.keywords
+            if model != self.sonar_model:
+                raise ValueError(f"Proccess sonar {self.sonar_model} does not match EchoData sonar {model}")
 
     def align_to_range(self, ed, param_source='file'):
         """
@@ -212,59 +198,34 @@ class Process:
         # Users then have the option to use ed.Sv.zarr() or other xarray function
         # to explore the data.
 
-    def calibrate(self, ed, param_source='file', save=True, save_format='zarr'):
+    def calibrate(self, ed, save=True, save_path=None, save_format='zarr'):
         """Calibrate raw data.
 
         Parameters
         ----------
         ed : EchoDataBase
             EchoData object to operate on
-        param_source : str
-            'file' or 'user'
         save : bool
         save_format : str
         """
-        # Check if sonar model matches
-        self._check_model_echodata_match(ed)
-
-        # Obtain env parameters
-        #  Here we want to obtain the env params stored in the data file, but
-        #  overwrite those that are specified by user
-        #  We can first do a check to see what parameters we still need to get
-        #  from the raw files before retrieving them (I/O is slow,
-        #  so let's not do that unless needed).
-
-        # To get access to parameters stored in the raw data, use:
-        ed.get_env_from_raw()
-        ed.get_vend_from_raw()
-
-        # Obtain cal parameters
-        #  Operations are very similar to those from the env parameters,
-        #  for AZFP AFAIK some additional parameters are needed from the vendor group
-        #  to calculate range
-
-        # Autofill save paths if not already generated
-        if save and ('Sv' not in self.save_paths):
-            self._autofill_save_path('Sv')
 
         # Perform calibration
         #  this operation should make an xarray Dataset in ed.Sv
-        #  if save=True: save the results as zarr in self.save_paths['Sv'] and update ed.Sv_path
-        #  users obviously would have the option to do ed.Sv.to_zarr() to wherever they like
-        self.process_obj.get_Sv(
+        self.get_Sv(
             ed,
-            env_params=self.env_params,
-            cal_params=self.cal_params,
             save=save,
+            save_path=save_path,
             save_format=save_format
         )
 
     def get_Sv(self, ed, save=False, save_path=None, save_format='zarr'):
+        self._check_model_echodata_match(ed)
         print('%s  calibrating data in %s' % (dt.now().strftime('%H:%M:%S'), ed.raw_path))
         return self.process_obj.get_Sv(ed=ed, env_params=self.env_params, cal_params=self.cal_params,
                                        save=save, save_path=save_path, save_format=save_format)
 
     def get_TS(self, ed, save=False, save_path=None, save_format='zarr'):
+        self._check_model_echodata_match(ed)
         print('%s  calibrating data in %s' % (dt.now().strftime('%H:%M:%S'), ed.raw_path))
         return self.process_obj.get_TS(ed=ed, env_params=self.env_params, cal_params=self.cal_params,
                                        save=save, save_path=save_path, save_format=save_format)
