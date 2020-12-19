3adeefa9aac9f948bb47dff3e7182e3d5d86c7e5
Kavin <ng.kavin97@gmail.com>
2020-12-19 15:05:50 -0800

    add file format check and add comments

--- echopype/utils/io.py
+++ echopype/utils/io.py
@@ -44,11 +44,14 @@ def validate_proc_path(ed, postfix, save_path=None):
 
 
 def get_files_from_dir(folder):
+    """Retrieves all Netcdf and Zarr files from a given folder"""
     valid_ext = ['.nc', '.zarr']
     return [f for f in os.listdir(folder) if os.path.splitext(f)[1] in valid_ext]
 
 
 def save_file(ds, path, mode, engine, group=None, compression_settings=None):
+    """Saves a dataset to netcdf or zarr depending on the engine
+    If ``compression_settings`` are set, compress all variables with those settings"""
     encoding = {var: compression_settings for var in ds.data_vars} if compression_settings is not None else {}
 
     # Allows saving both NetCDF and Zarr files from an xarray dataset
@@ -59,8 +62,11 @@ def save_file(ds, path, mode, engine, group=None, compression_settings=None):
 
 
 def get_file_format(file):
+    """Gets the file format (either Netcdf4 or Zarr) from the file extension"""
     file = file.root if isinstance(file, MutableMapping) else file
     if file.endswith('.nc'):
         return 'netcdf4'
     elif file.endswith('.zarr'):
         return 'zarr'
+    else:
+        raise ValueError(f"Unsupported file format: {os.path.splitext(file)[1]}")
