ede338698df2413816111292ad7a969d3d5f8c7f
Landung Setiawan <landungs@uw.edu>
2020-12-21 11:19:12 -0800

    Add few more tests for http files

--- echopype/tests/test_convert.py
+++ echopype/tests/test_convert.py
@@ -5,48 +5,75 @@ import xarray as xr
 import pandas as pd
 from ..convert import Convert
 
+
+def _converted_group_checker(model, engine, out_file):
+    groups = ['Environment', 'Platform', 'Provenance', 'Sonar']
+    if model in ['EK60', 'EK80']:
+        groups = groups + ['Beam', 'Vendor']
+
+    for g in groups:
+        ds = xr.open_dataset(out_file, engine=engine, group=g)
+
+        assert isinstance(ds, xr.Dataset) is True
+
+
+def _file_export_checks(ec, model):
+    file_map = {
+        '.nc': {
+            'engine': 'netcdf4',
+            'export_func': ec.to_netcdf,
+            'cleaner': os.unlink,
+        },
+        '.zarr': {
+            'engine': 'zarr',
+            'export_func': ec.to_zarr,
+            'cleaner': shutil.rmtree,
+        },
+    }
+    for k, v in file_map.items():
+        out_file = f"./test_{model.lower()}{k}"
+        func = v['export_func']
+        clean = v['cleaner']
+        func(out_file, overwrite=True)
+        _converted_group_checker(
+            model=model, engine=v['engine'], out_file=out_file
+        )
+        clean(out_file)
+
+
 def test_http_azfp_convert():
+    model = 'AZFP'
     # http
     azfp_path = 'https://rawdata.oceanobservatories.org/files/CE01ISSM/R00007/instrmts/dcl37/ZPLSC_sn55075/ce01issm_zplsc_55075_recovered_2017-10-27/DATA/201703/17032923.01A'
     xml_path = 'https://rawdata.oceanobservatories.org/files/CE01ISSM/R00007/instrmts/dcl37/ZPLSC_sn55075/ce01issm_zplsc_55075_recovered_2017-10-27/DATA/201703/17032922.XML'
 
-    out_file = "./test.nc"
-    ec = Convert(azfp_path, model='AZFP', xml_path=xml_path)
+    ec = Convert(azfp_path, model=model, xml_path=xml_path)
 
     assert ec.source_file[0] == azfp_path
     assert ec.xml_path == xml_path
 
-    ec.to_netcdf(out_file, overwrite=True)
-    groups = ['Environment', 'Platform', 'Provenance', 'Sonar']
+    _file_export_checks(ec, model)
 
-    for g in groups:
-        ds = xr.open_dataset(out_file, engine='netcdf4', group=g)
-
-        assert isinstance(ds, xr.Dataset) is True
-
-    os.unlink(out_file)
 
 def test_http_ek60_convert():
+    model = 'EK60'
     # http
-    ek60_path = 'https://rawdata.oceanobservatories.org/files/CE04OSPS/PC01B/ZPLSCB102_10.33.10.143/2017/08/21/OOI-D20170821-T031816.raw'
+    ek60_path = 'https://ncei-wcsd-archive.s3-us-west-2.amazonaws.com/data/raw/Bell_M._Shimada/SH1707/EK60/Summer2017-D20170615-T190214.raw'
 
-    out_file = "./test.nc"
-    ec = Convert(ek60_path, model='EK60')
-    ec.to_netcdf(out_file, overwrite=True)
+    ec = Convert(ek60_path, model=model)
 
     assert ec.source_file[0] == ek60_path
 
-    os.unlink(out_file)
+    _file_export_checks(ec, model)
 
-# TODO: Need ek80 http example
-# def test_http_ek80_convert():
-#     # http
-#     ek80_path = 'https://rawdata.oceanobservatories.org/files/CE01ISSM/R00007/instrmts/dcl37/ZPLSC_sn55075/ce01issm_zplsc_55075_recovered_2017-10-27/DATA/201703/17032923.01A'
 
-#     out_file = "./test.nc"
-#     ec = Convert(ek80_path, model='EK80')
-#     ec.to_netcdf(out_file, overwrite=True)
+def test_http_ek80_convert():
+    model = 'EK80'
+    # http
+    ek80_path = 'https://ncei-wcsd-archive.s3-us-west-2.amazonaws.com/data/raw/Bell_M._Shimada/SH1707/EK80/D20170826-T205615.raw'
 
-#     assert ec.source_file[0] == ek80_path
+    ec = Convert(ek80_path, model=model)
 
-#     os.unlink(out_file)
+    assert ec.source_file[0] == ek80_path
+
+    _file_export_checks(ec, model)
