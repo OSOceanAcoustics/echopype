7e6be75eeb9f08f4f822489a375b9794cd2fb3c6
imranmaj <49664304+imranmaj@users.noreply.github.com>
2021-01-15 17:20:59 -0800

    Add ad2cp parsing

--- echopype/convert/__init__.py
+++ echopype/convert/__init__.py
@@ -11,7 +11,9 @@ from .convert import Convert, ConvertEK80       # TODO remove ConvertEK80 in lat
 from .parse_ek60 import ParseEK60
 from .parse_ek80 import ParseEK80
 from .parse_azfp import ParseAZFP
+from .parse_ad2cp import ParseAd2cp
 from .parse_base import ParseBase
 from .set_groups_azfp import SetGroupsAZFP
 from .set_groups_ek60 import SetGroupsEK60
 from .set_groups_ek80 import SetGroupsEK80
+from .set_groups_ad2cp import SetGroupsAd2cp
--- echopype/convert/convert.py
+++ echopype/convert/convert.py
@@ -12,9 +12,11 @@ import fsspec
 from .parse_azfp import ParseAZFP
 from .parse_ek60 import ParseEK60
 from .parse_ek80 import ParseEK80
+from .parse_ad2cp import ParseAd2cp
 from .set_groups_azfp import SetGroupsAZFP
 from .set_groups_ek60 import SetGroupsEK60
 from .set_groups_ek80 import SetGroupsEK80
+from .set_groups_ad2cp import SetGroupsAd2cp
 from ..utils import io
 
 MODELS = {
@@ -41,6 +43,12 @@ MODELS = {
         "xml": False,
         "parser": ParseEK80,
         "set_groups": SetGroupsEK80
+    },
+    "AD2CP": {
+        "ext": ".ad2cp",
+        "xml": False,
+        "parser": ParseAd2cp,
+        "set_groups": SetGroupsAd2cp
     }
 }
 
