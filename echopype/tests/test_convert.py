d411f5bab1e6ec6e481c66e231af3a4e0674cbc9
Landung Setiawan <landungs@uw.edu>
2021-01-21 10:53:21 -0800

    Include validate path for object storage

--- echopype/tests/test_convert.py
+++ echopype/tests/test_convert.py
@@ -8,6 +8,21 @@ from pathlib import Path
 from ..convert import Convert
 
 
+@pytest.fixture(scope="session")
+def minio_bucket():
+    bucket_name = 'ooi-raw-data'
+    fs = fsspec.filesystem(
+        's3',
+        **dict(
+            client_kwargs=dict(endpoint_url='http://localhost:9000/'),
+            key='minioadmin',
+            secret='minioadmin',
+        ),
+    )
+    if not fs.exists(bucket_name):
+        fs.mkdir(bucket_name)
+
+
 @pytest.mark.parametrize("model", ["EK60"])
 @pytest.mark.parametrize("file_format", [".zarr"])
 @pytest.mark.parametrize(
@@ -24,36 +39,67 @@ from ..convert import Convert
         "./echopype/test_data/dump/",
         "./echopype/test_data/dump/tmp.zarr",
         "./echopype/test_data/dump/tmp.nc",
+        "s3://ooi-raw-data/dump/",
+        "s3://ooi-raw-data/dump/tmp.zarr",
+        "s3://ooi-raw-data/dump/tmp.nc",
     ],
 )
 def test_validate_path_single_source(
-    model, file_format, input_path, output_save_path
+    model, file_format, input_path, output_save_path, minio_bucket
 ):
+
+    output_storage_options = {}
+    if output_save_path and output_save_path.startswith('s3://'):
+        output_storage_options = dict(
+            client_kwargs=dict(endpoint_url='http://localhost:9000/'),
+            key='minioadmin',
+            secret='minioadmin',
+        )
     fsmap = fsspec.get_mapper(input_path)
     single_dir = os.path.dirname(fsmap.root)
     single_fname = os.path.splitext(os.path.basename(fsmap.root))[0]
     tmp_single = Convert(input_path, model=model)
+    tmp_single._output_storage_options = output_storage_options
 
     tmp_single._validate_path(
         file_format=file_format, save_path=output_save_path
     )
 
     if output_save_path is not None:
-        fsmap_tmp = fsspec.get_mapper(output_save_path)
-        if output_save_path.endswith('/'):
-            # if an output folder is given, below works with and without the slash at the end
-            assert tmp_single.output_file == [
-                os.path.join(fsmap_tmp.root, single_fname + '.zarr')
-            ]
-        elif output_save_path.endswith('.zarr'):
-            # if an output filename is given
-            assert tmp_single.output_file == [fsmap_tmp.root]
+        fsmap_tmp = fsspec.get_mapper(
+            output_save_path, **output_storage_options
+        )
+        fs = fsmap_tmp.fs
+        if not output_save_path.startswith('s3'):
+            if output_save_path.endswith('/'):
+                # if an output folder is given, below works with and without the slash at the end
+                assert tmp_single.output_file == [
+                    os.path.join(fsmap_tmp.root, single_fname + '.zarr')
+                ]
+            elif output_save_path.endswith('.zarr'):
+                # if an output filename is given
+                assert tmp_single.output_file == [fsmap_tmp.root]
+            else:
+                # force output file extension to the called type (here .zarr)
+                assert tmp_single.output_file == [
+                    os.path.splitext(fsmap_tmp.root)[0] + '.zarr'
+                ]
+            os.rmdir(os.path.dirname(tmp_single.output_file[0]))
         else:
-            # force output file extension to the called type (here .zarr)
-            assert tmp_single.output_file == [
-                os.path.splitext(fsmap_tmp.root)[0] + '.zarr'
-            ]
-        os.rmdir(os.path.dirname(tmp_single.output_file[0]))
+            if output_save_path.endswith('/'):
+                # if an output folder is given, below works with and without the slash at the end
+                assert tmp_single.output_file == [
+                    os.path.join(output_save_path, single_fname + '.zarr')
+                ]
+            elif output_save_path.endswith('.zarr'):
+                # if an output filename is given
+                assert tmp_single.output_file == [output_save_path]
+            else:
+                # force output file extension to the called type (here .zarr)
+                assert tmp_single.output_file == [
+                    os.path.splitext(output_save_path)[0] + '.zarr'
+                ]
+            fs.delete(tmp_single.output_file[0])
     else:
         if input_path.startswith('https') or input_path.startswith('s3'):
             current_dir = Path.cwd()
@@ -87,10 +133,13 @@ def test_validate_path_single_source(
         "./echopype/test_data/dump/",
         "./echopype/test_data/dump/tmp.zarr",
         "./echopype/test_data/dump/tmp.nc",
+        "s3://ooi-raw-data/dump/",
+        "s3://ooi-raw-data/dump/tmp.zarr",
+        "s3://ooi-raw-data/dump/tmp.nc",
     ],
 )
 def test_validate_path_multiple_source(
-    model, file_format, input_path, output_save_path
+    model, file_format, input_path, output_save_path, minio_bucket
 ):
     if isinstance(input_path, str):
         mult_path = glob.glob(input_path)
@@ -106,24 +155,45 @@ def test_validate_path_multiple_source(
 
     if output_save_path is not None:
         fsmap_tmp = fsspec.get_mapper(output_save_path)
-        if output_save_path.endswith('/'):
-            # if an output folder is given, below works with and without the slash at the end
-            assert tmp_mult.output_file == [
-                os.path.join(
-                    fsmap_tmp.root,
-                    os.path.splitext(os.path.basename(f))[0] + '.zarr',
-                )
-                for f in mult_path
-            ]
-        elif output_save_path.endswith('.zarr'):
-            # if an output filename is given: only use the directory
-            assert tmp_mult.output_file == [os.path.abspath(output_save_path)]
-        elif output_save_path.endswith('.nc'):
-            # force output file extension to the called type (here .zarr)
-            assert tmp_mult.output_file == [
-                os.path.abspath(output_save_path.replace('.nc', '.zarr'))
-            ]
-        os.rmdir(os.path.dirname(tmp_mult.output_file[0]))
+        fs = fsmap_tmp.fs
+        if not output_save_path.startswith('s3'):
+            if output_save_path.endswith('/'):
+                # if an output folder is given, below works with and without the slash at the end
+                assert tmp_mult.output_file == [
+                    os.path.join(
+                        fsmap_tmp.root,
+                        os.path.splitext(os.path.basename(f))[0] + '.zarr',
+                    )
+                    for f in mult_path
+                ]
+            elif output_save_path.endswith('.zarr'):
+                # if an output filename is given: only use the directory
+                assert tmp_mult.output_file == [os.path.abspath(output_save_path)]
+            elif output_save_path.endswith('.nc'):
+                # force output file extension to the called type (here .zarr)
+                assert tmp_mult.output_file == [
+                    os.path.abspath(output_save_path.replace('.nc', '.zarr'))
+                ]
+            os.rmdir(os.path.dirname(tmp_mult.output_file[0]))
+        else:
+            if output_save_path.endswith('/'):
+                # if an output folder is given, below works with and without the slash at the end
+                assert tmp_mult.output_file == [
+                    os.path.join(
+                        output_save_path,
+                        os.path.splitext(os.path.basename(f))[0] + '.zarr',
+                    )
+                    for f in mult_path
+                ]
+            elif output_save_path.endswith('.zarr'):
+                # if an output filename is given: only use the directory
+                assert tmp_mult.output_file == [output_save_path]
+            elif output_save_path.endswith('.nc'):
+                # force output file extension to the called type (here .zarr)
+                assert tmp_mult.output_file == [
+                    output_save_path.replace('.nc', '.zarr')
+                ]
+            fs.delete(tmp_mult.output_file[0])
     else:
         if input_path[0].startswith('https') or input_path[0].startswith('s3'):
             current_dir = Path.cwd()
