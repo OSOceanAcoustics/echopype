11c11d103ddb0e27f61320cc54095aa79afc1ad6
imranmaj <49664304+imranmaj@users.noreply.github.com>
2021-01-29 05:22:46 -0800

    Changes based on 1-22-21 meeting (WIP)

--- echopype/convert/set_groups_ad2cp.py
+++ echopype/convert/set_groups_ad2cp.py
@@ -1,9 +1,12 @@
 import os
+from typing import BinaryIO, Union, Callable, List, Optional, Any, Dict
 
 import xarray as xr
 import numpy as np
 
 from .set_groups_base import SetGroupsBase
+from .parse_ad2cp import DataRecordType, Field, Dimension, Ad2cpDataPacket
+from ..utils import io
 
 
 class SetGroupsAd2cp(SetGroupsBase):
@@ -12,41 +15,100 @@ class SetGroupsAd2cp(SetGroupsBase):
             mode = "a"
         else:
             mode = "w"
-        ds.to_netcdf(path=self.output_path, mode=mode, group=group)
+        io.save_file(ds=ds, path=self.output_path, mode=mode, engine=self.engine, group=group)
+        # ds.to_netcdf(path=self.output_path, mode=mode, group=group)
 
     def save(self):
+        self.combine_packets()
         self.set_environment()
         self.set_platform()
         self.set_beam()
         self.set_vendor_specific()
 
+    def combine_packets(self):
+        self.ds = None
+        string_data = dict()
+
+        burst_packets = []
+        average_packets = []
+        echosounder_packets = []
+        for packet in self.parser_obj.packets:
+            if packet.data_record_type in (DataRecordType.BURST_VERSION2, DataRecordType.BURST_VERSION3):
+                burst_packets.append(packet)
+            elif packet.data_record_type in (DataRecordType.AVERAGE_VERSION2, DataRecordType.AVERAGE_VERSION3):
+                average_packets.append(packet)
+            elif packet.data_record_type == DataRecordType.ECHOSOUNDER:
+                echosounder_packets.append(packet)
+        
+        def make_dataset(packets: List[Ad2cpDataPacket], time_dim: str) -> Optional[xr.Dataset]:
+            for i in range(len(packets)):
+                packet = packets[i]
+                data_vars = dict()
+                for field_name, field_value in packet.data.items():
+                    # TODO might not work with altimeter_spare
+                    # add dimension names to data vars for xarray
+                    dims = Field.dimensions(field_name, packet.data_record_type)
+                    if field_name in ("velocity_data", "amplitude_data", "correlation_data", "percentage_good_data"):
+                        if packet.data_record_type in (DataRecordType.BURST_VERSION2, DataRecordType.BURST_VERSION3):
+                            field_name += "_burst"
+                        elif packet.data_record_type in (DataRecordType.AVERAGE_VERSION2, DataRecordType.AVERAGE_VERSION3):
+                            field_name += "_average"
+                    data_vars[field_name] = (tuple(dim.value for dim in dims), [field_value])
+                new_packet = xr.Dataset(
+                    data_vars=data_vars,
+                    coords={"time": [packet.timestamp],
+                    time_dim: [packet.timestamp]}
+                )
+
+                # modify in place to reduce memory consumption
+                packets[i] = new_packet
+            if len(packets) > 0:
+                return xr.concat(packets, dim="time")
+            else:
+                return None
+
+        burst_ds = make_dataset(burst_packets, time_dim="time_burst")
+        # print(burst_ds)
+        average_ds = make_dataset(average_packets, time_dim="time_average")
+        # print(average_ds)
+        echosounder_ds = make_dataset(echosounder_packets, time_dim="time_echosounder")
+        # print(echosounder_ds)
+
+        datasets = [ds for ds in (burst_ds, average_ds, echosounder_ds) if ds]
+        self.ds = xr.merge(datasets)
+        self.ds.attrs["string_data"] = string_data
+
     def set_environment(self):
         ds = xr.Dataset(data_vars={
-            "sound_speed_indicative": self.parser_obj.ds.get("speed_of_sound"),
-            "temperature": self.parser_obj.ds.get("temperature"),
-            "pressure": self.parser_obj.ds.get("pressure")
+            "sound_speed_indicative": self.ds.get("speed_of_sound"),
+            "temperature": self.ds.get("temperature"),
+            "pressure": self.ds.get("pressure")
         },
             coords={
-            "time_burst": self.parser_obj.ds.get("time_burst", []),
-            "time_average": self.parser_obj.ds.get("time_average", [])
+            "time_burst": self.ds.get("time_burst", []),
+            "time_average": self.ds.get("time_average", []),
+            "time_echosounder": self.ds.get("time_echosounder", [])
         })
         self.write(ds, "Environment")
 
     def set_platform(self):
         ds = xr.Dataset(
             data_vars={
-                "heading": self.parser_obj.ds.get("heading"),
-                "pitch": self.parser_obj.ds.get("pitch"),
-                "roll": self.parser_obj.ds.get("roll"),
-                "magnetometer_raw_x": self.parser_obj.ds.get("magnetometer_raw_x"),
-                "magnetometer_raw_y": self.parser_obj.ds.get("magnetometer_raw_y"),
-                "magnetometer_raw_z": self.parser_obj.ds.get("magnetometer_raw_z"),
+                "heading": self.ds.get("heading"),
+                "pitch": self.ds.get("pitch"),
+                "roll": self.ds.get("roll"),
+                "magnetometer_raw_x": self.ds.get("magnetometer_raw_x"),
+                "magnetometer_raw_y": self.ds.get("magnetometer_raw_y"),
+                "magnetometer_raw_z": self.ds.get("magnetometer_raw_z"),
             },
             coords={
-                "time_burst": self.parser_obj.ds.get("time_burst"),
-                "time_average": self.parser_obj.ds.get("time_average"),
-                "beam": self.parser_obj.ds.get("beam"),
-                "range_bin": self.parser_obj.ds.get("range_bin")
+                "time_burst": self.ds.get("time_burst"),
+                "time_average": self.ds.get("time_average"),
+                "time_echosounder": self.ds.get("time_echosounder"),
+                "beam": self.ds.get("beam"),
+                "range_bin_burst": self.ds.get("range_bin_burst"),
+                "range_bin_average": self.ds.get("range_bin_average"),
+                "range_bin_echosounder": self.ds.get("range_bin_echosounder")
             },
             attrs={
                 "platform_name": self.ui_param["platform_name"],
@@ -61,113 +123,118 @@ class SetGroupsAd2cp(SetGroupsBase):
         # like was done for range_bin (we have range_bin_burst, range_bin_average,
         # and range_bin_echosounder)?
         data_vars = {
-            "number_of_beams": self.parser_obj.ds.get("num_beams"),
-            "coordinate_system": self.parser_obj.ds.get("coordinate_system"),
-            "number_of_cells": self.parser_obj.ds.get("num_cells"),
-            "blanking": self.parser_obj.ds.get("blanking"),
-            "cell_size": self.parser_obj.ds.get("cell_size"),
-            "velocity_range": self.parser_obj.ds.get("velocity_range"),
-            "echosounder_frequency": self.parser_obj.ds.get("echo_sounder_frequency"),
-            "ambiguity_velocity": self.parser_obj.ds.get("ambiguity_velocity"),
-            "data_set_description": self.parser_obj.ds.get("dataset_description"),
-            "transmit_energy": self.parser_obj.ds.get("transmit_energy"),
-            "velocity_scaling": self.parser_obj.ds.get("velocity_scaling"),
-            "velocity": self.parser_obj.ds.get("velocity_data"),
-            "amplitude": self.parser_obj.ds.get("amplitude_data"),
-            "correlation": self.parser_obj.ds.get("correlation_data"),
-            "echosounder": self.parser_obj.ds.get("echosounder_data"),
-            "figure_of_merit": self.parser_obj.ds.get("figure_of_merit_data"),
-            "altimeter_distance": self.parser_obj.ds.get("altimeter_distance"),
-            "altimeter_quality": self.parser_obj.ds.get("altimeter_quality"),
-            "ast_distance": self.parser_obj.ds.get("ast_distance"),
-            "ast_quality": self.parser_obj.ds.get("ast_quality"),
-            "ast_offset_10us": self.parser_obj.ds.get("ast_offset_10us"),
-            "ast_pressure": self.parser_obj.ds.get("ast_pressure"),
-            "altimeter_spare": self.parser_obj.ds.get("altimeter_spare"),
-            "altimeter_raw_data_num_samples": self.parser_obj.ds.get("altimeter_raw_data_num_samples"),
-            "altimeter_raw_data_sample_distance": self.parser_obj.ds.get("altimeter_raw_data_sample_distance"),
-            "altimeter_raw_data_samples": self.parser_obj.ds.get("altimeter_raw_data_samples")
+            "number_of_beams": self.ds.get("num_beams"),
+            "coordinate_system": self.ds.get("coordinate_system"),
+            "number_of_cells": self.ds.get("num_cells"),
+            "blanking": self.ds.get("blanking"),
+            "cell_size": self.ds.get("cell_size"),
+            "velocity_range": self.ds.get("velocity_range"),
+            "echosounder_frequency": self.ds.get("echo_sounder_frequency"),
+            "ambiguity_velocity": self.ds.get("ambiguity_velocity"),
+            "data_set_description": self.ds.get("dataset_description"),
+            "transmit_energy": self.ds.get("transmit_energy"),
+            "velocity_scaling": self.ds.get("velocity_scaling"),
+            "velocity": self.ds.get("velocity_data"),
+            "amplitude": self.ds.get("amplitude_data"),
+            "correlation": self.ds.get("correlation_data"),
+            "echosounder": self.ds.get("echosounder_data"),
+            "figure_of_merit": self.ds.get("figure_of_merit_data"),
+            "altimeter_distance": self.ds.get("altimeter_distance"),
+            "altimeter_quality": self.ds.get("altimeter_quality"),
+            "ast_distance": self.ds.get("ast_distance"),
+            "ast_quality": self.ds.get("ast_quality"),
+            "ast_offset_10us": self.ds.get("ast_offset_10us"),
+            "ast_pressure": self.ds.get("ast_pressure"),
+            "altimeter_spare": self.ds.get("altimeter_spare"),
+            "altimeter_raw_data_num_samples": self.ds.get("altimeter_raw_data_num_samples"),
+            "altimeter_raw_data_sample_distance": self.ds.get("altimeter_raw_data_sample_distance"),
+            "altimeter_raw_data_samples": self.ds.get("altimeter_raw_data_samples")
         }
 
         ds = xr.Dataset(
             data_vars=data_vars,
             coords={
-                "time_burst": self.parser_obj.ds.get("time_burst"),
-                "time_average": self.parser_obj.ds.get("time_average"),
-                "beam": self.parser_obj.ds.get("beam"),
-                "range_bin": self.parser_obj.ds.get("range_bin"),
-                "altimeter_sample_bin": self.parser_obj.ds.get("altimeter_sample_bin"),
-                "range_bin_echosounder": self.parser_obj.ds.get("num_beams_and_coordinate_system_and_num_cells")
+                "time_burst": self.ds.get("time_burst"),
+                "time_average": self.ds.get("time_average"),
+                "time_echosounder": self.ds.get("time_echosounder"),
+                "beam": self.ds.get("beam"),
+                "range_bin_burst": self.ds.get("range_bin_burst"),
+                "range_bin_average": self.ds.get("range_bin_average"),
+                "range_bin_echosounder": self.ds.get("num_beams_and_coordinate_system_and_num_cells"),
+                "altimeter_sample_bin": self.ds.get("altimeter_sample_bin"),
             }
         )
         self.write(ds, "Beam")
 
     def set_vendor_specific(self):
         attrs = {
-            "offset_of_data": self.parser_obj.ds.get("offset_of_data"),
-            "pressure_sensor_valid": self.parser_obj.ds.get("pressure_sensor_valid"),
-            "temperature_sensor_valid": self.parser_obj.ds.get("temperature_sensor_valid"),
-            "compass_sensor_valid": self.parser_obj.ds.get("compass_sensor_valid"),
-            "tilt_sensor_valid": self.parser_obj.ds.get("tilt_sensor_valid"),
-            "velocity_data_included": self.parser_obj.ds.get("velocity_data_included"),
-            "amplitude_data_included": self.parser_obj.ds.get("amplitude_data_included"),
-            "correlation_data_included": self.parser_obj.ds.get("correlation_data_included"),
-            "altimeter_data_included": self.parser_obj.ds.get("altimeter_data_included"),
-            "altimeter_raw_data_included": self.parser_obj.ds.get("altimeter_raw_data_included"),
-            "ast_data_included": self.parser_obj.ds.get("ast_data_included"),
-            "echo_sounder_data_included": self.parser_obj.ds.get("echo_sounder_data_included"),
-            "ahrs_data_included": self.parser_obj.ds.get("ahrs_data_included"),
-            "percentage_good_data_included": self.parser_obj.ds.get("percentage_good_data_included"),
-            "std_dev_data_included": self.parser_obj.ds.get("std_dev_data_included"),
-            "figure_of_merit_data_included": self.parser_obj.ds.get("figure_of_merit_data_included"),
+            "offset_of_data": self.ds.get("offset_of_data"),
+            "pressure_sensor_valid": self.ds.get("pressure_sensor_valid"),
+            "temperature_sensor_valid": self.ds.get("temperature_sensor_valid"),
+            "compass_sensor_valid": self.ds.get("compass_sensor_valid"),
+            "tilt_sensor_valid": self.ds.get("tilt_sensor_valid"),
+            "velocity_data_included": self.ds.get("velocity_data_included"),
+            "amplitude_data_included": self.ds.get("amplitude_data_included"),
+            "correlation_data_included": self.ds.get("correlation_data_included"),
+            "altimeter_data_included": self.ds.get("altimeter_data_included"),
+            "altimeter_raw_data_included": self.ds.get("altimeter_raw_data_included"),
+            "ast_data_included": self.ds.get("ast_data_included"),
+            "echo_sounder_data_included": self.ds.get("echo_sounder_data_included"),
+            "ahrs_data_included": self.ds.get("ahrs_data_included"),
+            "percentage_good_data_included": self.ds.get("percentage_good_data_included"),
+            "std_dev_data_included": self.ds.get("std_dev_data_included"),
+            "figure_of_merit_data_included": self.ds.get("figure_of_merit_data_included"),
         }
         attrs = {field_name: field_value.data[0] for field_name, field_value in attrs.items(
         ) if field_value is not None}
         ds = xr.Dataset(data_vars={
-            "data_record_version": self.parser_obj.ds.get("version"),
-            "error": self.parser_obj.ds.get("error"),
-            "status": self.parser_obj.ds.get("status"),
-            "status0": self.parser_obj.ds.get("status0"),
-            "battery_voltage": self.parser_obj.ds.get("battery_voltage"),
-            "power_level": self.parser_obj.ds.get("power_level"),
-            "temperature_of_pressure_sensor": self.parser_obj.ds.get("temperature_from_pressure_sensor"),
-            "nominal_correlation": self.parser_obj.ds.get("nominal_correlation"),
-            "magnetometer_temperature": self.parser_obj.ds.get("magnetometer_temperature"),
-            "real_time_clock_temperature": self.parser_obj.ds.get("real_time_clock_temperature"),
-            "ensemble_counter": self.parser_obj.ds.get("ensemble_counter"),
+            "data_record_version": self.ds.get("version"),
+            "error": self.ds.get("error"),
+            "status": self.ds.get("status"),
+            "status0": self.ds.get("status0"),
+            "battery_voltage": self.ds.get("battery_voltage"),
+            "power_level": self.ds.get("power_level"),
+            "temperature_of_pressure_sensor": self.ds.get("temperature_from_pressure_sensor"),
+            "nominal_correlation": self.ds.get("nominal_correlation"),
+            "magnetometer_temperature": self.ds.get("magnetometer_temperature"),
+            "real_time_clock_temperature": self.ds.get("real_time_clock_temperature"),
+            "ensemble_counter": self.ds.get("ensemble_counter"),
             "ahrs_rotation_matrix_mij": ("mij", [
-                self.parser_obj.ds.get("ahrs_rotation_matrix_m11"),
-                self.parser_obj.ds.get("ahrs_rotation_matrix_m12"),
-                self.parser_obj.ds.get("ahrs_rotation_matrix_m13"),
-                self.parser_obj.ds.get("ahrs_rotation_matrix_m21"),
-                self.parser_obj.ds.get("ahrs_rotation_matrix_m22"),
-                self.parser_obj.ds.get("ahrs_rotation_matrix_m23"),
-                self.parser_obj.ds.get("ahrs_rotation_matrix_m31"),
-                self.parser_obj.ds.get("ahrs_rotation_matrix_m32"),
-                self.parser_obj.ds.get("ahrs_rotation_matrix_m33")
+                self.ds.get("ahrs_rotation_matrix_m11"),
+                self.ds.get("ahrs_rotation_matrix_m12"),
+                self.ds.get("ahrs_rotation_matrix_m13"),
+                self.ds.get("ahrs_rotation_matrix_m21"),
+                self.ds.get("ahrs_rotation_matrix_m22"),
+                self.ds.get("ahrs_rotation_matrix_m23"),
+                self.ds.get("ahrs_rotation_matrix_m31"),
+                self.ds.get("ahrs_rotation_matrix_m32"),
+                self.ds.get("ahrs_rotation_matrix_m33")
             ]),
             "ahrs_quaternions_wxyz": ("wxyz", [
-                self.parser_obj.ds.get("ahrs_quaternions_w"),
-                self.parser_obj.ds.get("ahrs_quaternions_x"),
-                self.parser_obj.ds.get("ahrs_quaternions_y"),
-                self.parser_obj.ds.get("ahrs_quaternions_z"),
+                self.ds.get("ahrs_quaternions_w"),
+                self.ds.get("ahrs_quaternions_x"),
+                self.ds.get("ahrs_quaternions_y"),
+                self.ds.get("ahrs_quaternions_z"),
             ]),
             "ahrs_gyro_xyz": ("xyz", [
-                self.parser_obj.ds.get("ahrs_gyro_x"),
-                self.parser_obj.ds.get("ahrs_gyro_y"),
-                self.parser_obj.ds.get("ahrs_gyro_z"),
+                self.ds.get("ahrs_gyro_x"),
+                self.ds.get("ahrs_gyro_y"),
+                self.ds.get("ahrs_gyro_z"),
             ]),
-            "percentage_good_data": self.parser_obj.ds.get("percentage_good_data"),
-            "std_dev_pitch": self.parser_obj.ds.get("std_dev_pitch"),
-            "std_dev_roll": self.parser_obj.ds.get("std_dev_roll"),
-            "std_dev_heading": self.parser_obj.ds.get("std_dev_heading"),
-            "std_dev_pressure": self.parser_obj.ds.get("std_dev_pressure"),
+            "percentage_good_data": self.ds.get("percentage_good_data"),
+            "std_dev_pitch": self.ds.get("std_dev_pitch"),
+            "std_dev_roll": self.ds.get("std_dev_roll"),
+            "std_dev_heading": self.ds.get("std_dev_heading"),
+            "std_dev_pressure": self.ds.get("std_dev_pressure"),
         },
             coords={
-            "time_burst": self.parser_obj.ds.get("time_burst"),
-            "time_average": self.parser_obj.ds.get("time_average"),
-            "beam": self.parser_obj.ds.get("beam"),
-            "range_bin": self.parser_obj.ds.get("range_bin"),
+            "time_burst": self.ds.get("time_burst"),
+            "time_average": self.ds.get("time_average"),
+            "time_echosounder": self.ds.get("time_echosounder"),
+            "beam": self.ds.get("beam"),
+            "range_bin_average": self.ds.get("range_bin_average"),
+            "range_bin_burst": self.ds.get("range_bin_burst"),
+            "range_bin_echosounder": self.ds.get("range_bin_echosounder"),
         }, attrs=attrs)
         ds = ds.reindex({
             "mij": np.array(["11", "12", "13", "21", "22", "23", "31", "32", "33"]),
