Fix unit conversion bug

echopype/tests/test_process_ad2cp.py
@@ -53,7 +53,12 @@ OCEAN_CONTOUR_PARSED = [
     "./echopype/test_data/ad2cp/SG198_0099_b.ad2cp.nc"
 ]
 
+THRESHOLD = 0.02
+
 def test_ocean_contour():
+    def close(a, b):
+        return abs(np.float32(a) - np.float32(b)) < THRESHOLD
+
     for file, parsed_file in zip(OCEAN_CONTOUR_UNPARSED, OCEAN_CONTOUR_PARSED):
         test_convert = Convert(file, model="AD2CP")
         test_file = ".".join(file.split(".")[:-1]) + ".nc"
@@ -63,27 +68,27 @@ def test_ocean_contour():
         base = xr.open_dataset(parsed_file, group="Config")
         
         test = xr.open_dataset(test_file, group="Vendor")
-        assert base.attrs["Instrument_pressure"] == test.attrs["pressure_sensor_valid"]
-        assert base.attrs["Instrument_temperature"] == test.attrs["temperature_sensor_valid"]
-        assert base.attrs["Instrument_compass"] == test.attrs["compass_sensor_valid"]
-        assert base.attrs["Instrument_tilt"] == test.attrs["tilt_sensor_valid"]
-        assert base.attrs["Instrument_avg_nCells"] == test.dims["range_bin_average"]
-        assert base.attrs["Instrument_avg_nBeams"] == test.dims["beam"]
-        assert base.attrs["Instrument_echo_enable"] == test.attrs["echosounder_data_included"]
+        assert close(base.attrs["Instrument_pressure"], test.attrs["pressure_sensor_valid"])
+        assert close(base.attrs["Instrument_temperature"], test.attrs["temperature_sensor_valid"])
+        assert close(base.attrs["Instrument_compass"], test.attrs["compass_sensor_valid"])
+        assert close(base.attrs["Instrument_tilt"], test.attrs["tilt_sensor_valid"])
+        assert close(base.attrs["Instrument_avg_nCells"], test.dims["range_bin_average"])
+        assert close(base.attrs["Instrument_avg_nBeams"], test.dims["beam"])
+        assert close(base.attrs["Instrument_echo_enable"], test.attrs["echosounder_data_included"])
         test.close()
 
         test = xr.open_dataset(test_file, group="Beam")
-        assert base.attrs["Instrument_avg_cellSize"] == test["cell_size"].data[0] / 1000
-        assert base.attrs["Instrument_avg_blankingDistance"] == (test["blanking"].data[0] / 1000).astype(np.float32)
+        assert close(base.attrs["Instrument_avg_cellSize"], test["cell_size"].data[0])
+        assert close(base.attrs["Instrument_avg_blankingDistance"], (test["blanking"].data[0]).astype(np.float32))
         # FIXME: test["velocity_range"] is nan
-        # assert base.attrs["Instrument_avg_velocityRange"] == test["velocity_range"].data[0] / 1000
+        # assert close(base.attrs["Instrument_avg_velocityRange"], test["velocity_range"].data[0])
         # TODO: transmit power vs transmit energy?
-        # assert base.attrs["Instrument_avg_transmitPower"] == test["transmit_energy"].data[0]
+        # assert close(base.attrs["Instrument_avg_transmitPower"], test["transmit_energy"].data[0])
         test.close()
 
         # test = xr.open_dataset(r"C:\Users\strea\Desktop\UW\echopype\fork3\echopype\echopype\test_data\ad2cp\SG198_0097_a.nc", group="Environment")
-        # assert base.attrs["DataInfo_pressure_min"] == min(test["pressure"].data)
-        # assert base.attrs["DataInfo_pressure_max"] == max(test["pressure"].data)
+        # assert close(base.attrs["DataInfo_pressure_min"], min(test["pressure"].data))
+        # assert close(base.attrs["DataInfo_pressure_max"], max(test["pressure"].data))
         # test.close()
 
         base.close()
@@ -92,7 +97,7 @@ def test_ocean_contour():
 
         def test_field(base, test, base_field_name, test_field_name):
             for base_value, test_value in zip(base[base_field_name], test[test_field_name]):
-                assert base_value.data[()].astype(np.float32) == test_value.data[()].astype(np.float32)
+                assert close(base_value.data[()], test_value.data[()])
 
         base = xr.open_dataset(parsed_file, group="Data/Avg")
         
@@ -100,32 +105,32 @@ def test_ocean_contour():
         # does not work because ocean contour timestamps have rounding errors
         # so we can't compare times
         # for time in base["time"]:
-        #     assert base["Heading"].sel(time=time).data[()] == test["heading"].sel(time=time).data[()]
+        #     assert close(base["Heading"].sel(time=time).data[()], test["heading"].sel(time=time).data[()])
         for base_heading, test_heading in zip(base["Heading"], test["heading"]):
-            assert base_heading.data[()] == (test_heading.data[()] / 100).astype(np.float32)
+            assert close(base_heading.data[()], (test_heading.data[()]).astype(np.float32))
         for base_pitch, test_pitch in zip(base["Pitch"], test["pitch"]):
-            assert base_pitch.data[()] == (test_pitch.data[()] / 100).astype(np.float32)
+            assert close(base_pitch.data[()], (test_pitch.data[()]).astype(np.float32))
         for base_roll, test_roll in zip(base["Roll"], test["roll"]):
-            assert base_roll.data[()] == (test_roll.data[()] / 100).astype(np.float32)
+            assert close(base_roll.data[()], (test_roll.data[()]).astype(np.float32))
 
         test_field(base, test, "Magnetometer_X", "magnetometer_raw_x")
         # for base_magnetometer_x, test_magnetometer_x in zip(base["Magnetometer_X"], test["magnetometer_raw_x"]):
-        #     assert base_magnetometer_x.data[()] == test_magnetometer_x.data[()]
+        #     assert close(base_magnetometer_x.data[()], test_magnetometer_x.data[()])
         test_field(base, test, "Magnetometer_Y", "magnetometer_raw_y")
         # for base_magnetometer_y, test_magnetometer_y in zip(base["Magnetometer_Y"], test["magnetometer_raw_y"]):
-        #     assert base_magnetometer_y.data[()] == test_magnetometer_y.data[()]
+        #     assert close(base_magnetometer_y.data[()], test_magnetometer_y.data[()])
         test_field(base, test, "Magnetometer_Z", "magnetometer_raw_z")
         # for base_magnetometer_z, test_magnetometer_z in zip(base["Magnetometer_Z"], test["magnetometer_raw_z"]):
-        #     assert base_magnetometer_z.data[()] == test_magnetometer_z.data[()]
+        #     assert close(base_magnetometer_z.data[()], test_magnetometer_z.data[()])
         test.close()
 
         test = xr.open_dataset(test_file, group="Beam")
         test_field(base, test, "NumberofCells", "number_of_cells")
         # for base_number_of_cells, test_number_of_cells in zip(base["NumberofCells"], test["number_of_cells"]):
-        #     assert base_number_of_cells.data[()] == test_number_of_cells[()]
+        #     assert close(base_number_of_cells.data[()], test_number_of_cells[()])
         test_field(base, test, "CellSize", "cell_size")
         # for base_cell_size, test_cell_size in zip(base["CellSize"], test["cell_size"]):
-        #     assert base_cell_size.data[()] == test_cell_size.data[()]
+        #     assert close(base_cell_size.data[()], test_cell_size.data[()])
         test_field(base, test, "Blanking", "blanking")
         test_field(base, test, "TransmitEnergy", "transmit_energy")
         test_field(base, test, "Ambiguity", "ambiguity_velocity")
@@ -133,11 +138,11 @@ def test_ocean_contour():
         for beam in range(3):
             for range_bin in range(15):
                 for base_vel, test_vel in zip(base[f"Vel_Beam{beam + 2}"].isel(AvgVelocityBeam_Range=range_bin), test["velocity_average"].isel(beam=beam, range_bin_average=range_bin)):
-                    assert base_vel.data[()] == test_vel.data[()]
+                    assert close(base_vel.data[()], test_vel.data[()])
                 for base_cor, test_cor in zip(base[f"Cor_Beam{beam + 2}"].isel(AvgCorrelationBeam_Range=range_bin), test["correlation_average"].isel(beam=beam, range_bin_average=range_bin)):
-                    assert base_cor.data[()] == test_cor.data[()]
+                    assert close(base_cor.data[()], test_cor.data[()])
                 for base_amp, test_amp in zip(base[f"Amp_Beam{beam + 2}"].isel(AvgAmplitudeBeam_Range=range_bin), test["amplitude_average"].isel(beam=beam, range_bin_average=range_bin)):
-                    assert base_amp.data[()] == test_amp.data[()]
+                    assert close(base_amp.data[()], test_amp.data[()])
         test.close()
 
         test = xr.open_dataset(test_file, group="Vendor")