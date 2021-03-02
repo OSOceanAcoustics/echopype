a08c2eadd4496cc66e2bd3c6b9a3711a955f7eb1
leewujung <leewujung@gmail.com>
2021-03-02 14:24:07 -0500

    update EK80 get_env_params to use center freq if BB mode, add waveform_mode and encode_mode in compute_Sv call

--- echopype/calibrate/cal_func.py
+++ echopype/calibrate/cal_func.py
@@ -8,17 +8,23 @@ CALIBRATOR = {
 }
 
 
-def get_Sv(echodata, env_params=None, cal_params=None):
+def get_Sv(echodata, env_params=None, cal_params=None, waveform_mode=None, encode_mode=None):
     """Compute volume backscattering strength (Sv) from raw data.
     """
+    # Sanity check on inputs
+    if waveform_mode not in ('BB', 'CW'):
+        raise ValueError('Input waveform_mode not recognized!')
+    if encode_mode not in ('complex', 'power'):
+        raise ValueError('Input encode_mode not recognized!')
+
     # Set up calibration object
     cal_obj = CALIBRATOR[echodata.sonar_model](echodata)
     if env_params is None:
         env_params = {}
-    cal_obj.get_env_params(env_params)
+    cal_obj.get_env_params(env_params, waveform_mode=waveform_mode)
     if cal_params is None:
         cal_params = {}
     cal_obj.get_cal_params(cal_params)
 
     # Perform calibration
-    return cal_obj.compute_Sv()
+    return cal_obj.compute_Sv(waveform_mode=waveform_mode, encode_mode=encode_mode)
