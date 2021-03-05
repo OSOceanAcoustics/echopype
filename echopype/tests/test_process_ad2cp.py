c679114e1f9dcf42c9127606af758bcfbe2e4590
imranmaj <49664304+imranmaj@users.noreply.github.com>
2021-03-05 02:42:34 -0800

    Use np.isclose

--- echopype/tests/test_process_ad2cp.py
+++ echopype/tests/test_process_ad2cp.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import xarray as xr
 import numpy as np
@@ -39,14 +40,14 @@ def test_process():
 
         os.remove(tmp_file)
 
-OCEAN_CONTOUR_UNPARSED = [
+AD2CP_UNPARSED = [
     "./echopype/test_data/ad2cp/SG198_0097_a.ad2cp",
     "./echopype/test_data/ad2cp/SG198_0097_b.ad2cp",
     "./echopype/test_data/ad2cp/SG198_0099_a.ad2cp",
     "./echopype/test_data/ad2cp/SG198_0099_b.ad2cp"
 ]
 
-OCEAN_CONTOUR_PARSED = [
+AD2CP_PARSED_BY_OCEAN_CONTOUR = [
     "./echopype/test_data/ad2cp/SG198_0097_a.ad2cp.nc",
     "./echopype/test_data/ad2cp/SG198_0097_b.ad2cp.nc",
     "./echopype/test_data/ad2cp/SG198_0099_a.ad2cp.nc",
@@ -56,30 +57,27 @@ OCEAN_CONTOUR_PARSED = [
 THRESHOLD = 0.02
 
 def test_ocean_contour():
-    def close(a, b):
-        return abs(np.float32(a) - np.float32(b)) < THRESHOLD
-
-    for file, parsed_file in zip(OCEAN_CONTOUR_UNPARSED, OCEAN_CONTOUR_PARSED):
+    for file, parsed_file in zip(AD2CP_UNPARSED, AD2CP_PARSED_BY_OCEAN_CONTOUR):
         test_convert = Convert(file, model="AD2CP")
-        test_file = ".".join(file.split(".")[:-1]) + ".nc"
+        test_file = str(Path(file).with_suffix(".nc"))
         test_convert.to_netcdf(save_path=test_file, overwrite=True)
 
         # Config
         base = xr.open_dataset(parsed_file, group="Config")
         
         test = xr.open_dataset(test_file, group="Vendor")
-        assert close(base.attrs["Instrument_pressure"], test.attrs["pressure_sensor_valid"])
-        assert close(base.attrs["Instrument_temperature"], test.attrs["temperature_sensor_valid"])
-        assert close(base.attrs["Instrument_compass"], test.attrs["compass_sensor_valid"])
-        assert close(base.attrs["Instrument_tilt"], test.attrs["tilt_sensor_valid"])
-        assert close(base.attrs["Instrument_avg_nCells"], test.dims["range_bin_average"])
-        assert close(base.attrs["Instrument_avg_nBeams"], test.dims["beam"])
-        assert close(base.attrs["Instrument_echo_enable"], test.attrs["echosounder_data_included"])
+        assert np.isclose(base.attrs["Instrument_pressure"], test.attrs["pressure_sensor_valid"]).all()
+        assert np.isclose(base.attrs["Instrument_temperature"], test.attrs["temperature_sensor_valid"]).all()
+        assert np.isclose(base.attrs["Instrument_compass"], test.attrs["compass_sensor_valid"]).all()
+        assert np.isclose(base.attrs["Instrument_tilt"], test.attrs["tilt_sensor_valid"]).all()
+        assert base.attrs["Instrument_avg_nCells"] == test.dims["range_bin_average"]
+        assert base.attrs["Instrument_avg_nBeams"] == test.dims["beam"]
+        assert base.attrs["Instrument_echo_enable"] == test.attrs["echosounder_data_included"]
         test.close()
 
         test = xr.open_dataset(test_file, group="Beam")
-        assert close(base.attrs["Instrument_avg_cellSize"], test["cell_size"].data[0])
-        assert close(base.attrs["Instrument_avg_blankingDistance"], (test["blanking"].data[0]).astype(np.float32))
+        assert np.isclose(base.attrs["Instrument_avg_cellSize"], test["cell_size"].data[0]).all()
+        assert np.isclose(base.attrs["Instrument_avg_blankingDistance"], (test["blanking"].data[0]).astype(np.float32)).all()
         # FIXME: test["velocity_range"] is nan
         # assert close(base.attrs["Instrument_avg_velocityRange"], test["velocity_range"].data[0])
         # TODO: transmit power vs transmit energy?
@@ -96,19 +94,15 @@ def test_ocean_contour():
         # Data
 
         def test_field(base, test, base_field_name, test_field_name):
-            for base_value, test_value in zip(base[base_field_name], test[test_field_name]):
-                assert close(base_value.data[()], test_value.data[()])
+            assert np.isclose(base[base_field_name].data, test[test_field_name].data).all()
 
         base = xr.open_dataset(parsed_file, group="Data/Avg")
         
         test = xr.open_dataset(test_file, group="Platform")
         # ocean contour timestamps have rounding errors so we can't compare times
-        for base_heading, test_heading in zip(base["Heading"], test["heading"]):
-            assert close(base_heading.data[()], (test_heading.data[()]).astype(np.float32))
-        for base_pitch, test_pitch in zip(base["Pitch"], test["pitch"]):
-            assert close(base_pitch.data[()], (test_pitch.data[()]).astype(np.float32))
-        for base_roll, test_roll in zip(base["Roll"], test["roll"]):
-            assert close(base_roll.data[()], (test_roll.data[()]).astype(np.float32))
+        assert np.isclose(base["Heading"].data, test["heading"].data).all()
+        assert np.isclose(base["Pitch"].data, test["pitch"].data).all()
+        assert np.isclose(base["Roll"].data, test["roll"].data).all()
 
         test_field(base, test, "Magnetometer_X", "magnetometer_raw_x")
         test_field(base, test, "Magnetometer_Y", "magnetometer_raw_y")
@@ -124,12 +118,9 @@ def test_ocean_contour():
 
         for beam in test["beam"]:
             for range_bin in range(15):
-                for base_vel, test_vel in zip(base[f"Vel_Beam{beam.data[()]}"].isel(AvgVelocityBeam_Range=range_bin), test["velocity_average"].sel(beam=beam.data[()]).isel(range_bin_average=range_bin)):
-                    assert close(base_vel.data[()], test_vel.data[()])
-                for base_cor, test_cor in zip(base[f"Cor_Beam{beam.data[()]}"].isel(AvgCorrelationBeam_Range=range_bin), test["correlation_average"].sel(beam=beam.data[()]).isel(range_bin_average=range_bin)):
-                    assert close(base_cor.data[()], test_cor.data[()])
-                for base_amp, test_amp in zip(base[f"Amp_Beam{beam.data[()]}"].isel(AvgAmplitudeBeam_Range=range_bin), test["amplitude_average"].sel(beam=beam.data[()]).isel(range_bin_average=range_bin)):
-                    assert close(base_amp.data[()], test_amp.data[()])
+                assert np.isclose(base[f"Vel_Beam{beam.data[()]}"].isel(AvgVelocityBeam_Range=range_bin).data, test["velocity_average"].sel(beam=beam.data[()]).isel(range_bin_average=range_bin).data).all()
+                assert np.isclose(base[f"Cor_Beam{beam.data[()]}"].isel(AvgCorrelationBeam_Range=range_bin).data, test["correlation_average"].sel(beam=beam.data[()]).isel(range_bin_average=range_bin).data).all()
+                assert np.isclose(base[f"Amp_Beam{beam.data[()]}"].isel(AvgAmplitudeBeam_Range=range_bin).data, test["amplitude_average"].sel(beam=beam.data[()]).isel(range_bin_average=range_bin).data).all()
         test.close()
 
         test = xr.open_dataset(test_file, group="Vendor")
