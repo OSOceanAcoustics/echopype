e451a2d388180cd905c08dcf5100df5c494af474
imranmaj <49664304+imranmaj@users.noreply.github.com>
2021-03-22 21:17:15 -0700

    Only test closeness when necessary; atol = 10e-7

--- echopype/tests/test_process_ad2cp.py
+++ echopype/tests/test_process_ad2cp.py
@@ -54,7 +54,7 @@ AD2CP_PARSED_BY_OCEAN_CONTOUR = [
     "./echopype/test_data/ad2cp/SG198_0099_b.ad2cp.nc"
 ]
 
-THRESHOLD = 0.02
+ABSOLUTE_TOLERANCE = 10e-7
 
 def test_ocean_contour():
     for file, parsed_file in zip(AD2CP_UNPARSED, AD2CP_PARSED_BY_OCEAN_CONTOUR):
@@ -66,10 +66,10 @@ def test_ocean_contour():
         base = xr.open_dataset(parsed_file, group="Config")
         
         test = xr.open_dataset(test_file, group="Vendor")
-        assert np.isclose(base.attrs["Instrument_pressure"], test.attrs["pressure_sensor_valid"]).all()
-        assert np.isclose(base.attrs["Instrument_temperature"], test.attrs["temperature_sensor_valid"]).all()
-        assert np.isclose(base.attrs["Instrument_compass"], test.attrs["compass_sensor_valid"]).all()
-        assert np.isclose(base.attrs["Instrument_tilt"], test.attrs["tilt_sensor_valid"]).all()
+        assert base.attrs["Instrument_pressure"] == test.attrs["pressure_sensor_valid"]
+        assert base.attrs["Instrument_temperature"] == test.attrs["temperature_sensor_valid"]
+        assert base.attrs["Instrument_compass"] == test.attrs["compass_sensor_valid"]
+        assert base.attrs["Instrument_tilt"] == test.attrs["tilt_sensor_valid"]
         assert base.attrs["Instrument_avg_nCells"] == test.dims["range_bin_average"]
         assert base.attrs["Instrument_avg_nBeams"] == test.dims["beam"]
         assert base.attrs["Instrument_echo_enable"] == test.attrs["echosounder_data_included"]
@@ -93,42 +93,45 @@ def test_ocean_contour():
 
         # Data
 
-        def test_field(base, test, base_field_name, test_field_name):
-            assert np.isclose(base[base_field_name].data, test[test_field_name].data).all()
+        def test_field_close(base, test, base_field_name, test_field_name):
+            assert np.isclose(base[base_field_name].data, test[test_field_name].data, atol=ABSOLUTE_TOLERANCE).all()
+
+        def test_field_equal(base, test, base_field_name, test_field_name):
+            assert np.equal(base[base_field_name].data, test[test_field_name].data).all()
 
         base = xr.open_dataset(parsed_file, group="Data/Avg")
         
         test = xr.open_dataset(test_file, group="Platform")
         # ocean contour timestamps have rounding errors so we can't compare times
-        assert np.isclose(base["Heading"].data, test["heading"].data).all()
-        assert np.isclose(base["Pitch"].data, test["pitch"].data).all()
-        assert np.isclose(base["Roll"].data, test["roll"].data).all()
+        test_field_close(base, test, "Heading", "heading")
+        test_field_close(base, test, "Pitch", "pitch")
+        test_field_close(base, test, "Roll", "roll")
 
-        test_field(base, test, "Magnetometer_X", "magnetometer_raw_x")
-        test_field(base, test, "Magnetometer_Y", "magnetometer_raw_y")
-        test_field(base, test, "Magnetometer_Z", "magnetometer_raw_z")
+        test_field_equal(base, test, "Magnetometer_X", "magnetometer_raw_x")
+        test_field_equal(base, test, "Magnetometer_Y", "magnetometer_raw_y")
+        test_field_equal(base, test, "Magnetometer_Z", "magnetometer_raw_z")
         test.close()
 
         test = xr.open_dataset(test_file, group="Beam")
-        test_field(base, test, "NumberofCells", "number_of_cells")
-        test_field(base, test, "CellSize", "cell_size")
-        test_field(base, test, "Blanking", "blanking")
-        test_field(base, test, "TransmitEnergy", "transmit_energy")
-        test_field(base, test, "Ambiguity", "ambiguity_velocity")
+        test_field_equal(base, test, "NumberofCells", "number_of_cells")
+        test_field_equal(base, test, "CellSize", "cell_size")
+        test_field_close(base, test, "Blanking", "blanking")
+        test_field_equal(base, test, "TransmitEnergy", "transmit_energy")
+        test_field_close(base, test, "Ambiguity", "ambiguity_velocity")
 
         for beam in test["beam"]:
             for range_bin in range(15):
-                assert np.isclose(base[f"Vel_Beam{beam.data[()]}"].isel(AvgVelocityBeam_Range=range_bin).data, test["velocity_average"].sel(beam=beam.data[()]).isel(range_bin_average=range_bin).data).all()
-                assert np.isclose(base[f"Cor_Beam{beam.data[()]}"].isel(AvgCorrelationBeam_Range=range_bin).data, test["correlation_average"].sel(beam=beam.data[()]).isel(range_bin_average=range_bin).data).all()
-                assert np.isclose(base[f"Amp_Beam{beam.data[()]}"].isel(AvgAmplitudeBeam_Range=range_bin).data, test["amplitude_average"].sel(beam=beam.data[()]).isel(range_bin_average=range_bin).data).all()
+                assert np.isclose(base[f"Vel_Beam{beam.data[()]}"].isel(AvgVelocityBeam_Range=range_bin).data, test["velocity_average"].sel(beam=beam.data[()]).isel(range_bin_average=range_bin).data, atol=ABSOLUTE_TOLERANCE).all()
+                assert np.isclose(base[f"Cor_Beam{beam.data[()]}"].isel(AvgCorrelationBeam_Range=range_bin).data, test["correlation_average"].sel(beam=beam.data[()]).isel(range_bin_average=range_bin).data, atol=ABSOLUTE_TOLERANCE).all()
+                assert np.isclose(base[f"Amp_Beam{beam.data[()]}"].isel(AvgAmplitudeBeam_Range=range_bin).data, test["amplitude_average"].sel(beam=beam.data[()]).isel(range_bin_average=range_bin).data, atol=ABSOLUTE_TOLERANCE).all()
         test.close()
 
         test = xr.open_dataset(test_file, group="Vendor")
         # uncalibrated (no units so cannot compare)
-        # test_field(base, test, "MagnetometerTemperature", "magnetometer_temperature")
-        test_field(base, test, "EnsembleCount", "ensemble_counter")
-        test_field(base, test, "RTCTemperature", "real_time_clock_temperature")
-        test_field(base, test, "NominalCor", "nominal_correlation")
+        # test_field_close(base, test, "MagnetometerTemperature", "magnetometer_temperature")
+        test_field_equal(base, test, "EnsembleCount", "ensemble_counter")
+        test_field_equal(base, test, "RTCTemperature", "real_time_clock_temperature")
+        test_field_equal(base, test, "NominalCor", "nominal_correlation")
         test.close()
 
         base.close()
