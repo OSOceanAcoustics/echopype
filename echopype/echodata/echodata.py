eba57265e4cba9e61a6bb86db1a96f59d9d24c96
leewujung <leewujung@gmail.com>
2021-04-05 09:05:23 -0700

    add open_converted, add storage_options

--- echopype/echodata/echodata.py
+++ echopype/echodata/echodata.py
@@ -3,29 +3,37 @@ import xarray as xr
 from zarr.errors import GroupNotFoundError
 
 
+def open_converted(converted_raw_path, storage_options):
+    return EchoData(converted_raw_path=converted_raw_path, storage_options=storage_options)
+
+
 class EchoData:
     """Echo data model class for handling raw converted data,
     including multiple files associated with the same data set.
     """
 
-    def __init__(self, raw_path=None):
+    def __init__(self, converted_raw_path=None, storage_options=None):
+
+        # TODO: consider if should open datasets in init
+        #  or within each function call when echodata is used. Need to benchmark.
 
         self.top = None
         self.beam = None
+        self.beam_power = None  # only present for EK80 containing both complex and power data, may remain empty
         self.environment = None
         self.vendor = None
 
-        # TODO: need to consider if should open datasets in init here
-        #  or within each function call when echodata is used. Need to benchmark.
-        if raw_path:
-            # TODO: need to check if raw_path is valid on either local or remote filesystem
-            self._load_file(raw_path)
+        self.storage_options = storage_options if storage_options is not None else {}
+
+        # TODO: verify if converted_raw_path is valid on either local or remote filesystem
+        if converted_raw_path:
+            self._load_file(converted_raw_path)
             self.sonar_model = self.top.keywords
 
         # EK80 data may have a Beam_power group if both complex and power data exist.
         if self.sonar_model == 'EK80':
             try:
-                self.beam_power = self._load_groups(raw_path, group='Beam_power')
+                self.beam_power = self._load_groups(converted_raw_path, group='Beam_power')
             except (OSError, GroupNotFoundError):
                 pass
 
@@ -39,6 +47,7 @@ class EchoData:
 
     @staticmethod
     def _load_groups(filepath, group=None):
+        # TODO: handle multiple files through the same set of checks for combining files
         suffix = Path(filepath).suffix
         if suffix == '.nc':
             return xr.open_dataset(filepath, group=group)
