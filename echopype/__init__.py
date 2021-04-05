f395de214b0f43cdf795906019417af31a3798b5
leewujung <leewujung@gmail.com>
2021-04-05 13:43:45 -0700

    update test_calibrate with work with echopype.open_converted

--- echopype/__init__.py
+++ echopype/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function
 from .convert import Convert
 from .process import Process
-from .echodata import EchoData
+from .echodata import open_converted
 from .calibrate import calibrate_func as calibrate
 
 from ._version import get_versions
