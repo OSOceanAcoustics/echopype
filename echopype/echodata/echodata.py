3ff9f09b68f6bae9cc95b7629f12cb078250f275
leewujung <leewujung@gmail.com>
2021-04-05 08:56:07 -0700

    tidy up EchoData to be just an interface

--- echopype/echodata/echodata.py
+++ echopype/echodata/echodata.py
@@ -8,45 +8,37 @@ class EchoData:
     including multiple files associated with the same data set.
     """
 
-    def __init__(self, raw_path=None, Sv_path=None):
+    def __init__(self, raw_path=None):
 
-        self.raw_top = None
-        self.raw_beam = None
-        self.raw_env = None
-        self.raw_vend = None
+        self.top = None
+        self.beam = None
+        self.environment = None
+        self.vendor = None
 
         # TODO: need to consider if should open datasets in init here
         #  or within each function call when echodata is used. Need to benchmark.
         if raw_path:
             # TODO: need to check if raw_path is valid on either local or remote filesystem
-            self._open_raw(raw_path)
-            self.sonar_model = self.raw_top.keywords
+            self._load_file(raw_path)
+            self.sonar_model = self.top.keywords
 
         # EK80 data may have a Beam_power group if both complex and power data exist.
         if self.sonar_model == 'EK80':
             try:
-                self.raw_beam_power = self._open_data_file(raw_path, group='Beam_power')
+                self.beam_power = self._load_groups(raw_path, group='Beam_power')
             except (OSError, GroupNotFoundError):
                 pass
 
-        # TODO: change this to something following best practices.
-        #  Need something for user to set Sv from outside by either
-        #  passing in Sv directly or supplying a file path
-        self.paths = {
-            'raw': raw_path,
-            'Sv': Sv_path
-        }
-
-    def _open_raw(self, raw_path):
+    def _load_file(self, raw_path):
         """Lazy load Top-level, Beam, Environment, and Vendor groups from raw file.
         """
-        self.raw_top = self._open_data_file(raw_path)
-        self.raw_beam = self._open_data_file(raw_path, group='Beam')
-        self.raw_env = self._open_data_file(raw_path, group='Environment')
-        self.raw_vend = self._open_data_file(raw_path, group='Vendor')
+        self.top = self._load_groups(raw_path)
+        self.beam = self._load_groups(raw_path, group='Beam')
+        self.environment = self._load_groups(raw_path, group='Environment')
+        self.vendor = self._load_groups(raw_path, group='Vendor')
 
     @staticmethod
-    def _open_data_file(filepath, group=None):
+    def _load_groups(filepath, group=None):
         suffix = Path(filepath).suffix
         if suffix == '.nc':
             return xr.open_dataset(filepath, group=group)
