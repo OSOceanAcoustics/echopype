8618079a6da33269594e20fb3638b48e4ec4a5ff
Landung "Don" Setiawan <landungs@uw.edu>
2021-04-09 17:13:38 -0700

    Use setuptools scm (#277)
    * Use setuptools scm for auto-versioning
    
    * Don't include docs folder into package
    
    * Use no-local-version for test pypi
    
    * Change how version is imported

--- echopype/__init__.py
+++ echopype/__init__.py
@@ -4,6 +4,4 @@ from .process import Process
 from .process import EchoDataNew
 from .calibrate import cal_func as calibrate
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+from _echopype_version import version as __version__
