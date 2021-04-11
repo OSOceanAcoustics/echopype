72a5cdb645326e2807bc5a0989db9e9c3798fe06
leewujung <leewujung@gmail.com>
2021-04-10 17:11:42 -0700

    add test_convert_ek60_echoview_raw

--- echopype/tests/test_convert_ek60.py
+++ echopype/tests/test_convert_ek60.py
@@ -7,11 +7,6 @@ from ..convert import open_raw
 
 ek60_path = Path('./echopype/test_data/ek60/')
 
-csv_paths = ['./echopype/test_data/ek60/from_echoview/DY1801_EK60-D20180211-T164025-Power18.csv',
-             './echopype/test_data/ek60/from_echoview/DY1801_EK60-D20180211-T164025-Power38.csv',
-             './echopype/test_data/ek60/from_echoview/DY1801_EK60-D20180211-T164025-Power70.csv',
-             './echopype/test_data/ek60/from_echoview/DY1801_EK60-D20180211-T164025-Power120.csv',
-             './echopype/test_data/ek60/from_echoview/DY1801_EK60-D20180211-T164025-Power200.csv']
 # raw_paths = ['./echopype/test_data/ek60/set1/' + file
             #  for file in os.listdir('./echopype/test_data/ek60/set1')]    # 2 range lengths
 # raw_path = ['./echopype/test_data/ek60/set2/' + file
@@ -45,27 +40,37 @@ def test_convert_ek60_matlab_raw():
         )
         # angle: alongship and athwartship
         for angle in ['alongship', 'athwartship']:
-            assert np.alltrue(
-                [ds_matlab['rawData'][0]['pings'][0][angle][0][fidx] for fidx in range(5)]
-                == ds_beam['angle_' + angle].transpose('frequency', 'range_bin', 'ping_time')
+            assert np.array_equal(
+                [ds_matlab['rawData'][0]['pings'][0][angle][0][fidx] for fidx in range(5)],
+                ds_beam['angle_' + angle].transpose('frequency', 'range_bin', 'ping_time')
             )
 
     Path(echodata.output_file).unlink()
 
 
-def test_convert_power_echoview():
-    """Test converting power and compare it to echoview output"""
-    tmp = Convert(file=raw_path, model='EK60')
-    tmp.to_netcdf()
+def test_convert_ek60_echoview_raw():
+    """Compare parsed power data (count) with csv exported by EchoView.
+    """
+    ek60_raw_path = str(ek60_path.joinpath('DY1801_EK60-D20180211-T164025.raw'))
+    ek60_csv_path = [
+        ek60_path.joinpath('from_echoview/DY1801_EK60-D20180211-T164025-Power%d.csv' % freq)
+        for freq in [18, 38, 70, 120, 200]
+    ]
 
+    # Read csv files exported by EchoView
     channels = []
-    for file in csv_paths:
+    for file in ek60_csv_path:
         channels.append(pd.read_csv(file, header=None, skiprows=[0]).iloc[:, 13:])
     test_power = np.stack(channels)
-    with xr.open_dataset(tmp.output_file, group='Beam') as ds_beam:
-        assert np.allclose(
-            test_power,
-            ds_beam.backscatter_r.isel(ping_time=slice(None, 10), range_bin=slice(1, None)),
-            atol=1e-10)
 
-    os.remove(tmp.output_file)
+    # Convert to netCDF and check
+    echodata = open_raw(file=ek60_raw_path, model='EK60')
+    echodata.to_netcdf()
+    with xr.open_dataset(echodata.output_file, group='Beam') as ds_beam:
+        for fidx, atol in zip(range(5), [1e-5, 1.1e-5, 1.1e-5, 1e-5, 1e-5]):
+            assert np.allclose(
+                test_power[fidx, :, :],
+                ds_beam.backscatter_r.isel(frequency=fidx, ping_time=slice(None, 10), range_bin=slice(1, None)),
+                atol=9e-6, rtol=atol
+            )
+    Path(echodata.output_file).unlink()
