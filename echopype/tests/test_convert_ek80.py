3033a79667d783442f56b83d00462babc386dbaa
leewujung <leewujung@gmail.com>
2021-04-11 17:21:21 -0700

    add test_convert_ek80_freq_subset

--- echopype/tests/test_convert_ek80.py
+++ echopype/tests/test_convert_ek80.py
@@ -13,7 +13,7 @@ ek80_path = Path('./echopype/test_data/ek80/')
 #                     './echopype/test_data/ek80/simrad/EK80_SimradEcho_WC381_Sequential-D20150513-T091105.raw']
 # raw_paths = ['./echopype/test_data/ek80/Summer2018--D20180905-T033113.raw',
 #              './echopype/test_data/ek80/Summer2018--D20180905-T033258.raw']  # Multiple files (CW and BB)
-raw_path_2_f = './echopype/test_data/ek80/2019118 group2survey-D20191214-T081342.raw'
+
 
 
 def test_convert_ek80_cw_power_echoview():
@@ -25,10 +25,10 @@ def test_convert_ek80_cw_power_echoview():
         ek80_path.joinpath('from_echoview/D20190822-T161221/%dkHz.power.csv' % freq)
         for freq in freq_list
     ]
-    ek80_echoview_angle_csv = [
-        ek80_path.joinpath('from_echoview/D20190822-T161221/%dkHz.angles.points.csv' % freq)
-        for freq in freq_list
-    ]
+    # ek80_echoview_angle_csv = [
+    #     ek80_path.joinpath('from_echoview/D20190822-T161221/%dkHz.angles.points.csv' % freq)
+    #     for freq in freq_list
+    # ]
 
     # Convert file
     echodata = open_raw(ek80_raw_path_cw, model='EK80')
@@ -99,7 +99,7 @@ def test_convert_ek80_complex_echoview():
     Path(echodata.output_file).unlink()
 
 
-def test_convert_ek80_cw_bb_single_file():
+def test_convert_ek80_cw_bb_in_single_file():
     """Make sure can convert a single EK80 file containing both CW and BB mode data.
     """
     ek80_raw_path_bb_cw = str(ek80_path.joinpath('Summer2018--D20180905-T033113.raw'))
@@ -115,42 +115,45 @@ def test_convert_ek80_cw_bb_single_file():
     shutil.rmtree(echodata.output_file)
 
 
-def test_freq_subset():
-    # Test converting file with multiple frequencies that do not record power data
-    tmp = Convert(file=raw_path_2_f, model='EK80')
-    tmp.to_netcdf(compress=False)
-    # Check if parsed output has the correct shape
-    with xr.open_dataset(tmp.output_file, group='Beam') as ds_beam:
-        assert ds_beam.backscatter_r.shape == (2, 2, 191327, 4)
-    os.remove(tmp.output_file)
+def test_convert_ek80_freq_subset():
+    """Make sure can convert EK80 file with multiple frequency channels off.
+    """
+    ek80_raw_path_freq_subset = str(ek80_path.joinpath('2019118 group2survey-D20191214-T081342.raw'))
+    echodata = open_raw(file=ek80_raw_path_freq_subset, model='EK80')
+    echodata.to_zarr()
+
+    # Check if converted output has only 2 frequency channels
+    with xr.open_dataset(echodata.output_file, group='Beam') as ds_beam:
+        assert ds_beam.frequency.size == 2
+    shutil.rmtree(echodata.output_file)
 
 
-def test_xml():
-    # Tests the exporting of the configuration xml as well as the environment xml
-    tmp = Convert(file=raw_path_bb_cw, model='EK80')
-    tmp.to_xml(data_type='CONFIG')
-    assert os.path.exists(tmp.output_file)
-    os.remove(tmp.output_file)
-
-    tmp.to_xml(save_path='env.xml', data_type='ENV')
-    assert os.path.exists(tmp.output_file)
-    os.remove(tmp.output_file)
-
-
-def test_add_platform():
-    # Construct lat/lon dataset with fake data using a date range that includes
-    # the ping_time ranges of the raw EK80 file. 7 pings over 28.166 seconds.
-    # (2019-08-22T16:12:21.398000128 to 2019-08-22T16:12:49.564000256)
-    location_time = pd.date_range(start='2019-08-22T16:00:00.0',
-                                  end='2019-08-22T16:15:00.0', periods=100)
-    lat = np.random.rand(100)
-    lon = np.random.rand(100)
-    testing_ds = xr.Dataset({'lat': (['location_time'], lat),
-                             'lon': (['location_time'], lon)},
-                            coords={'location_time': (['location_time'], location_time)})
-    tmp = Convert(file=raw_path_cw, model='EK80')
-    tmp.to_netcdf(overwrite=True, extra_platform_data=testing_ds)
-    with xr.open_dataset(tmp.output_file, group='Platform') as ds_plat:
-        # Test if the slicing the location_time with the ping_time worked
-        assert len(ds_plat.location_time) == 3
-    os.remove(tmp.output_file)
+# def test_xml():
+#     # Tests the exporting of the configuration xml as well as the environment xml
+#     tmp = Convert(file=raw_path_bb_cw, model='EK80')
+#     tmp.to_xml(data_type='CONFIG')
+#     assert os.path.exists(tmp.output_file)
+#     os.remove(tmp.output_file)
+#
+#     tmp.to_xml(save_path='env.xml', data_type='ENV')
+#     assert os.path.exists(tmp.output_file)
+#     os.remove(tmp.output_file)
+#
+#
+# def test_add_platform():
+#     # Construct lat/lon dataset with fake data using a date range that includes
+#     # the ping_time ranges of the raw EK80 file. 7 pings over 28.166 seconds.
+#     # (2019-08-22T16:12:21.398000128 to 2019-08-22T16:12:49.564000256)
+#     location_time = pd.date_range(start='2019-08-22T16:00:00.0',
+#                                   end='2019-08-22T16:15:00.0', periods=100)
+#     lat = np.random.rand(100)
+#     lon = np.random.rand(100)
+#     testing_ds = xr.Dataset({'lat': (['location_time'], lat),
+#                              'lon': (['location_time'], lon)},
+#                             coords={'location_time': (['location_time'], location_time)})
+#     tmp = Convert(file=raw_path_cw, model='EK80')
+#     tmp.to_netcdf(overwrite=True, extra_platform_data=testing_ds)
+#     with xr.open_dataset(tmp.output_file, group='Platform') as ds_plat:
+#         # Test if the slicing the location_time with the ping_time worked
+#         assert len(ds_plat.location_time) == 3
+#     os.remove(tmp.output_file)
