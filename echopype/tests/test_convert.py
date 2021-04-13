2fd9cdbc82551bf914a5f9f623caa09c39890b1c
Landung "Don" Setiawan <landungs@uw.edu>
2021-04-13 11:44:07 -0700

    Improving CI by only running tests on changed/added test files (#280)
    * Turning off test for now and try changed files action
    
    * Use mamba and black test_convert
    
    * Adding test back, utilize python script to run pytest
    
    * Detect changes to py files and run appropriate test
    
    * Capture failures and save ci test log
    
    * Add --pytest-args argument and remove -x
    
    * Disable fail-fast to test on different python version effectively
    
    * Deprecate travis ci in future tests/releases [skip ci]

--- echopype/tests/test_convert.py
+++ echopype/tests/test_convert.py
@@ -26,17 +26,17 @@ def _check_file_group(data_file, engine, groups):
 
 def _check_output_files(engine, output_files, storage_options):
     groups = [
-        'Provenance',
-        'Environment',
-        'Beam',
-        'Sonar',
-        'Vendor',
-        'Platform',
+        "Provenance",
+        "Environment",
+        "Beam",
+        "Sonar",
+        "Vendor",
+        "Platform",
     ]
     if isinstance(output_files, list):
         fs = fsspec.get_mapper(output_files[0], **storage_options).fs
         for f in output_files:
-            if engine == 'zarr':
+            if engine == "zarr":
                 _check_file_group(fs.get_mapper(f), engine, groups)
                 fs.delete(f, recursive=True)
             else:
@@ -44,7 +44,7 @@ def _check_output_files(engine, output_files, storage_options):
                 fs.delete(f)
     else:
         fs = fsspec.get_mapper(output_files, **storage_options).fs
-        if engine == 'zarr':
+        if engine == "zarr":
             _check_file_group(fs.get_mapper(output_files), engine, groups)
             fs.delete(output_files, recursive=True)
         else:
@@ -53,7 +53,7 @@ def _check_output_files(engine, output_files, storage_options):
 
 
 def _download_file(source_url, target_url):
-    fs = fsspec.filesystem('file')
+    fs = fsspec.filesystem("file")
     if not fs.exists(os.path.dirname(target_url)):
         fs.mkdir(os.path.dirname(target_url))
 
@@ -66,16 +66,16 @@ def _download_file(source_url, target_url):
 @pytest.fixture(scope="session")
 def minio_bucket():
     common_storage_options = dict(
-        client_kwargs=dict(endpoint_url='http://localhost:9000/'),
-        key='minioadmin',
-        secret='minioadmin',
+        client_kwargs=dict(endpoint_url="http://localhost:9000/"),
+        key="minioadmin",
+        secret="minioadmin",
     )
-    bucket_name = 'ooi-raw-data'
+    bucket_name = "ooi-raw-data"
     fs = fsspec.filesystem(
-        's3',
+        "s3",
         **common_storage_options,
     )
-    test_data = 'data'
+    test_data = "data"
     if not fs.exists(test_data):
         fs.mkdir(test_data)
 
@@ -83,7 +83,7 @@ def minio_bucket():
         fs.mkdir(bucket_name)
 
     # Load test data into bucket
-    test_data_path = Path(__file__).parent.parent.joinpath(Path('test_data'))
+    test_data_path = Path(__file__).parent.parent.joinpath(Path("test_data"))
     for d in test_data_path.iterdir():
         final_path = str(Path(test_data).joinpath(d.name))
         source_path = str(d)
@@ -91,27 +91,24 @@ def minio_bucket():
 
     return common_storage_options
 
+
 @pytest.fixture(scope="session")
 def download_files():
-    ek60_source = 'https://ncei-wcsd-archive.s3-us-west-2.amazonaws.com/data/raw/Bell_M._Shimada/SH1707/EK60/Summer2017-D20170615-T190214.raw'
-    ek80_source = 'https://ncei-wcsd-archive.s3-us-west-2.amazonaws.com/data/raw/Bell_M._Shimada/SH1707/EK80/D20170826-T205615.raw'
-    azfp_source = 'https://rawdata.oceanobservatories.org/files/CE01ISSM/R00007/instrmts/dcl37/ZPLSC_sn55075/ce01issm_zplsc_55075_recovered_2017-10-27/DATA/201703/17032923.01A'
-    azfp_xml_source = 'https://rawdata.oceanobservatories.org/files/CE01ISSM/R00007/instrmts/dcl37/ZPLSC_sn55075/ce01issm_zplsc_55075_recovered_2017-10-27/DATA/201703/17032922.XML'
+    ek60_source = "https://ncei-wcsd-archive.s3-us-west-2.amazonaws.com/data/raw/Bell_M._Shimada/SH1707/EK60/Summer2017-D20170615-T190214.raw"
+    ek80_source = "https://ncei-wcsd-archive.s3-us-west-2.amazonaws.com/data/raw/Bell_M._Shimada/SH1707/EK80/D20170826-T205615.raw"
+    azfp_source = "https://rawdata.oceanobservatories.org/files/CE01ISSM/R00007/instrmts/dcl37/ZPLSC_sn55075/ce01issm_zplsc_55075_recovered_2017-10-27/DATA/201703/17032923.01A"
+    azfp_xml_source = "https://rawdata.oceanobservatories.org/files/CE01ISSM/R00007/instrmts/dcl37/ZPLSC_sn55075/ce01issm_zplsc_55075_recovered_2017-10-27/DATA/201703/17032922.XML"
 
     ek60_path = os.path.join(
-        './echopype/test_data/ek60/ncei-wcsd',
+        "./echopype/test_data/ek60/ncei-wcsd",
         os.path.basename(ek60_source),
     )
     ek80_path = os.path.join(
-        './echopype/test_data/ek80/ncei-wcsd',
+        "./echopype/test_data/ek80/ncei-wcsd",
         os.path.basename(ek80_source),
     )
-    azfp_path = os.path.join(
-        './echopype/test_data/azfp/ooi', os.path.basename(azfp_source)
-    )
-    azfp_xml_path = os.path.join(
-        './echopype/test_data/azfp/ooi', os.path.basename(azfp_xml_source)
-    )
+    azfp_path = os.path.join("./echopype/test_data/azfp/ooi", os.path.basename(azfp_source))
+    azfp_xml_path = os.path.join("./echopype/test_data/azfp/ooi", os.path.basename(azfp_xml_source))
     download_paths = [
         (ek60_source, ek60_path),
         (ek80_source, ek80_path),
@@ -149,62 +146,56 @@ def test_validate_path_single_source(
 ):
 
     output_storage_options = {}
-    if output_save_path and output_save_path.startswith('s3://'):
+    if output_save_path and output_save_path.startswith("s3://"):
         output_storage_options = dict(
-            client_kwargs=dict(endpoint_url='http://localhost:9000/'),
-            key='minioadmin',
-            secret='minioadmin',
+            client_kwargs=dict(endpoint_url="http://localhost:9000/"),
+            key="minioadmin",
+            secret="minioadmin",
         )
     fsmap = fsspec.get_mapper(input_path)
     single_fname = os.path.splitext(os.path.basename(fsmap.root))[0]
     echodata_single = open_raw(input_path, model=model)
     echodata_single._output_storage_options = output_storage_options
 
-    echodata_single._validate_path(
-        file_format=file_format, save_path=output_save_path
-    )
+    echodata_single._validate_path(file_format=file_format, save_path=output_save_path)
 
     if output_save_path is not None:
-        fsmap_tmp = fsspec.get_mapper(
-            output_save_path, **output_storage_options
-        )
+        fsmap_tmp = fsspec.get_mapper(output_save_path, **output_storage_options)
         fs = fsmap_tmp.fs
-        if not output_save_path.startswith('s3'):
-            if output_save_path.endswith('/'):
+        if not output_save_path.startswith("s3"):
+            if output_save_path.endswith("/"):
                 # if an output folder is given, below works with and without the slash at the end
                 assert echodata_single.output_file == [
-                    os.path.join(fsmap_tmp.root, single_fname + '.zarr')
+                    os.path.join(fsmap_tmp.root, single_fname + ".zarr")
                 ]
-            elif output_save_path.endswith('.zarr'):
+            elif output_save_path.endswith(".zarr"):
                 # if an output filename is given
                 assert echodata_single.output_file == [fsmap_tmp.root]
             else:
                 # force output file extension to the called type (here .zarr)
                 assert echodata_single.output_file == [
-                    os.path.splitext(fsmap_tmp.root)[0] + '.zarr'
+                    os.path.splitext(fsmap_tmp.root)[0] + ".zarr"
                 ]
             os.rmdir(os.path.dirname(echodata_single.output_file[0]))
         else:
-            if output_save_path.endswith('/'):
+            if output_save_path.endswith("/"):
                 # if an output folder is given, below works with and without the slash at the end
                 assert echodata_single.output_file == [
-                    os.path.join(output_save_path, single_fname + '.zarr')
+                    os.path.join(output_save_path, single_fname + ".zarr")
                 ]
-            elif output_save_path.endswith('.zarr'):
+            elif output_save_path.endswith(".zarr"):
                 # if an output filename is given
                 assert echodata_single.output_file == [output_save_path]
             else:
                 # force output file extension to the called type (here .zarr)
                 assert echodata_single.output_file == [
-                    os.path.splitext(output_save_path)[0] + '.zarr'
+                    os.path.splitext(output_save_path)[0] + ".zarr"
                 ]
             fs.delete(echodata_single.output_file[0])
     else:
         current_dir = Path.cwd()
-        temp_dir = current_dir.joinpath(Path('temp_echopype_output'))
-        assert echodata_single.output_file == [
-            str(temp_dir.joinpath(Path(single_fname + '.zarr')))
-        ]
+        temp_dir = current_dir.joinpath(Path("temp_echopype_output"))
+        assert echodata_single.output_file == [str(temp_dir.joinpath(Path(single_fname + ".zarr")))]
         os.rmdir(os.path.dirname(echodata_single.output_file[0]))
 
 
@@ -215,7 +206,7 @@ def test_validate_path_single_source(
     [
         "./echopype/test_data/ek60/*.raw",
         [
-            'http://localhost:8080/data/ek60/ncei-wcsd/Summer2017-D20170615-T190214.raw',
+            "http://localhost:8080/data/ek60/ncei-wcsd/Summer2017-D20170615-T190214.raw",
         ],
     ],
 )
@@ -235,80 +226,66 @@ def test_validate_path_multiple_source(
     model, file_format, input_path, output_save_path, minio_bucket
 ):
     output_storage_options = {}
-    if output_save_path and output_save_path.startswith('s3://'):
+    if output_save_path and output_save_path.startswith("s3://"):
         output_storage_options = dict(
-            client_kwargs=dict(endpoint_url='http://localhost:9000/'),
-            key='minioadmin',
-            secret='minioadmin',
+            client_kwargs=dict(endpoint_url="http://localhost:9000/"),
+            key="minioadmin",
+            secret="minioadmin",
         )
 
     if isinstance(input_path, str):
         mult_path = glob.glob(input_path)
     else:
         mult_path = input_path
-    echodata_mult = open_raw(mult_path, model='EK60')
+    echodata_mult = open_raw(mult_path, model="EK60")
     echodata_mult._output_storage_options = output_storage_options
 
-    echodata_mult._validate_path(
-        file_format=file_format, save_path=output_save_path
-    )
+    echodata_mult._validate_path(file_format=file_format, save_path=output_save_path)
 
     if output_save_path is not None:
-        fsmap_tmp = fsspec.get_mapper(
-            output_save_path, **output_storage_options
-        )
+        fsmap_tmp = fsspec.get_mapper(output_save_path, **output_storage_options)
         fs = fsmap_tmp.fs
-        if not output_save_path.startswith('s3'):
-            if output_save_path.endswith('/'):
+        if not output_save_path.startswith("s3"):
+            if output_save_path.endswith("/"):
                 # if an output folder is given, below works with and without the slash at the end
                 assert echodata_mult.output_file == [
                     os.path.join(
                         fsmap_tmp.root,
-                        os.path.splitext(os.path.basename(f))[0] + '.zarr',
+                        os.path.splitext(os.path.basename(f))[0] + ".zarr",
                     )
                     for f in mult_path
                 ]
-            elif output_save_path.endswith('.zarr'):
+            elif output_save_path.endswith(".zarr"):
                 # if an output filename is given: only use the directory
-                assert echodata_mult.output_file == [
-                    os.path.abspath(output_save_path)
-                ]
-            elif output_save_path.endswith('.nc'):
+                assert echodata_mult.output_file == [os.path.abspath(output_save_path)]
+            elif output_save_path.endswith(".nc"):
                 # force output file extension to the called type (here .zarr)
                 assert echodata_mult.output_file == [
-                    os.path.abspath(output_save_path.replace('.nc', '.zarr'))
+                    os.path.abspath(output_save_path.replace(".nc", ".zarr"))
                 ]
             os.rmdir(os.path.dirname(echodata_mult.output_file[0]))
         else:
-            if output_save_path.endswith('/'):
+            if output_save_path.endswith("/"):
                 # if an output folder is given, below works with and without the slash at the end
                 assert echodata_mult.output_file == [
                     os.path.join(
                         output_save_path,
-                        os.path.splitext(os.path.basename(f))[0] + '.zarr',
+                        os.path.splitext(os.path.basename(f))[0] + ".zarr",
                     )
                     for f in mult_path
                 ]
-            elif output_save_path.endswith('.zarr'):
+            elif output_save_path.endswith(".zarr"):
                 # if an output filename is given: only use the directory
                 assert echodata_mult.output_file == [output_save_path]
-            elif output_save_path.endswith('.nc'):
+            elif output_save_path.endswith(".nc"):
                 # force output file extension to the called type (here .zarr)
-                assert echodata_mult.output_file == [
-                    output_save_path.replace('.nc', '.zarr')
-                ]
+                assert echodata_mult.output_file == [output_save_path.replace(".nc", ".zarr")]
             fs.delete(echodata_mult.output_file[0])
     else:
         current_dir = Path.cwd()
-        temp_dir = current_dir.joinpath(Path('temp_echopype_output'))
+        temp_dir = current_dir.joinpath(Path("temp_echopype_output"))
         assert echodata_mult.output_file == [
-            str(
-                temp_dir.joinpath(
-                    Path(
-                        os.path.splitext(os.path.basename(f))[0] + '.zarr'
-                    )
-                )
-            )
+            str(temp_dir.joinpath(Path(os.path.splitext(os.path.basename(f))[0] + ".zarr")))
             for f in mult_path
         ]
         os.rmdir(os.path.dirname(echodata_mult.output_file[0]))
@@ -321,8 +298,8 @@ def test_validate_path_multiple_source(
         "./echopype/test_data/ek60/ncei-wcsd/Summer2017-D20170615-T190214.raw",
         "s3://data/ek60/ncei-wcsd/Summer2017-D20170615-T190214.raw",
         [
-            'http://localhost:8080/data/ek60/ncei-wcsd/Summer2017-D20170615-T190214.raw',
-            'http://localhost:8080/data/ek60/ncei-wcsd/Summer2017-D20170615-T190843.raw',
+            "http://localhost:8080/data/ek60/ncei-wcsd/Summer2017-D20170615-T190214.raw",
+            "http://localhost:8080/data/ek60/ncei-wcsd/Summer2017-D20170615-T190843.raw",
         ],
     ],
 )
@@ -354,18 +331,16 @@ def test_convert_ek60(
     if isinstance(input_path, list):
         ipath = input_path[0]
 
-    input_storage_options = common_storage_options if ipath.startswith('s3://') else {}
-    if output_save_path and output_save_path.startswith('s3://'):
+    input_storage_options = common_storage_options if ipath.startswith("s3://") else {}
+    if output_save_path and output_save_path.startswith("s3://"):
         output_storage_options = common_storage_options
 
-    ec = open_raw(
-        file=input_path, model=model, storage_options=input_storage_options
-    )
+    ec = open_raw(file=input_path, model=model, storage_options=input_storage_options)
 
     if (
-        export_engine == 'netcdf4'
+        export_engine == "netcdf4"
         and output_save_path is not None
-        and output_save_path.startswith('s3://')
+        and output_save_path.startswith("s3://")
     ):
         return
     ec._to_file(
@@ -423,8 +398,8 @@ def test_convert_azfp(
     if isinstance(input_path, list):
         ipath = input_path[0]
 
-    input_storage_options = common_storage_options if ipath.startswith('s3://') else {}
-    if output_save_path and output_save_path.startswith('s3://'):
+    input_storage_options = common_storage_options if ipath.startswith("s3://") else {}
+    if output_save_path and output_save_path.startswith("s3://"):
         output_storage_options = common_storage_options
 
     ec = open_raw(
@@ -437,9 +412,9 @@ def test_convert_azfp(
     assert ec.xml_path == xml_path
 
     if (
-        export_engine == 'netcdf4'
+        export_engine == "netcdf4"
         and output_save_path is not None
-        and output_save_path.startswith('s3://')
+        and output_save_path.startswith("s3://")
     ):
         return
     ec._to_file(
@@ -490,18 +465,16 @@ def test_convert_ek80(
     if isinstance(input_path, list):
         ipath = input_path[0]
 
-    input_storage_options = common_storage_options if ipath.startswith('s3://') else {}
-    if output_save_path and output_save_path.startswith('s3://'):
+    input_storage_options = common_storage_options if ipath.startswith("s3://") else {}
+    if output_save_path and output_save_path.startswith("s3://"):
         output_storage_options = common_storage_options
 
-    ec = open_raw(
-        file=input_path, model=model, storage_options=input_storage_options
-    )
+    ec = open_raw(file=input_path, model=model, storage_options=input_storage_options)
 
     if (
-        export_engine == 'netcdf4'
+        export_engine == "netcdf4"
         and output_save_path is not None
-        and output_save_path.startswith('s3://')
+        and output_save_path.startswith("s3://")
     ):
         return
     ec._to_file(
