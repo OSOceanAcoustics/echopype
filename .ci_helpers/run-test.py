8fd01f9c0cff63ca208176d8443f61a3d0b10918
lsetiawan <landungs@uw.edu>
2021-04-13 10:22:20 -0700

    Detect changes to py files and run appropriate test

--- .ci_helpers/run-test.py
+++ .ci_helpers/run-test.py
@@ -3,22 +3,48 @@
 Script to run tests in Github.
 
 """
-import os
-import pytest
 import argparse
+import glob
+import os
+from pathlib import Path
+
+import pytest
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Run tests listed.')
-    parser.add_argument('testfiles', metavar='TEST_FILES', type=str)
+    parser = argparse.ArgumentParser(description="Run tests listed.")
+    parser.add_argument("touchedfiles", metavar="TOUCHED_FILES", type=str)
     args = parser.parse_args()
-    file_list = args.testfiles.split(',')
+    file_list = args.touchedfiles.split(",")
 
     pytest_args = ["--log-cli-level=WARNING", "-vv"]
-    test_files = []
+    test_to_run = {}
     for f in file_list:
+        file_path = Path(f)
         file_name, file_ext = os.path.splitext(os.path.basename(f))
-        if (file_ext == ".py") and ("test_" in file_name):
-            test_files.append(f)
+        if file_ext == ".py":
+            if any(
+                [
+                    (file_path.match("echopype/convert/*")),
+                    (file_path.match("echopype/tests/test_convert*")),
+                ]
+            ):
+                if "convert" not in test_to_run:
+                    test_to_run["convert"] = []
+                test_to_run["convert"].append(file_path)
+            elif any(
+                [
+                    (file_path.match("echopype/calibrate/*")),
+                    (file_path.match("echopype/tests/test_calibrate*")),
+                ]
+            ):
+                if "calibrate" not in test_to_run:
+                    test_to_run["calibrate"] = []
+                test_to_run["calibrate"].append(file_path)
 
-    final_args = pytest_args + test_files
-    pytest.main(final_args)
+    for k, v in test_to_run.items():
+        print(f"=== RUNNING {k.upper()} TESTS===")
+        print(f"Touched files: {','.join([os.path.basename(p) for p in v])}")
+        test_files = glob.glob(f"echopype/tests/test_{k}*.py")
+        final_args = pytest_args + test_files
+        print(f"Pytest args: {final_args}")
+        pytest.main(final_args)
