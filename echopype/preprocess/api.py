b553746d11b5a8477412e4f8f1cb2244a41a1639
leewujung <leewujung@gmail.com>
2021-04-25 09:01:52 -0700

    factor out noise-related ops to noise_est.py as a class

--- echopype/preprocess/api.py
+++ echopype/preprocess/api.py
@@ -4,6 +4,7 @@ Functions for enhancing the spatial and temporal coherence of data.
 
 import numpy as np
 from ..utils import uwa
+from .noise_est import NoiseEst
 
 
 def _check_range_uniqueness(ds):
@@ -106,12 +107,36 @@ def compute_MVBS_index_binning(ds_Sv, range_bin_interval=100, ping_num_interval=
     return ds_out
 
 
+def estimate_noise(ds_Sv, ping_num, range_bin_num, noise_max=None):
+    """Remove noise by using estimates of background noise from mean calibrated power of a collection of pings.
+
+    See ``remove_noise`` for reference.
+
+    Parameters
+    ----------
+    ds_Sv : xr.Dataset
+        dataset containing Sv and range [m]
+    ping_num : int
+        number of pings to obtain noise estimates
+    range_bin_num : int
+        number of samples along range to obtain noise estimates
+    noise_max : float
+        the upper limit for background noise expected under the operating conditions
+
+    Returns
+    -------
+    A DataArray containing noise estimated from the input ``ds_Sv``
+    """
+    noise_obj = NoiseEst(ds_Sv=ds_Sv.copy(), ping_num=ping_num, range_bin_num=range_bin_num)
+    noise_obj.estimate_noise(noise_max=noise_max)
+    return noise_obj.Sv_noise
+
+
 def remove_noise(ds_Sv, ping_num, range_bin_num, noise_max=None, SNR_threshold=3):
     """Remove noise by using estimates of background noise from mean calibrated power of a collection of pings.
 
     Reference: De Robertis & Higginbottom. 2007.
-    A post-processing technique to estimate the signal-to-noise ratio and
-    remove echosounder background noise.
+    A post-processing technique to estimate the signal-to-noise ratio and remove echosounder background noise.
     ICES Journal of Marine Sciences 64(6): 1282–1291.
 
     Parameters
@@ -130,62 +155,11 @@ def remove_noise(ds_Sv, ping_num, range_bin_num, noise_max=None, SNR_threshold=3
     Returns
     -------
     The input dataset with additional variables, including
-    the corrected Sv (``Sv_corrected``)
-    and the noise estimates (``Sv_noise``)
+    the corrected Sv (``Sv_corrected``) and the noise estimates (``Sv_noise``)
     """
-    # Obtain sound absorption coefficients
-    if 'sound_absorption' not in ds_Sv:
-        sound_absorption = uwa.calc_absorption(
-            frequency=ds_Sv.frequency,
-            temperature=ds_Sv['temperature'],
-            salinity=ds_Sv['salinity'],
-            pressure=ds_Sv['pressure'],
-        )
-    else:
-        sound_absorption = ds_Sv['sound_absorption']
-
-    # Transmission loss
-    spreading_loss = 20 * np.log10(ds_Sv['range'].where(ds_Sv['range'] >= 1, other=1))
-    absorption_loss = 2 * sound_absorption * ds_Sv['range']
-
-    # Noise estimates
-    power_cal = 10 ** ((ds_Sv['Sv'] - spreading_loss - absorption_loss) / 10)  # calibrated power without TVG, linear
-    power_cal_binned_avg = 10 * np.log10(  # binned averages of calibrated power
-        power_cal.coarsen(
-            ping_time=ping_num,
-            range_bin=range_bin_num,
-            boundary='pad'
-        ).mean()
-    )
-    noise = power_cal_binned_avg.min(dim='range_bin', skipna=True)
-    noise['ping_time'] = power_cal['ping_time'][::ping_num]  # align ping_time to first of each ping collection
-    if noise_max is not None:
-        noise = noise.where(noise < noise_max, noise_max)  # limit max noise level
-    Sv_noise = (
-            noise.reindex({'ping_time': power_cal['ping_time']}, method='ffill')  # forward fill empty index
-            + spreading_loss
-            + absorption_loss
-    )
-
-    # Sv corrected for noise
-    fac = 10 ** (ds_Sv['Sv'] / 10) - 10 ** (Sv_noise / 10)  # linear domain
-    Sv_corr = 10 * np.log10(fac.where(fac > 0, other=np.nan))
-    Sv_corr = Sv_corr.where(Sv_corr - Sv_noise > SNR_threshold, other=np.nan)  # other=-999 proposed in the paper
-
-    # Assemble output dataset
-    ds_out = ds_Sv.copy()
-    ds_out['Sv_corr'] = Sv_corr
-    ds_out['Sv_noise'] = Sv_noise
-    ds_out = ds_out.assign_attrs(
-        {
-            'noise_ping_num': ping_num,
-            'noise_range_bin_num': range_bin_num,
-            'SNR_threshold': SNR_threshold,
-            'noise_max': noise_max
-        }
-    )
-
-    return ds_out
+    noise_obj = NoiseEst(ds_Sv=ds_Sv.copy(), ping_num=ping_num, range_bin_num=range_bin_num)
+    noise_obj.remove_noise(noise_max=noise_max, SNR_threshold=SNR_threshold)
+    return noise_obj.ds_Sv
 
 
 def regrid():
