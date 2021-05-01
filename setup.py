30c21f5a0124fb12a8173b29f31b1d486a1a6888
lsetiawan <landungs@uw.edu>
2021-04-30 17:11:57 -0700

    Moving some metadata to setup cfg

--- setup.py
+++ setup.py
@@ -14,27 +14,7 @@ with open("requirements.txt") as f:
 INSTALL_REQUIRES = [t.strip() for t in requirements]
 
 opts = dict(
-    name="echopype",
-    maintainer="Wu-Jung Lee",
-    maintainer_email="leewujung@gmail.com",
-    description="Enhancing the interoperability and scalability in analyzing ocean sonar data",
     long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    url="https://github.com/OSOceanAcoustics/echopype",
-    download_url="",
-    license="Apache License, Version 2.0",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Console",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Topic :: Scientific/Engineering",
-    ],
-    author="Wu-Jung Lee",
-    author_email="leewujung@gmail.com",
-    platforms="OS Independent",
     packages=find_packages(exclude=["*tests", "*test_data"]),
     package_dir={"": "."},
     package_data={"echopype": [pjoin("data", "*")]},
