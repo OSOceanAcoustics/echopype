55088e4e4bc85e83b0014bc5af795a15a38ad074
Yang Yunyi <yunyiyang@ln.hk>
2021-05-11 22:12:33 +0800

    Added code to check if the file exists

--- echopype/echodata/api.py
+++ echopype/echodata/api.py
@@ -1,9 +1,13 @@
+from os import path
+
 from .echodata import EchoData
 
 
 def open_converted(converted_raw_path, storage_options=None):
     """Create an EchoData object from a single converted zarr/nc files."""
     # TODO: combine multiple files when opening
+    if not path.isfile(converted_raw_path):
+        raise FileNotFoundError(f"File {converted_raw_path} does not exist")
     return EchoData(
         converted_raw_path=converted_raw_path, storage_options=storage_options
     )
