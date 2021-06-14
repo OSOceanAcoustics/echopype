caaca3912d67d86f8887877f7e6ad9ee67ee2931
Landung "Don" Setiawan <landungs@uw.edu>
2021-06-14 09:39:38 -0700

    Comment out root init __all__

--- echopype/__init__.py
+++ echopype/__init__.py
@@ -7,4 +7,4 @@ from .convert.api import open_raw
 from .echodata.api import open_converted
 from .process import Process  # noqa
 
-__all__ = [open_raw, open_converted, calibrate]
+# __all__ = [open_raw, open_converted, calibrate]
