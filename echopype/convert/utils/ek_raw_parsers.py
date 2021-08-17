658e1b3962be16d157ca2d1b118f20fc7942b386
Gavin Macaulay <gavin@macaulay.co.nz>
2021-08-18 11:31:05 +1200

    Better fix for commit 1c446f7
    When there is no angle data for a particular ping, use a 2D empty array instead of a 1D array. This is to match the dimension of the angle data for the other pings (otherwise a crash happens when making the data have the same number of elements for all pings in the file).

--- echopype/convert/utils/ek_raw_parsers.py
+++ echopype/convert/utils/ek_raw_parsers.py
@@ -1693,7 +1693,7 @@ class SimradRawParser(_SimradDatagramParser):
 
             else:
                 data["power"] = np.empty((0,), dtype="int16")
-                data["angle"] = np.empty((0,), dtype="int8")
+                data["angle"] = np.empty((0,2), dtype="int8")
 
         elif version == 3:
 
