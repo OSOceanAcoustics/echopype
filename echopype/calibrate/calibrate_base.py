f08fb9b334aaf3e6a8da4ac3fdec4181c1bc6465
imranmaj <49664304+imranmaj@users.noreply.github.com>
2021-10-07 02:29:09 -0700

    Add mobile case

--- echopype/calibrate/calibrate_base.py
+++ echopype/calibrate/calibrate_base.py
@@ -1,7 +1,8 @@
 import abc
-from typing import Dict, Literal, Optional
+from typing import Dict, List, Literal, Optional
 
 import numpy as np
+import scipy.interpolate
 import xarray as xr
 
 ENV_PARAMS = ("temperature", "salinity", "pressure", "sound_speed", "sound_absorption")
@@ -11,15 +12,32 @@ CAL_PARAMS = {
     "AZFP": ("EL", "DS", "TVR", "VTX", "equivalent_beam_angle", "Sv_offset"),
 }
 
+DataKind = Literal["stationary", "mobile", "organized"]
+InterpMethod = Literal["linear", "nearest", "zero", "slinear", "quadratic", "cubic"]
+VALID_INTERP_METHODS: Dict[DataKind, List[InterpMethod]] = {
+    "stationary": ["linear", "nearest", "zero", "slinear", "quadratic", "cubic"],
+    "mobile": ["linear", "nearest", "cubic"],
+    "organized": ["linear", "nearest"],
+}
+
 
 class EnvParams:
     def __init__(
         self,
         env_params,
-        data_kind: Literal["stationary", "mobile", "organized"],  # organized name?
-        interp_method: Literal["linear", "nearest"] = "linear",
+        data_kind: DataKind,  # organized name?
+        interp_method: InterpMethod = "linear",
         extrap_method: Optional[Literal["linear", "nearest"]] = None,
     ):
+        """
+        Interp methods limited depending on data_kind
+        Extrap only available for stationary and organized
+        """
+        if interp_method not in VALID_INTERP_METHODS[data_kind]:
+            raise ValueError(
+                f"invalid interp_method {interp_method} for data_kind {data_kind}"
+            )
+
         self.env_params = env_params
         self.data_kind = data_kind
         self.interp_method = interp_method
@@ -30,76 +48,102 @@ class EnvParams:
             dims = ["ping_time"]
         elif self.data_kind == "mobile":
             dims = ["latitude", "longitude"]
+            # dims = ["time"] # lat/lon are coords
         elif self.data_kind == "organized":
-            dims = ["location_time", "latitude", "longitude"]
+            dims = ["time", "latitude", "longitude"]
         else:
             raise ValueError("invalid data_kind")
 
         env_params = self.env_params
 
-        min_max = {
-            dim: {"min": env_params[dim].min(), "max": env_params[dim].max()}
-            for dim in dims
-        }
+        if self.data_kind == "mobile":
+            result = {}
+            for var, values in env_params.data_vars.items():
+                points = np.column_stack(
+                    (env_params["latitude"].data, env_params["longitude"].data)
+                )
+                values = values.data
+                xi = np.column_stack(
+                    (
+                        echodata.platform["latitude"].data,
+                        echodata.platform["longitude"].data,
+                    )
+                )
+                interp = scipy.interpolate.griddata(
+                    points, values, xi, method=self.interp_method
+                )
+                result[var] = interp
+            env_params = xr.Dataset(result)
+        else:
+            min_max = {
+                dim: {"min": env_params[dim].min(), "max": env_params[dim].max()}
+                for dim in dims
+            }
 
-        extrap = env_params.interp(
-            {dim: echodata.platform[dim].data for dim in dims},
-            method=self.extrap_method,
-            # scipy interp uses "extrapolate" but scipy interpn uses None
-            kwargs={"fill_value": "extrapolate" if len(dims) == 1 else None},
-        )
-        extrap_unique_idx = {
-            dim: np.unique(extrap[dim], return_index=True)[1] for dim in dims
-        }
-        extrap = extrap.isel(**extrap_unique_idx)
-        interp = env_params.interp(
-            {dim: echodata.platform[dim].data for dim in dims},
-            method=self.interp_method,
-        )
-        interp_unique_idx = {
-            dim: np.unique(interp[dim], return_index=True)[1] for dim in dims
-        }
-        interp = interp.isel(**interp_unique_idx)
+            extrap = env_params.interp(
+                {dim: echodata.platform[dim].data for dim in dims},
+                method=self.extrap_method,
+                # scipy interp uses "extrapolate" but scipy interpn uses None
+                kwargs={"fill_value": "extrapolate" if len(dims) == 1 else None},
+            )
+            extrap_unique_idx = {
+                dim: np.unique(extrap[dim], return_index=True)[1] for dim in dims
+            }
+            extrap = extrap.isel(**extrap_unique_idx)
+            interp = env_params.interp(
+                {dim: echodata.platform[dim].data for dim in dims},
+                method=self.interp_method,
+            )
+            interp_unique_idx = {
+                dim: np.unique(interp[dim], return_index=True)[1] for dim in dims
+            }
+            interp = interp.isel(**interp_unique_idx)
 
-        if self.extrap_method is not None:
-            less = extrap.sel(
-                {dim: extrap[dim][extrap[dim] < min_max[dim]["min"]] for dim in dims}
-            )
-            middle = interp.sel(
-                {
-                    dim: interp[dim][
-                        np.logical_and(
-                            interp[dim] >= min_max[dim]["min"],
-                            interp[dim] <= min_max[dim]["max"],
-                        )
-                    ]
-                    for dim in dims
-                }
-            )
-            greater = extrap.sel(
-                {dim: extrap[dim][extrap[dim] > min_max[dim]["max"]] for dim in dims}
-            )
+            if self.extrap_method is not None:
+                less = extrap.sel(
+                    {
+                        dim: extrap[dim][extrap[dim] < min_max[dim]["min"]]
+                        for dim in dims
+                    }
+                )
+                middle = interp.sel(
+                    {
+                        dim: interp[dim][
+                            np.logical_and(
+                                interp[dim] >= min_max[dim]["min"],
+                                interp[dim] <= min_max[dim]["max"],
+                            )
+                        ]
+                        for dim in dims
+                    }
+                )
+                greater = extrap.sel(
+                    {
+                        dim: extrap[dim][extrap[dim] > min_max[dim]["max"]]
+                        for dim in dims
+                    }
+                )
 
-            # remove zero length dims (xarray does not support in combine_by_coords)
-            non_zero_dims = [
-                ds
-                for ds in (less, middle, greater)
-                if all(dim_len > 0 for dim_len in ds.dims.values())
-            ]
-            env_params = xr.combine_by_coords(non_zero_dims)
+                # remove zero length dims (xarray does not support in combine_by_coords)
+                non_zero_dims = [
+                    ds
+                    for ds in (less, middle, greater)
+                    if all(dim_len > 0 for dim_len in ds.dims.values())
+                ]
+                env_params = xr.combine_by_coords(non_zero_dims)
 
-        if self.data_kind in ("mobile", "organized"):
-            # get platform latitude and longitude indexed by ping_time
-            interp_plat = echodata.platform.interp(
-                {"location_time": echodata.platform["ping_time"]}
-            )
-            # get env_params latitude and longitude indexed by ping_time
-            env_params = env_params.interp(
-                {
-                    "latitude": interp_plat["latitude"],
-                    "longitude": interp_plat["longitude"],
-                }
-            )
+        # if self.data_kind == "organized":
+        #     # get platform latitude and longitude indexed by ping_time
+        #     interp_plat = echodata.platform.interp(
+        #         {"time": echodata.platform["ping_time"]}
+        #     )
+        #     # get env_params latitude and longitude indexed by ping_time
+        #     env_params = env_params.interp(
+        #         {
+        #             "latitude": interp_plat["latitude"],
+        #             "longitude": interp_plat["longitude"],
+        #         }
+        #     )
 
         return {var: env_params[var] for var in env_params.data_vars}
 
