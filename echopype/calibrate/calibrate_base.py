fdefd6cc60584c6e1c306ef4c223ed58e4fbb824
imranmaj <49664304+imranmaj@users.noreply.github.com>
2021-10-28 12:43:18 -0700

    Use Literal from typing_extensions for compatibility with Python 3.7

--- echopype/calibrate/calibrate_base.py
+++ echopype/calibrate/calibrate_base.py
@@ -1,9 +1,10 @@
 import abc
-from typing import Dict, List, Literal, Optional
+from typing import Dict, List, Optional
 
 import numpy as np
 import scipy.interpolate
 import xarray as xr
+from typing_extensions import Literal
 
 ENV_PARAMS = ("temperature", "salinity", "pressure", "sound_speed", "sound_absorption")
 
