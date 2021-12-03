64496305c39e6c186c43095a0e625e4ade34e602
Emilio Mayorga <emiliomayorga@gmail.com>
2021-12-02 19:11:55 -0800

    Update set_groups_ek80.py
    Remove unused imports (flake8 fix)

--- echopype/convert/set_groups_ek80.py
+++ echopype/convert/set_groups_ek80.py
@@ -4,11 +4,7 @@ from typing import List
 import numpy as np
 import xarray as xr
 
-from ..echodata.convention.attrs import (
-    DEFAULT_BEAM_COORD_ATTRS,
-    DEFAULT_PLATFORM_COORD_ATTRS,
-    DEFAULT_PLATFORM_VAR_ATTRS,
-)
+from ..echodata.convention.attrs import DEFAULT_BEAM_COORD_ATTRS
 from ..utils.coding import set_encodings
 from .set_groups_base import SetGroupsBase
 
