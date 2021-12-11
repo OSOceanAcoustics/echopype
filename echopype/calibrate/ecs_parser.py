056f470a72cac667889ecfe12e4aec179a6c5e75
leewujung <leewujung@gmail.com>
2021-12-11 02:21:57 -0800

    fix all noqa

--- echopype/calibrate/ecs_parser.py
+++ echopype/calibrate/ecs_parser.py
@@ -3,22 +3,22 @@ from datetime import datetime
 
 SEPARATOR = re.compile("#=+#\n")
 STATUS_CRUDE = re.compile(
-    "#\s+(?P<status>(.+))\s+#\n"
-)  # noqa  # TODO: removing trailing 0s
+    "#\s+(?P<status>(.+))\s+#\n"  # noqa
+)
 STATUS_FINE = re.compile("#\s+(?P<status>\w+) SETTINGS\s*#\n")  # noqa
 ECS_HEADER = re.compile(
-    "#\s+ECHOVIEW CALIBRATION SUPPLEMENT \(.ECS\) FILE \((?P<data_type>\w+)\)\s+#\n"
-)  # noqa
+    "#\s+ECHOVIEW CALIBRATION SUPPLEMENT \(.ECS\) FILE \((?P<data_type>\w+)\)\s+#\n"  # noqa
+)
 ECS_TIME = re.compile(
-    "#\s+(?P<date>\d{1,2}\/\d{1,2}\/\d{4}) (?P<time>\d{1,2}\:\d{1,2}\:\d{1,2})(.\d+)?\s+#\n"
-)  # noqa
+    "#\s+(?P<date>\d{1,2}\/\d{1,2}\/\d{4}) (?P<time>\d{1,2}\:\d{1,2}\:\d{1,2})(.\d+)?\s+#\n"  # noqa
+)
 ECS_VERSION = re.compile("Version (?P<version>\d+\.\d+)\s*\n")  # noqa
 PARAM_MATCHER = re.compile(
-    "\s*(?P<skip>#?)\s*(?P<param>\w+)\s*=\s*(?P<val>-?\d+(?:\.\d+)?)?\s*#?(.*)\n"
-)  # noqa
+    "\s*(?P<skip>#?)\s*(?P<param>\w+)\s*=\s*(?P<val>-?\d+(?:\.\d+)?)?\s*#?(.*)\n"  # noqa
+)
 CAL = re.compile(
-    "(SourceCal|LocalCal) (?P<source>\w+)\s*\n", re.I
-)  # ignore case  # noqa
+    "(SourceCal|LocalCal) (?P<source>\w+)\s*\n", re.I  # ignore case  # noqa
+)
 
 
 class ECSParser:
