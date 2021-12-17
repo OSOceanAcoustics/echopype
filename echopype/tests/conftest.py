d02b188b2219aa5cd81f9d1d7f048965a0df014f
lsetiawan <landungs@uw.edu>
2021-12-17 11:51:36 -0800

    Add test_path fixture

--- echopype/tests/conftest.py
+++ echopype/tests/conftest.py
@@ -9,6 +9,22 @@ from echopype.testing import TEST_DATA_FOLDER
 
 
 @pytest.fixture(scope="session")
+def test_path():
+    ek60_path = TEST_DATA_FOLDER / "ek60"
+    ek80_path = TEST_DATA_FOLDER / "ek80"
+    ek80_new_path = TEST_DATA_FOLDER / "ek80_new"
+    azfp_path = TEST_DATA_FOLDER / "azfp"
+    ad2cp_path = TEST_DATA_FOLDER / "ad2cp"
+    return {
+        'EK60': ek60_path,
+        'EK80': ek80_path,
+        'EK80_NEW': ek80_new_path,
+        'AZFP': azfp_path,
+        'AD2CP': ad2cp_path
+    }
+
+
+@pytest.fixture(scope="session")
 def minio_bucket():
     common_storage_options = dict(
         client_kwargs=dict(endpoint_url="http://localhost:9000/"),
