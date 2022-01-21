1a5068a2aab7f3eefff83b7ec854968771e3b740
leewujung <leewujung@gmail.com>
2022-01-20 19:11:55 -0800

    simplify FG absorption implementation

--- echopype/utils/uwa.py
+++ echopype/utils/uwa.py
@@ -120,31 +120,30 @@ def calc_absorption(
         P1 = 1.0
         f1 = 2.8 * np.sqrt(salinity / 35) * 10 ** (4 - 1245 / (temperature + 273))
         A2 = 21.44 * salinity / c * (1 + 0.025 * temperature)
-        P2 = 1.0 - 1.37e-4 * pressure + 6.2e-9 * pressure * pressure
+        P2 = 1.0 - 1.37e-4 * pressure + 6.2e-9 * pressure ** 2
         f2 = (
-            8.17
-            * 10 ** (8 - 1990 / (temperature + 273))
+            8.17 * 10 ** (8 - 1990 / (temperature + 273))
             / (1 + 0.0018 * (salinity - 35))
         )
-        P3 = 1.0 - 3.83e-5 * pressure + 4.9e-10 * pressure * pressure
+        P3 = 1.0 - 3.83e-5 * pressure + 4.9e-10 * pressure ** 2
         if temperature < 20:
             A3 = (
                 4.937e-4
                 - 2.59e-5 * temperature
-                + 9.11e-7 * temperature * temperature
-                - 1.5e-8 * temperature * temperature * temperature
+                + 9.11e-7 * temperature ** 2
+                - 1.5e-8 * temperature ** 3
             )
         else:
             A3 = (
                 3.964e-4
                 - 1.146e-5 * temperature
-                + 1.45e-7 * temperature * temperature
-                - 6.5e-10 * temperature * temperature * temperature
+                + 1.45e-7 * temperature ** 2
+                - 6.5e-10 * temperature ** 3
             )
         a = (
-            A1 * P1 * f1 * f * f / (f1 * f1 + f * f)
-            + A2 * P2 * f2 * f * f / (f2 * f2 + f * f)
-            + A3 * P3 * f * f
+            A1 * P1 * f1 * f ** 2 / (f ** 2 + f1 ** 2)
+            + A2 * P2 * f2 * f ** 2 / (f ** 2 + f2 ** 2)
+            + A3 * P3 * f ** 2
         )
         sea_abs = a / 1000  # formula output is in unit [dB/km]
 
