7a2b9af5921220c871a2f2573c2d755884fcb615
imranmaj <49664304+imranmaj@users.noreply.github.com>
2022-03-03 04:26:15 -0800

    Test parsed_params with dictionary

--- echopype/tests/calibrate/test_ecs.py
+++ echopype/tests/calibrate/test_ecs.py
@@ -6,6 +6,57 @@ from echopype.calibrate.ecs_parser import ECSParser
 data_dir = Path("./echopype/test_data/ecs")
 
 
+CORRECT_PARSED_PARAMS = {
+    "fileset": {
+        "SoundSpeed": 1496.0,
+        "TvgRangeCorrection": "BySamples",
+        "TvgRangeCorrectionOffset": 2.0,
+    },
+    "sourcecal": {
+        "T1": {
+            "AbsorptionCoefficient": 0.002822,
+            "EK60SaCorrection": -0.7,
+            "Ek60TransducerGain": 22.95,
+            "MajorAxis3dbBeamAngle": 10.82,
+            "MajorAxisAngleOffset": 0.25,
+            "MajorAxisAngleSensitivity": 13.89,
+            "MinorAxis3dbBeamAngle": 10.9,
+            "MinorAxisAngleOffset": -0.18,
+            "MinorAxisAngleSensitivity": 13.89,
+            "SoundSpeed": 1480.6,
+            "TwoWayBeamAngle": -17.37,
+        },
+        "T2": {
+            "AbsorptionCoefficient": 0.009855,
+            "EK60SaCorrection": -0.52,
+            "Ek60TransducerGain": 26.07,
+            "MajorAxis3dbBeamAngle": 6.85,
+            "MajorAxisAngleOffset": 0.0,
+            "MajorAxisAngleSensitivity": 21.970001,
+            "MinorAxis3dbBeamAngle": 6.81,
+            "MinorAxisAngleOffset": -0.08,
+            "MinorAxisAngleSensitivity": 21.970001,
+            "SoundSpeed": 1480.6,
+            "TwoWayBeamAngle": -21.01,
+        },
+        "T3": {
+            "AbsorptionCoefficient": 0.032594,
+            "EK60SaCorrection": -0.3,
+            "Ek60TransducerGain": 26.55,
+            "MajorAxis3dbBeamAngle": 6.52,
+            "MajorAxisAngleOffset": 0.37,
+            "MajorAxisAngleSensitivity": 23.12,
+            "MinorAxis3dbBeamAngle": 6.58,
+            "MinorAxisAngleOffset": -0.05,
+            "MinorAxisAngleSensitivity": 23.12,
+            "SoundSpeed": 1480.6,
+            "TwoWayBeamAngle": -20.47,
+        },
+    },
+    "localcal": {"MyCal": {"TwoWayBeamAngle": -17.37}},
+}
+
+
 def test_convert_ecs():
     # Test converting an EV calibration file (ECS)
     ecs_path = data_dir / "Summer2017_JuneCal_3freq_mod.ecs"
@@ -19,13 +70,7 @@ def test_convert_ecs():
     assert ecs.file_creation_time == datetime(
         year=2015, month=6, day=19, hour=23, minute=26, second=4
     )
-    assert ecs.parsed_params["fileset"]["SoundSpeed"] == 1496
-    assert ecs.parsed_params["fileset"]["TvgRangeCorrection"] == "BySamples"
-    assert ecs.parsed_params["fileset"]["TvgRangeCorrectionOffset"] == 2
-    assert ecs.parsed_params["sourcecal"]["T1"]["MinorAxisAngleOffset"] == -0.18
-    assert ecs.parsed_params["sourcecal"]["T2"]["MajorAxis3dbBeamAngle"] == 6.85
-    assert ecs.parsed_params["sourcecal"]["T3"]["Ek60TransducerGain"] == 26.55
-    assert ecs.parsed_params["localcal"]["MyCal"]["TwoWayBeamAngle"] == -17.37
+    assert ecs.parsed_params == CORRECT_PARSED_PARAMS
 
     cal_params = ecs.get_cal_params()
 
