c1426f299ac1429dfca0d84a3d2c24d26c166e9f
b-reyes <reyesb123@gmail.com>
2022-08-30 11:45:55 -0700

    create PreprocessCallable class and add functionality to laze_combine

--- echopype/echodata/combine_lazily.py
+++ echopype/echodata/combine_lazily.py
@@ -1,38 +1,75 @@
-from .combine_preprocess import ProvenancePreprocess
+from .combine_preprocess import PreprocessCallable
 from echopype.echodata import EchoData
 from datatree import DataTree
 import xarray as xr
 
-group_preprocess = {'provenance': ProvenancePreprocess}
-
-
 # desired_raw_file_paths = fs.glob('OOI_zarrs_ep_ex/temp/*.zarr')
 
 
+def reassign_attrs(ed_comb: EchoData):
+    """
+    Reassigns stored group attributes to the Provenance group.
+    """
 
-# initial strucuture for lazy combine
-# tree_dict = {}
-# result = EchoData()
-#
-# # for group, value in EchoData.group_map.items()[:2]:
-# for group, value in list(EchoData.group_map.items())[:3]:
-#
-#     print(value["ep_group"])
-#
-#     obj = ProvenancePreprocess(desired_raw_file_paths)
-#
-#     combined_group = xr.open_mfdataset(desired_raw_file_paths,
-#                                        engine='zarr', coords='minimal', preprocess=obj,
-#                                        combine="nested", group=value["ep_group"], concat_dim=None)
-#
-#     if value["ep_group"] is None:
-#         tree_dict["/"] = combined_group
-#     else:
-#         tree_dict[value["ep_group"]] = combined_group
-#
-# # Set tree into echodata object
-# result._set_tree(tree=DataTree.from_dict(tree_dict, name="root"))
-# result._load_tree()
+    for group, value in EchoData.group_map.items():
+
+        if value["ep_group"] not in ['Sonar/Beam_group2', 'Sonar/Beam_group3', 'Sonar/Beam_group4']:
+
+            if value["ep_group"] != "Provenance":
+
+                attr_var_name = group + '_attrs'
+                attr_coord_name = group + '_attr_key'
+
+                if value["ep_group"]:
+                    ed_grp = value["ep_group"]
+                else:
+                    ed_grp = "Top-level"
+
+                # move attribute variable to Provenance
+                ed_comb["Provenance"][attr_var_name] = ed_comb[ed_grp][attr_var_name]
+
+                # remove attribute variable and coords from group
+                ed_comb[ed_grp] = ed_comb[ed_grp].drop_vars([attr_var_name, attr_coord_name,
+                                                             'echodata_filename'])
+
+
+def lazy_combine(desired_raw_file_paths):
+
+    # initial strucuture for lazy combine
+    tree_dict = {}
+    result = EchoData()
+
+    # grab object that does pre-processing
+    preprocess_obj = PreprocessCallable(desired_raw_file_paths)
+
+    for group, value in EchoData.group_map.items():
+
+        print(value["ep_group"])
+
+        if value["ep_group"] not in ['Sonar/Beam_group2', 'Sonar/Beam_group3', 'Sonar/Beam_group4']:
+
+            preprocess_obj.update_ed_group(group)
+
+            combined_group = xr.open_mfdataset(desired_raw_file_paths,
+                                               engine='zarr', coords='minimal', preprocess=preprocess_obj,
+                                               combine="nested", group=value["ep_group"], concat_dim=None)
+
+            if value["ep_group"] is None:
+                tree_dict["/"] = combined_group
+            else:
+                tree_dict[value["ep_group"]] = combined_group
+
+    # Set tree into echodata object
+    result._set_tree(tree=DataTree.from_dict(tree_dict, name="root"))
+    result._load_tree()
+
+    # reassign stored group attributes to the provenance group
+    reassign_attrs(result)
+
+    # TODO: modify Provenance conversion_time attribute
+    #   dt.utcnow().isoformat(timespec="seconds") + "Z",  # use UTC time
+
+    return result
 
 
 
--- echopype/echodata/combine_preprocess.py
+++ echopype/echodata/combine_preprocess.py
@@ -1,39 +1,47 @@
 import numpy as np
 from pathlib import Path
 import xarray as xr
+from typing import List
 
 
-class ProvenancePreprocess:
-    def __init__(self, file_paths):
+class PreprocessCallable:
+    """
+    Class that has all preprocessing functions and is callable.
+    """
+    def __init__(self, file_paths: List[str]):
         self.file_paths = file_paths
+        self.ed_group = None
 
     def __call__(self, ds):
-        self.assign_file_index(ds)
-        self.store_attrs(ds)
+
+        if self.ed_group == "provenance":
+            self._assign_file_index(ds)
+
+        self._store_attrs(ds)
+
+        # TODO: add method to check and correct reversed times
 
         return ds
 
-    def assign_file_index(self, ds):
+    def update_ed_group(self, group: str):
+        self.ed_group = group
+
+    def _assign_file_index(self, ds):
 
         ind_file = self.file_paths.index(ds.encoding["source"])
         ds['filenames'] = (['filenames'], np.array([ind_file]))
 
-    def store_attrs(self, ds):
+    # TODO: add method to check and correct reversed times
+
+    def _store_attrs(self, ds):
 
         file_name = Path(ds.encoding["source"]).name
 
+        grp_key_name = self.ed_group + '_attr_key'
+        grp_attr_names = np.array(list(ds.attrs.keys()))
+
         attrs_var = xr.DataArray(data=np.array([list(ds.attrs.values())]),
                                  coords={'echodata_filename': (['echodata_filename'], np.array([file_name])),
-                                         'provenance_attr_key': (['provenance_attr_key'],
-                                                                 np.array(['conversion_software_name',
-                                                                           'conversion_software_version',
-                                                                           'conversion_time',
-                                                                           'duplicate_ping_times']))})
+                                         grp_key_name: ([grp_key_name], grp_attr_names)})
 
-        ds['provenance_attrs'] = attrs_var
-
-
-
-
-
-
+        ds[self.ed_group + '_attrs'] = attrs_var
