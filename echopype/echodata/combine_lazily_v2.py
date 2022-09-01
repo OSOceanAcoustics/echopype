e2b9ec664a2b01003b56c9683c569b11bd9706a5
b-reyes <reyesb123@gmail.com>
2022-08-31 17:37:42 -0700

    get a working version of direct_write in combine_lazily_v2

--- echopype/echodata/combine_lazily_v2.py
+++ echopype/echodata/combine_lazily_v2.py
@@ -2,6 +2,7 @@ import xarray as xr
 import pandas as pd
 import dask.array
 import dask
+import numpy as np
 
 const_dims = ['channel']
 
@@ -16,19 +17,19 @@ def get_ds_dims_info(ds_list):
     dims_max = dims_df.max(axis=0).to_dict()
     dims_csum = dims_df.cumsum(axis=0).to_dict()
 
-    return dims_sum, dims_csum, dims_max
+    return dims_sum, dims_csum, dims_max, dims_df
 
 
-def get_temp_arr_vals(dims, dims_max, dims_sum):
+def get_temp_arr_vals(dims, dims_max, dims_sum, dims_df):
 
     shape = [dims_max[dim] if dim in const_dims else dims_sum[dim] for dim in dims]
 
-    chnk_shape = [None if dim in const_dims else dims_max[dim] for dim in dims]
+    chnk_shape = [None if dim in const_dims else tuple(dims_df[dim].to_list()) for dim in dims]
 
     return shape, chnk_shape
 
 
-def constuct_lazy_ds(ds_model, dims_sum, dims_max):
+def constuct_lazy_ds(ds_model, dims_sum, dims_max, dims_df):
 
     xr_dict = dict()
 
@@ -36,8 +37,9 @@ def constuct_lazy_ds(ds_model, dims_sum, dims_max):
     for name, val in ds_model.variables.items():
 
         if ('channel',) != val.dims:
-            shape, chnk_shape = get_temp_arr_vals(val.dims, dims_max, dims_sum)
-            temp_arr = dask.array.zeros(shape=shape, chunks=chnk_shape)
+            shape, chnk_shape = get_temp_arr_vals(val.dims, dims_max, dims_sum, dims_df)
+
+            temp_arr = dask.array.zeros(shape=shape, chunks=chnk_shape, dtype=val.dtype)
 
             xr_dict[name] = (val.dims, temp_arr, val.attrs)
 
@@ -51,8 +53,6 @@ def constuct_lazy_ds(ds_model, dims_sum, dims_max):
 
 def get_region(ds_ind, dims_csum):
 
-    print([csum[ds_ind] for dim, csum in dims_csum.items()])
-
     if ds_ind == 0:
         region = {dim: slice(0, csum[ds_ind]) for dim, csum in dims_csum.items() if dim not in const_dims}
 
@@ -62,31 +62,39 @@ def get_region(ds_ind, dims_csum):
     return region
 
 
+def get_fill_dict(ds_lazy):
+
+    fill_vals = dict()
+    for var, val in ds_lazy.variables.items():
+
+        if val.dtype == np.float64:
+            fill_vals[var] = {'_FillValue': np.nan}
+        elif val.dtype == np.dtype('<M8[ns]'):
+            fill_vals[var] = {'_FillValue': np.datetime64("NaT")}
+        else:
+            raise NotImplementedError("Setting fill value for dtype not implemented!")
+
+    return fill_vals
+
 
 def direct_write(path, ds_list):
 
-    dims_sum, dims_csum, dims_max = get_ds_dims_info(ds_list)
+    dims_sum, dims_csum, dims_max, dims_df = get_ds_dims_info(ds_list)
 
-    ds_lazy, unwritten_vars = constuct_lazy_ds(ds_list[0], dims_sum, dims_max)
+    ds_lazy, unwritten_vars = constuct_lazy_ds(ds_list[0], dims_sum, dims_max, dims_df)
 
-    # ds_lazy.to_zarr(path, compute=False)
+    # set fill value for each of the arrays
+    fill_vals = get_fill_dict(ds_lazy)
+
+    ds_lazy.to_zarr(path, compute=False, encoding=fill_vals)
 
     for i in range(len(ds_list)):
 
-        print(get_region(i, dims_csum))
+        ds_list[i] = ds_list[i].drop(unwritten_vars)
 
+        ds_list[i].to_zarr(path, region=get_region(i, dims_csum))
+        #TODO: figure out why time1 is not being correctly written to zarr
 
-    #
-    # eds_lazy[0] = eds_lazy[0].drop(['time1', 'channel', 'frequency_nominal'])
-    # eds_lazy[0].to_zarr(path, region={"time1": slice(0, var_cumulative_sum["time1"].loc[0])})
-    #
-    # for i in range(1, len(eds_lazy)):
-    #     print(i)
-    #     eds_lazy[i] = eds_lazy[i].drop(['time1', 'channel', 'frequency_nominal'])
-    #
-    #     print(var_cumulative_sum["time1"].loc[i - 1], var_cumulative_sum["time1"].loc[i])
-    #     slc = slice(var_cumulative_sum["time1"].loc[i - 1], var_cumulative_sum["time1"].loc[i])
-    #     eds_lazy[i].to_zarr(path, region={"time1": slc})
 
 
 # def lazy_combine(path, eds):
