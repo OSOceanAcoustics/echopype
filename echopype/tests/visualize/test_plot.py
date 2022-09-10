4612901317f39f65ba32cea3ba659ed51161f721
Emilio Mayorga <emiliomayorga@gmail.com>
2022-09-09 17:24:11 -0700

    Turn filenames dimension into a coordinate variable

--- echopype/tests/visualize/test_plot.py
+++ echopype/tests/visualize/test_plot.py
@@ -173,6 +173,10 @@ def test_plot_mvbs(
     azfp_xml_path,
     range_kwargs,
 ):
+
+    # pytest.xfail("Test momentarily (2022-9-9, EM) set to xfail due to 'Killed' failure")
+
+
     # TODO: Need to figure out how to compare the actual rendered plots
     ed = echopype.open_raw(filepath, sonar_model, azfp_xml_path)
     if ed.sonar_model.lower() == 'azfp':
--- echopype/utils/prov.py
+++ echopype/utils/prov.py
@@ -27,7 +27,7 @@ def echopype_prov_attrs(process_type: ProcessType) -> Dict[str, str]:
     return prov_dict
 
 
-def source_files_vars(source_paths: Union[str, List[Any]]) -> Dict[str, Tuple]:
+def source_files_vars(source_paths: Union[str, List[Any]]) -> Tuple[Dict[str, Tuple], Dict[str, Tuple]]:
     """
     Create source_filenames provenance variable dict to be used for creating
     xarray dataarray.
@@ -42,6 +42,9 @@ def source_files_vars(source_paths: Union[str, List[Any]]) -> Dict[str, Tuple]:
     source_files_var: Dict[str, Tuple]
         Single-element dict containing a tuple for creating the
         source_filenames xarray dataarray with filenames dimension
+    source_files_coord: Dict[str, Tuple]
+        Single-element dict containing a tuple for creating the
+        filenames coordinate variable dataarray
     """
 
     # Handle a plain string containing a single path,
@@ -59,4 +62,11 @@ def source_files_vars(source_paths: Union[str, List[Any]]) -> Dict[str, Tuple]:
         ),
     }
 
-    return source_files_var
+    source_files_coord = {
+        "filenames": (
+            "filenames",
+            list(range(len(source_files))),
+            {"long_name": "Source filenames index"},
+        ),
+    }
+    return source_files_var, source_files_coord
