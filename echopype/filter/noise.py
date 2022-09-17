30b15309db59ac8f6f81a1d3b986c088cfb6e890
leewujung <leewujung@gmail.com>
2022-09-17 16:55:37 -0400

    add filter/noise.py

---
+++ echopype/filter/noise.py
@@ -0,0 +1,93 @@
+from typing import Union
+
+import xarray as xr
+
+
+def estimate_noise(ds_Sv, ping_num, range_sample_num, noise_max=None):
+    """
+    Remove noise by using estimates of background noise
+    from mean calibrated power of a collection of pings.
+
+    See ``remove_noise`` for reference.
+
+    Parameters
+    ----------
+    ds_Sv : xr.Dataset
+        dataset containing ``Sv`` and ``echo_range`` [m]
+    ping_num : int
+        number of pings to obtain noise estimates
+    range_sample_num : int
+        number of samples along the ``range_sample`` dimension to obtain noise estimates
+    noise_max : float
+        the upper limit for background noise expected under the operating conditions
+
+    Returns
+    -------
+    A DataArray containing noise estimated from the input ``ds_Sv``
+    """
+    pass
+
+
+def mean_bkg(ds_Sv, ping_num, range_sample_num, noise_max=None, SNR_threshold=3):
+    """
+    Remove noise by using estimates of background noise
+    from mean calibrated power of a collection of pings.
+
+    Reference: De Robertis & Higginbottom. 2007.
+    A post-processing technique to estimate the signal-to-noise ratio
+    and remove echosounder background noise.
+    ICES Journal of Marine Sciences 64(6): 1282–1291.
+
+    Parameters
+    ----------
+    ds_Sv : xr.Dataset
+        dataset containing ``Sv`` and ``echo_range`` [m]
+    ping_num : int
+        number of pings to obtain noise estimates
+    range_sample_num : int
+        number of samples along the ``range_sample`` dimension to obtain noise estimates
+    noise_max : float
+        the upper limit for background noise expected under the operating conditions
+    SNR_threshold : float
+        acceptable signal-to-noise ratio, default to 3 dB
+
+    Returns
+    -------
+    The input dataset with additional variables, including
+    the corrected Sv (``Sv_corrected``) and the noise estimates (``Sv_noise``)
+    """
+    pass
+
+
+class MeanBkgNoiseEst:
+    """
+    Attributes
+    ----------
+    ds_Sv : xr.Dataset
+        dataset containing ``Sv`` and ``echo_range`` [m]
+    ping_num : int
+        number of pings to obtain noise estimates
+    range_sample_num : int
+        number of samples along ``echo_range`` to obtain noise estimates
+    """
+    pass
+
+
+def spike(ds: xr.Dataset, ping_num: int, spike_threshold: Union[int, float]) -> xr.Dataset:
+    """
+    Remove sipky noise from the Sv data.
+
+    Parameters
+    ----------
+    ds : xr.Dataset
+        dataset containing ``Sv`` and ``echo_range`` [m]
+    ping_num : int
+        number of pings to consider when detecting spikes
+    spike_threshold : {int, float}
+        threshold in dB for detecting spikes
+
+    Returns
+    -------
+    The input Sv dataset with spikes removed (set to NaN)
+    """
+    pass
