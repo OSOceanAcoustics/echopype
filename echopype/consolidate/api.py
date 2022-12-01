ba6a9784129ffc559a860056e840b0798b620a3a
b-reyes <reyesb123@gmail.com>
2022-11-30 17:04:15 -0800

    create general structure of add_splitbeam_angle function

--- echopype/consolidate/api.py
+++ echopype/consolidate/api.py
@@ -1,5 +1,5 @@
 import datetime
-from typing import Optional
+from typing import Optional, Tuple
 
 import numpy as np
 import xarray as xr
@@ -174,3 +174,168 @@ def add_location(ds: xr.Dataset, echodata: EchoData = None, nmea_sentence: Optio
     interp_ds["longitude"] = interp_ds["longitude"].assign_attrs({"history": history})
 
     return interp_ds.drop_vars("time1")
+
+
+def _check_splitbeam_angle_waveform_encode_mode(
+    echodata: EchoData, waveform_mode: str, encode_mode: str
+):
+    """
+    A function to make sure that the user has provided the correct
+    ``waveform_mode`` and ``encode_mode`` inputs to the function
+    ``add_splitbeam_angle`` based off of the supplied ``echodata`` object.
+
+    """
+
+    if waveform_mode not in ["CW", "BB"]:
+        raise RuntimeError("The input waveform_mode must be either 'CW' or 'BB'!")
+
+    if encode_mode not in ["complex", "power"]:
+        raise RuntimeError("The input encode_mode must be either 'complex' or 'power'!")
+
+    if echodata.sonar_model in ["EK60", "ES70"]:
+
+        if waveform_mode != "CW":
+            raise RuntimeError("Incorrect waveform_mode input provided!")
+
+        if encode_mode != "power":
+            raise RuntimeError("Incorrect encode_mode input provided!")
+
+        # TODO: make sure echodata['Sonar/Beam_group1'].beam_type only has 1's
+
+    # TODO: check waveform and encode mode for EK80
+
+
+def _get_ds_beam(echodata: EchoData):
+    """
+    Obtains the appropriate beam group used to compute the split-beam angle data.
+    """
+
+    if echodata.sonar_model in ["EK60", "ES70"]:
+        return echodata["Sonar/Beam_group1"]
+    else:
+        # TODO: determine ds_beam for EK80 sensors
+        return xr.Dataset()
+
+
+def _get_splitbeam_angle_power_CW(ds_beam: xr.Dataset) -> Tuple[xr.Dataset, xr.Dataset]:
+    """
+    Obtains the split-beam angle data from power encoded data with CW waveform.
+
+
+    Returns
+    -------
+    theta_fc: xr.Dataset
+        The calculated split-beam alongship angle
+    phi_fc: xr.Dataset
+        The calculated split-beam athwartship angle
+
+    Notes
+    -----
+    Can be used on both EK60 and EK80 data
+
+    physical_angle = ((raw_angle * 180 / 128) / sensitivity) - offset
+    """
+
+    # raw_angle scaling constant
+    conversion_const = 180 / 128
+
+    # TODO: maybe create a function to remove repetition below?
+
+    # obtain split-beam alongship angle
+    theta_fc = (
+        conversion_const * ds_beam["angle_alongship"] / ds_beam["angle_sensitivity_alongship"]
+        - ds_beam["angle_offset_alongship"]
+    )
+
+    # obtain split-beam athwartship angle
+    phi_fc = (
+        conversion_const * ds_beam["angle_athwartship"] / ds_beam["angle_sensitivity_athwartship"]
+        - ds_beam["angle_offset_athwartship"]
+    )
+
+    return theta_fc, phi_fc
+
+
+def _get_splitbeam_angle_complex_CW(ds_beam: xr.Dataset) -> Tuple[xr.Dataset, xr.Dataset]:
+    """
+    Obtains the split-beam angle data from complex encoded data with CW waveform.
+
+    Returns
+    -------
+    theta_fc: xr.Dataset
+        The calculated split-beam alongship angle
+    phi_fc: xr.Dataset
+        The calculated split-beam athwartship angle
+    """
+
+    return xr.Dataset(), xr.Dataset()
+
+
+def _get_splitbeam_angle_complex_BB(ds_beam: xr.Dataset) -> Tuple[xr.Dataset, xr.Dataset]:
+    """
+    Obtains the split-beam angle data from complex encoded data with BB waveform.
+
+    Returns
+    -------
+    theta_fc: xr.Dataset
+        The calculated split-beam alongship angle
+    phi_fc: xr.Dataset
+        The calculated split-beam athwartship angle
+    """
+
+    return xr.Dataset(), xr.Dataset()
+
+
+def add_splitbeam_angle(
+    ds: xr.Dataset, echodata: EchoData, waveform_mode: str, encode_mode: str
+) -> xr.Dataset:
+    """
+    Add split-beam (alongship/athwartship) angles into the Sv dataset.
+
+    Parameters
+    ----------
+    waveform_mode : {"CW", "BB"}
+        Type of transmit waveform.
+        Required only for data from the EK80 echosounder.
+
+        - `"CW"` for narrowband transmission,
+          returned echoes recorded either as complex or power/angle samples
+        - `"BB"` for broadband transmission,
+          returned echoes recorded as complex samples
+
+    encode_mode : {"complex", "power"}
+        Type of encoded return echo data.
+        Required only for data from the EK80 echosounder.
+
+        - `"complex"` for complex samples
+        - `"power"` for power/angle samples, only allowed when
+          the echosounder is configured for narrowband transmission
+    """
+
+    # ensure that echodata was produced by EK60 or EK80-like sensors
+    if echodata.sonar_model not in ["EK60", "ES70", "EK80", "ES80", "EA640"]:
+        raise RuntimeError(
+            "The sonar model that produced echodata does not have split-beam "
+            "transducers, split-beam angles cannot be added to ds!"
+        )
+
+    # check that the appropriate waveform and encode mode have been given
+    _check_splitbeam_angle_waveform_encode_mode(echodata, waveform_mode, encode_mode)
+
+    # get the appropriate ds_beam Dataset
+    ds_beam = _get_ds_beam(echodata)
+
+    # obtain split-beam angles from
+    if (waveform_mode == "CW") and (encode_mode == "power"):
+        theta_fc, phi_fc = _get_splitbeam_angle_power_CW(ds_beam=ds_beam)
+    elif (waveform_mode == "CW") and (encode_mode == "complex"):
+        theta_fc, phi_fc = _get_splitbeam_angle_complex_CW(ds_beam=ds_beam)
+    elif (waveform_mode == "BB") and (encode_mode == "complex"):
+        theta_fc, phi_fc = _get_splitbeam_angle_complex_BB(ds_beam=ds_beam)
+    else:
+        raise RuntimeError(
+            f"Unable to compute split-beam angle data for "
+            f"waveform_mode = {waveform_mode} and encode_mode = {encode_mode}!"
+        )
+
+    # TODO: create function that adds theta_fc and phi_fc to ds input
