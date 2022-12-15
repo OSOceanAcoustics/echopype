947ed1dfb24e4e6e32d2c89324e19903bea87105
b-reyes <reyesb123@gmail.com>
2022-12-15 13:55:01 -0800

    rename validate_source_Sv to validate_source_ds and remove Sv references throughout the function

--- echopype/utils/data_proc_lvls.py
+++ echopype/utils/data_proc_lvls.py
@@ -10,29 +10,29 @@ import xarray as xr
 from echopype.utils.io import check_file_existence, get_file_format, validate_output_path
 
 
-def validate_source_Sv(
-    source_Sv: Union[xr.Dataset, str, pathlib.Path], storage_options: Optional[dict]
+def validate_source_ds(
+    source_ds: Union[xr.Dataset, str, pathlib.Path], storage_options: Optional[dict]
 ) -> Tuple[Union[xr.Dataset, str, xr.DataArray], Optional[str]]:
     """
-    This function ensures that ``source_Sv`` is of the correct
-    type and validates the path of ``source_Sv``, if it is provided.
+    This function ensures that ``source_ds`` is of the correct
+    type and validates the path of ``source_ds``, if it is provided.
 
     Parameters
     ----------
-    source_Sv: xr.Dataset or str or pathlib.Path
-        If a Dataset this value contains the Sv data,
-        else it specifies the path to a zarr or netcdf file
+    source_ds: xr.Dataset or str or pathlib.Path
+        A source that points to a Dataset. If the input is a path, it specifies
+        the path to a zarr or netcdf file.
     storage_options: dict, optional
         Any additional parameters for the storage backend, corresponding to the
-        path provided for ``source_Sv``
+        path provided for ``source_ds``
 
     Returns
     -------
-    source_Sv: xr.Dataset or str
-        A Dataset that contains the Sv data (which will be the same as
-        the input) or a validated path to a zarr or netcdf file
+    source_ds: xr.Dataset or str
+        A Dataset which will be the same as the input ``source_ds`` or a validated
+        path to a zarr or netcdf file
     file_type: {"netcdf4", "zarr"}, optional
-        The file type of the input path if ``source_Sv`` is a path, otherwise ``None``
+        The file type of the input path if ``source_ds`` is a path, otherwise ``None``
     """
 
     # initialize file_type
@@ -42,24 +42,24 @@ def validate_source_Sv(
     if not isinstance(storage_options, dict):
         raise TypeError("storage_options must be a dict!")
 
-    # check that source_Sv is of the correct type, if it is a path validate
+    # check that source_ds is of the correct type, if it is a path validate
     # the path and open the dataset using xarray
-    if not isinstance(source_Sv, (xr.Dataset, str, pathlib.Path)):
-        raise TypeError("source_Sv must be a Dataset or str or pathlib.Path!")
-    elif isinstance(source_Sv, (str, pathlib.Path)):
+    if not isinstance(source_ds, (xr.Dataset, str, pathlib.Path)):
+        raise TypeError("source_ds must be a Dataset or str or pathlib.Path!")
+    elif isinstance(source_ds, (str, pathlib.Path)):
 
         # determine if we obtained a zarr or netcdf file
-        file_type = get_file_format(source_Sv)
+        file_type = get_file_format(source_ds)
 
-        # validate source_Sv if it is a path
-        source_Sv = validate_output_path(
-            source_file="blank",  # will be unused since source_Sv cannot be none
+        # validate source_ds if it is a path
+        source_ds = validate_output_path(
+            source_file="blank",  # will be unused since source_ds cannot be none
             engine=file_type,
             output_storage_options=storage_options,
-            save_path=source_Sv,
+            save_path=source_ds,
         )
 
         # check that the path exists
-        check_file_existence(file_path=source_Sv, storage_options=storage_options)
+        check_file_existence(file_path=source_ds, storage_options=storage_options)
 
-    return source_Sv, file_type
+    return source_ds, file_type
