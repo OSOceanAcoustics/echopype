6334a2d583c518f57100b285d5d5fa065c511293
b-reyes <reyesb123@gmail.com>
2022-12-16 11:07:51 -0800

    create first simple test for apply_mask with mock Dataset creation

--- echopype/tests/mask/test_mask.py
+++ echopype/tests/mask/test_mask.py
@@ -4,6 +4,7 @@ import numpy as np
 import xarray as xr
 
 import echopype as ep
+import echopype.mask
 from echopype.mask.api import _check_source_Sv_freq_diff
 
 from typing import List, Union
@@ -84,6 +85,49 @@ def get_mock_freq_diff_data(n: int, n_chan_freq: int, add_chan: bool,
     mock_Sv_ds = xr.Dataset(data_vars=data_vars)
 
     return mock_Sv_ds
+
+
+def get_mock_source_ds_apply_mask(n: int, n_chan: int) -> xr.Dataset:
+    """
+    Constructs a mock ``source_ds`` Dataset input for the
+    ``apply_mask`` function.
+
+    Parameters
+    ----------
+    n: int
+        The number of rows (``x``) and columns (``y``) of
+        each channel matrix
+    n_chan: int
+        Determines the size of the ``channel`` coordinate
+
+    Returns
+    -------
+    xr.Dataset
+        A Dataset with coordinates ``channel, x, y`` and
+        variables ``var1, var2`` (with the created coordinates). The
+        variables will contain square matrices of ones for each ``channel``.
+    """
+
+    # construct channel values
+    chan_vals = ['chan' + str(i) for i in range(1, n_chan + 1)]
+
+    # construct mock variable data for each channel
+    mock_var_data = [np.ones((n, n)) for i in range(n_chan)]
+
+    # create mock var1 and var2 DataArrays
+    mock_var1_da = xr.DataArray(data=np.stack(mock_var_data),
+                                coords={"channel": chan_vals, "x": np.arange(n),
+                                        "y": np.arange(n)},
+                                attrs={"long_name": "variable 1"})
+    mock_var2_da = xr.DataArray(data=np.stack(mock_var_data),
+                                coords={"channel": chan_vals, "x": np.arange(n),
+                                        "y": np.arange(n)},
+                                attrs={"long_name": "variable 2"})
+
+    # create mock Dataset
+    mock_ds = xr.Dataset(data_vars={"var1": mock_var1_da, "var2": mock_var2_da})
+
+    return mock_ds
 
 
 @pytest.mark.parametrize(
@@ -217,6 +261,47 @@ def test_frequency_differencing(n: int, n_chan_freq: int,
 
     # test that the output DataArray is correctly names
     assert out.name == "mask"
+
+
+@pytest.mark.parametrize(
+    ("n", "n_chan", "var_name", "mask", "var_masked_truth"),
+    [
+        (5, 1, "var1", np.identity(5), np.nan*np.ones((5, 5))),
+    ],
+    ids=["test1"]
+)
+def test_apply_mask(n: int, n_chan: int, var_name: str,
+                    mask: Union[Union[np.ndarray], List[np.ndarray]],
+                    var_masked_truth: np.ndarray):
+
+    # TODO: test different fill_value
+    fill_value = np.nan
+
+    mock_ds = get_mock_source_ds_apply_mask(n, n_chan)
+
+    # TODO: create test for list of masks
+
+    mask = xr.DataArray(data=np.stack([mask for i in range(n_chan)]),
+                        coords=mock_ds.coords)
+
+    np.fill_diagonal(var_masked_truth, 1.0)
+
+    var_masked_truth = xr.DataArray(data=np.stack([var_masked_truth for i in range(n_chan)]),
+                                    coords=mock_ds[var_name].coords, attrs=mock_ds[var_name].attrs)
+    var_masked_truth.name = mock_ds[var_name].name
+
+    masked_ds = echopype.mask.apply_mask(source_ds=mock_ds, var_name=var_name, mask=mask,
+                                         fill_value=fill_value, storage_options_ds={},
+                                         storage_options_mask={})
+
+    # TODO: check that masked_ds has the same structure as mock_ds
+
+    # TODO: check that masked_ds[var_name] == var_masked_truth
+
+    print("hello")
+
+    assert masked_ds[var_name].identical(var_masked_truth)
+
 
 
 
