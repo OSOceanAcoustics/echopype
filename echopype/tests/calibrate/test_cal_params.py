13a38c13b4ca182b33655b251559921635b496c6
leewujung <leewujung@gmail.com>
2023-03-06 07:53:22 -0800

    tidy up comments

--- echopype/tests/calibrate/test_cal_params.py
+++ echopype/tests/calibrate/test_cal_params.py
@@ -16,16 +16,8 @@ def test_param_dict2da():
     pass
 
 
-# sonar_type: EK or AZFP
-# input dict:
-#   - contain extra param: should come out with only those defined in CAL_PARAMS
-#   - contain missing param: missing ones (wrt CAL_PARAMS) should be empty
 # input params:
 #   - scalar: no change -- THIS NEEDS EXTRA WORK TO ORGANIZE INPUT SCALAR/LIST TO XR.DATAARRAY
-#   - xr.DataArray without channel coorindate: fail with value error
-#   - xr.DataArray with channel cooridinate not identical to argin channel: fail with value error
-# argin channel:
-#   - is not a list nor an xr.DataArray: fail with value error
 @pytest.mark.parametrize(
     ("sonar_type", "user_dict", "channel", "out_dict"),
     [
@@ -91,22 +83,21 @@ def test_sanitize_user_cal_dict(sonar_type, user_dict, channel, out_dict):
             assert p_val == out_dict[p_name]
 
 
-# da_param: None
-#   - alternative is const: output is xr.DataArray with all const
-#   - alternative is xr.DataArray: output selected with the right channel
 # THIS CASE DOES NOT EXIST: da_param: xr.DataArray without freq-dependent values/coorindates
 #   - output selected with right channels
-# da_param: xr.DataArray with freq-dependent values/coordinates
-#   - output interpolated with the right values
 @pytest.mark.parametrize(
     ("da_param", "alternative", "da_output"),
     [
+        # da_param: alternative is const: output is xr.DataArray with all const
         (None, 1, xr.DataArray([1, 1], dims=["channel"], coords={"channel": ["chA", "chB"]})),
+        # da_param: alternative is xr.DataArray: output selected with the right channel
         (
             None,
             xr.DataArray([1, 1, 2], dims=["channel"], coords={"channel": ["chA", "chB", "chC"]}),
             xr.DataArray([1, 1], dims=["channel"], coords={"channel": ["chA", "chB"]})
         ),
+        # da_param: xr.DataArray with freq-dependent values/coordinates
+        #   - output should be interpolated with the right values
         (
             xr.DataArray(
                 np.array([[1, 2, 3, np.nan, np.nan, np.nan],
