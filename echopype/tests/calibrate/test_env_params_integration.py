634ea095bc5799e5481d928efb2fbe321f3a3601
Wu-Jung Lee <leewujung@gmail.com>
2023-03-14 12:40:27 -0700

    add a narrow integration test to ensure env_params intake for CalibrateAZFP

--- echopype/tests/calibrate/test_env_params_integration.py
+++ echopype/tests/calibrate/test_env_params_integration.py
@@ -22,7 +22,29 @@ def ek80_cal_path(test_path):
 
 
 def test_env_params_intake_AZFP(azfp_path):
-    pass
+    """
+    Test env param intake for AZFP calibration.
+    """
+    azfp_01a_path = str(azfp_path.joinpath('17082117.01A'))
+    azfp_xml_path = str(azfp_path.joinpath('17041823.XML'))
+    ed = ep.open_raw(azfp_01a_path, sonar_model='AZFP', xml_path=azfp_xml_path)
+
+    # Assemble external env param
+    env_ext = {"salinity": 30, "pressure": 100}
+
+    # Manually go through env params intake
+    env_params_manual = ep.calibrate.env_params.get_env_params_AZFP(echodata=ed, user_env_dict=env_ext)
+    for p in env_params_manual.keys():
+        env_params_manual[p] = ep.calibrate.env_params.harmonize_env_param_time(
+            env_params_manual[p], ping_time=ed["Sonar/Beam_group1"]["ping_time"]
+        )
+    env_params_manual["sound_speed"].name = "sound_speed"
+    env_params_manual["sound_absorption"].name = "sound_absorption"
+
+    # Check against the final env params in the calibration output
+    ds_Sv = ep.calibrate.compute_Sv(ed, env_params=env_ext)
+    assert ds_Sv["sound_speed"].identical(env_params_manual["sound_speed"])
+    assert ds_Sv["sound_absorption"].identical(env_params_manual["sound_absorption"])
 
 
 def test_env_params_intake_EK60(ek60_path):
@@ -34,7 +56,7 @@ def test_env_params_intake_EK60(ek60_path):
     # Assemble external env param
     env_ext = {"temperature": 10, "salinity": 30, "pressure": 100}
 
-    # Manually go through cal params intake
+    # Manually go through env params intake
     env_params_manual = ep.calibrate.env_params.get_env_params_EK60(echodata=ed, user_env_dict=env_ext)
     for p in env_params_manual.keys():
         env_params_manual[p] = ep.calibrate.env_params.harmonize_env_param_time(
