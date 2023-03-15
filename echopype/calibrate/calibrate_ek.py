c1e244612b03f50ab5631f289113343c63a9d2a8
Emilio Mayorga <emiliomayorga@gmail.com>
2023-03-15 14:05:33 -0700

    For log10 calibration warning fix (previous commit), replace with np.nan instead of 0

--- echopype/calibrate/calibrate_ek.py
+++ echopype/calibrate/calibrate_ek.py
@@ -14,9 +14,6 @@ from .range import compute_range_EK, range_mod_TVG_EK
 
 logger = _init_logger(__name__)
 
-# Small positive number, to use in place of zero
-EPS = 1e-5
-
 
 class CalibrateEK(CalibrateBase):
     def __init__(self, echodata: EchoData, env_params, cal_params):
@@ -68,7 +65,7 @@ class CalibrateEK(CalibrateBase):
         tvg_mod_range = range_mod_TVG_EK(
             self.echodata, self.ed_group, self.range_meter, sound_speed
         )
-        tvg_mod_range = tvg_mod_range.where(tvg_mod_range > 0, EPS)
+        tvg_mod_range = tvg_mod_range.where(tvg_mod_range > 0, np.nan)
 
         spreading_loss = 20 * np.log10(tvg_mod_range)
         absorption_loss = 2 * absorption * tvg_mod_range
@@ -380,14 +377,14 @@ class CalibrateEK80(CalibrateEK):
 
         # TVG compensation with modified range
         tvg_mod_range = range_mod_TVG_EK(self.echodata, self.ed_group, range_meter, sound_speed)
-        tvg_mod_range = tvg_mod_range.where(tvg_mod_range > 0, EPS)
+        tvg_mod_range = tvg_mod_range.where(tvg_mod_range > 0, np.nan)
 
         spreading_loss = 20 * np.log10(tvg_mod_range)
         absorption_loss = 2 * absorption * tvg_mod_range
 
         # Get power from complex samples
         prx = self._get_power_from_complex(beam=beam, chirp=tx, z_et=z_et, z_er=z_er)
-        prx = prx.where(prx > 0, EPS)
+        prx = prx.where(prx > 0, np.nan)
 
         # Compute based on cal_type
         if cal_type == "Sv":
