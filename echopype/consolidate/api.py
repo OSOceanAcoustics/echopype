a10731ea1544ab6297fb20afc191e66c587276ba
Emilio Mayorga <emiliomayorga@gmail.com>
2023-03-21 01:15:34 -0700

    Rename coord_var to position_var, for clarity

--- echopype/consolidate/api.py
+++ echopype/consolidate/api.py
@@ -153,23 +153,23 @@ def add_location(ds: xr.Dataset, echodata: EchoData = None, nmea_sentence: Optio
     def sel_interp(var):
         # NMEA sentence selection
         if nmea_sentence:
-            coord_var = echodata["Platform"][var][
+            position_var = echodata["Platform"][var][
                 echodata["Platform"]["sentence_type"] == nmea_sentence
             ]
         else:
-            coord_var = echodata["Platform"][var]
+            position_var = echodata["Platform"][var]
 
-        if len(coord_var) == 1:
+        if len(position_var) == 1:
             # Propagate single, fixed-location coordinate
             return xr.DataArray(
-                data=coord_var.values[0] * np.ones(len(ds["ping_time"]), dtype=np.float64),
+                data=position_var.values[0] * np.ones(len(ds["ping_time"]), dtype=np.float64),
                 dims=["ping_time"],
-                attrs=coord_var.attrs,
+                attrs=position_var.attrs,
             )
         else:
             # Interpolation. time1 is always associated with location data
             # The first value ends up being always nan. Use "extrapolate"
-            return coord_var.interp(time1=ds["ping_time"], kwargs={"fill_value": "extrapolate"})
+            return position_var.interp(time1=ds["ping_time"], kwargs={"fill_value": "extrapolate"})
 
     if "longitude" not in echodata["Platform"] or echodata["Platform"]["longitude"].isnull().all():
         raise ValueError("Coordinate variables not present or all nan")
