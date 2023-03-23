2ae3a278c5c095cc1cfcdacba08cd44c9e820888
Wu-Jung Lee <leewujung@gmail.com>
2023-03-23 11:19:35 -0700

    Allow using ECS for calibrating Simrad echosounders (#996)
    * add Frequency to be the store set of params, update test
    
    * add functions for converting parsed/organized dict to xr.Datasets and check frequency order
    
    * fix bug in setting values for each source dict
    
    * add check_source_channel_order, convert kHz to Hz
    
    * add test_check_source_channel_order
    
    * add test for ECS-derived datasets
    
    * rename ecs_parser.py to ecs.py
    
    * add ecs intake at Calibrate* init, run pre-commit
    
    * sketch out ecs param intake in CarlibateBase.__init__
    
    * add all ECS variables
    
    * add test_convert_ecs_template_ek60, fix existing tests
    
    * incorporate ecs_file intake to CalibrateEK60
    
    * add test_path["ECS"] to fixture
    
    * fix missing pieces in api::_compute_cal for ecs intake, add test_path["ECS"] to fixture
    
    * complete ecs intake for CalibrateEK60, add integration test test_ecs_intake_ek60
    
    * add integration test test_ecs_intake_ek60
    
    * change list to List in typing
    
    * fix automerge errors
    
    * revise matcher patterns to fit EK80 ECS template style, fix bug in converting str to number/array/bool multiple times
    
    * revise ecs_ev2ep to allow some channels missing some params and add EK80-specific vars
    
    * accommondate 3rd output from ecs_ev2ep in CalibrateEK60 and test
    
    * fix tests after updating test files
    
    * revise variable names in organizerd dataset cal_params_BB, remove unused import
    
    * change order of ecs_ev2ep output
    
    * fix uwa.calc_absorption problem with accepting temperature as a data array
    
    * add EK80 intake draft, add test_ecs_intake_ek80_CW_power
    
    * add CalibrateEK80._assimilate_ecs_cal_params() to assimilate narrowband and broadband cal params from ECS
    
    * add CalibrateEK80._scale_ecs_cal_params_NB, add test_ecs_intake_ek80_BB_complex
    
    * remove comment that is no longer relevant
    
    * Tweak warning message, fix typo in comments
    
    * fix long line

--- echopype/calibrate/calibrate_ek.py
+++ echopype/calibrate/calibrate_ek.py
@@ -6,8 +6,9 @@ import xarray as xr
 from ..echodata import EchoData
 from ..echodata.simrad import retrieve_correct_beam_group
 from ..utils.log import _init_logger
-from .cal_params import get_cal_params_EK
+from .cal_params import _get_interp_da, get_cal_params_EK
 from .calibrate_base import CalibrateBase
+from .ecs import conform_channel_order, ecs_ds2dict, ecs_ev2ep
 from .ek80_complex import compress_pulse, get_filter_coeff, get_tau_effective, get_transmit_signal
 from .env_params import get_env_params_EK
 from .range import compute_range_EK, range_mod_TVG_EK
@@ -16,8 +17,10 @@ logger = _init_logger(__name__)
 
 
 class CalibrateEK(CalibrateBase):
-    def __init__(self, echodata: EchoData, env_params, cal_params):
-        super().__init__(echodata, env_params, cal_params)
+    def __init__(self, echodata: EchoData, env_params, cal_params, ecs_file, **kwargs):
+        super().__init__(echodata, env_params, cal_params, ecs_file)
+
+        self.ed_beam_group = None  # will be assigned in child class
 
         self.ed_beam_group = None  # will be assigned in child class
 
@@ -127,11 +130,13 @@ class CalibrateEK(CalibrateBase):
 
 
 class CalibrateEK60(CalibrateEK):
-    def __init__(self, echodata, env_params, cal_params, **kwargs):
-        super().__init__(echodata, env_params, cal_params)
+    def __init__(self, echodata: EchoData, env_params, cal_params, ecs_file, **kwargs):
+        super().__init__(echodata, env_params, cal_params, ecs_file)
 
         # Set sonar_type and waveform/encode mode
         self.sonar_type = "EK60"
+
+        # Set cal type
         self.waveform_mode = "CW"
         self.encode_mode = "power"
 
@@ -140,23 +145,31 @@ class CalibrateEK60(CalibrateEK):
             echodata=self.echodata, waveform_mode=self.waveform_mode, encode_mode=self.encode_mode
         )
 
-        # Get env_params
+        # Set the channels to calibrate
+        # For EK60 this is all channels
+        self.chan_sel = self.echodata[self.ed_beam_group]["channel"]
+        beam = self.echodata[self.ed_beam_group]
+
+        # Convert env_params and cal_params if self.ecs_file exists
+        # Note a warning if thrown out in CalibrateBase.__init__
+        # to let user know cal_params and env_params are ignored if ecs_file is provided
+        if self.ecs_file is not None:  # also means self.ecs_dict != {}
+            ds_env_tmp, ds_cal_tmp, _ = ecs_ev2ep(self.ecs_dict, "EK60")
+            self.cal_params = ecs_ds2dict(
+                conform_channel_order(ds_cal_tmp, beam["frequency_nominal"])
+            )
+            self.env_params = ecs_ds2dict(
+                conform_channel_order(ds_env_tmp, beam["frequency_nominal"])
+            )
+
+        # Regardless of the source cal and env params,
+        # go through the same sanitization and organization process
         self.env_params = get_env_params_EK(
             sonar_type=self.sonar_type,
             beam=self.echodata[self.ed_beam_group],
             env=self.echodata["Environment"],
             user_dict=self.env_params,
         )
-
-        # Compute range
-        self.compute_echo_range()
-
-        # Set the channels to calibrate
-        # For EK60 this is all channels
-        self.chan_sel = self.echodata[self.ed_beam_group]["channel"]
-        beam = self.echodata[self.ed_beam_group]
-
-        # Get cal_params
         self.cal_params = get_cal_params_EK(
             waveform_mode=self.waveform_mode,
             freq_center=beam["frequency_nominal"],
@@ -165,6 +178,9 @@ class CalibrateEK60(CalibrateEK):
             user_dict=self.cal_params,
             sonar_type=self.sonar_type,
         )
+
+        # Compute range
+        self.compute_echo_range()
 
     def compute_Sv(self, **kwargs):
         return self._cal_power_samples(cal_type="Sv")
@@ -190,8 +206,17 @@ class CalibrateEK80(CalibrateEK):
         "WBT LF": 93750,
     }
 
-    def __init__(self, echodata, env_params, cal_params, waveform_mode, encode_mode):
-        super().__init__(echodata, env_params, cal_params)
+    def __init__(
+        self,
+        echodata: EchoData,
+        env_params,
+        cal_params,
+        waveform_mode,
+        encode_mode,
+        ecs_file=None,
+        **kwargs,
+    ):
+        super().__init__(echodata, env_params, cal_params, ecs_file)
 
         # Set sonar_type
         self.sonar_type = "EK80"
@@ -230,6 +255,32 @@ class CalibrateEK80(CalibrateEK):
             # use nominal channel frequency for CW pulse
             self.freq_center = beam["frequency_nominal"].sel(channel=self.chan_sel)
 
+        # Convert env_params and cal_params if self.ecs_file exists
+        # Note a warning if thrown out in CalibrateBase.__init__
+        # to let user know cal_params and env_params are ignored if ecs_file is provided
+        if self.ecs_file is not None:  # also means self.ecs_dict != {}
+            ds_env, ds_cal_NB, ds_cal_BB = ecs_ev2ep(self.ecs_dict, "EK80")
+            self.env_params = ecs_ds2dict(
+                conform_channel_order(ds_env, beam["frequency_nominal"].sel(channel=self.chan_sel))
+            )
+            ds_cal_BB = conform_channel_order(
+                ds_cal_BB, beam["frequency_nominal"].sel(channel=self.chan_sel)
+            )
+            ds_cal_NB = self._scale_ecs_cal_params_NB(
+                conform_channel_order(
+                    ds_cal_NB, beam["frequency_nominal"].sel(channel=self.chan_sel)
+                ),
+                beam,
+            )
+            cal_params_dict = ecs_ds2dict(ds_cal_NB)
+
+            if ds_cal_BB is not None:
+                # get_cal_params_EK fill in empty params at param level, not channel level,
+                # so need to do freq-dep interpolation here
+                self.cal_params = self._assimilate_ecs_cal_params(cal_params_dict, ds_cal_BB)
+            else:
+                self.cal_params = cal_params_dict
+
         # Get env_params: depends on waveform mode
         self.env_params = get_env_params_EK(
             sonar_type=self.sonar_type,
@@ -246,6 +297,7 @@ class CalibrateEK80(CalibrateEK):
             beam=beam,  # already subset above
             vend=self.echodata["Vendor_specific"].sel(channel=self.chan_sel),
             user_dict=self.cal_params,
+            sonar_type="EK80",
         )
 
         # Compute echo range in meters
@@ -274,6 +326,70 @@ class CalibrateEK80(CalibrateEK):
         else:
             # All channels are CW
             return {"BB": None, "CW": beam.channel}
+
+    def _scale_ecs_cal_params_NB(self, ds_cal_NB: xr.Dataset, beam: xr.Dataset) -> xr.Dataset:
+        """
+        Scale narrowband parameters based on center frequency of each ping
+        with respect to channel nominal frequency.
+        """
+        for p in ds_cal_NB:
+            if p in ["angle_sensitivity_alongship", "angle_sensitivity_athwartship"]:
+                ds_cal_NB[p] = ds_cal_NB[p] * self.freq_center / beam["frequency_nominal"]
+            elif p in ["beamwidth_alongship", "beamwidth_athwartship"]:
+                ds_cal_NB[p] = ds_cal_NB[p] * beam["frequency_nominal"] / self.freq_center
+            elif p == "equivalent_beam_angle":
+                ds_cal_NB[p] = ds_cal_NB[p] + 20 * np.log10(
+                    beam["frequency_nominal"] / self.freq_center
+                )
+        return ds_cal_NB
+
+    def _assimilate_ecs_cal_params(self, cal_params_dict: Dict, ds_cal_BB: xr.Dataset):
+        """
+        Combine narrowband and broadband parameters derived from ECS.
+        """
+        if ds_cal_BB is not None:
+            ds_cal_BB = ds_cal_BB.rename({"channel": "cal_channel_id"})
+
+            for p in ds_cal_BB.data_vars:
+                # For parameters where there is frequency-dependent values,
+                # the corresponding narrowband (CW mode) values should exist for all channels
+                if not np.all(
+                    [
+                        ch in cal_params_dict[p]["channel"].values
+                        for ch in ds_cal_BB["cal_channel_id"].values
+                    ]
+                ):
+                    raise ValueError(
+                        f"Narrowband (CW mode) parameter {p} should exist "
+                        "for all channels with frequency-dependent parameter values."
+                    )
+
+                # Assemble parameter data array with all channels
+                # Either interpolate or pull from narrowband input
+                if "ping_time" in cal_params_dict[p].coords:
+                    ds_cal_BB[p] = _get_interp_da(
+                        da_param=ds_cal_BB[p],  # freq-dep xr.DataArray
+                        freq_center=self.freq_center,
+                        alternative=cal_params_dict[p],
+                    )
+                else:
+                    ds_cal_BB[p] = _get_interp_da(
+                        da_param=ds_cal_BB[p],  # freq-dep xr.DataArray
+                        freq_center=self.freq_center,
+                        alternative=cal_params_dict[p].expand_dims(
+                            dim={"ping_time": self.freq_center["ping_time"].size}, axis=1
+                        ),
+                    )
+
+            # Keep only 'channel' and 'ping_time' coorindates
+            ds_cal_BB = ds_cal_BB.drop_dims(["cal_frequency", "cal_channel_id"])
+
+            # Substitute params in narrowband dict
+            return dict(cal_params_dict, **ecs_ds2dict(ds_cal_BB))
+
+        else:
+            # Do nothing if ds_cal_BB is None
+            return cal_params_dict
 
     def _get_power_from_complex(
         self,
