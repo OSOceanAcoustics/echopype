cb44dbce0f9f5a239315881c56ddca08431886f7
Wu-Jung Lee <leewujung@gmail.com>
2023-03-24 14:16:08 -0700

    Add `compute_NASC` (#1005)
    * add partial draft of compute_NASC
    
    * add brute force compute_NASC
    
    * add test_compute_NASC
    
    * fix test file path
    
    * add .copy for df ops
    
    * attach frequency_nominal and add depth to docstring
    
    * NASC: Fix conversion to dataset, and tests. Add variable attributes to NASC, depth, distance; first generalized attr handling scheme
    
    * NASC: Add ACDD time and lat-lon bounding box global attributes
    
    * add actual_range_round to round_digits
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Emilio Mayorga <emiliomayorga@gmail.com>

---
+++ echopype/tests/commongrid/test_nasc.py
@@ -0,0 +1,38 @@
+import pytest
+
+import numpy as np
+
+from echopype import open_raw
+from echopype.calibrate import compute_Sv
+from echopype.commongrid import compute_NASC
+from echopype.commongrid.nasc import (
+    get_distance_from_latlon,
+    get_depth_bin_info,
+    get_dist_bin_info,
+    get_distance_from_latlon,
+)
+from echopype.consolidate import add_location, add_depth
+
+
+@pytest.fixture
+def ek60_path(test_path):
+    return test_path['EK60']
+
+
+def test_compute_NASC(ek60_path):
+    raw_path = ek60_path / "ncei-wcsd/Summer2017-D20170620-T011027.raw"
+
+    ed = open_raw(raw_path, sonar_model="EK60")
+    ds_Sv = add_depth(add_location(compute_Sv(ed), ed, nmea_sentence="GGA"))
+    cell_dist = 0.1
+    cell_depth = 20
+    ds_NASC = compute_NASC(ds_Sv, cell_dist, cell_depth)
+
+    dist_nmi = get_distance_from_latlon(ds_Sv)
+
+    # Check dimensions
+    da_NASC = ds_NASC["NASC"]
+    assert da_NASC.dims == ("channel", "distance", "depth")
+    assert np.all(ds_NASC["channel"].values == ds_Sv["channel"].values)
+    assert da_NASC["depth"].size == np.ceil(ds_Sv["depth"].max() / cell_depth)
+    assert da_NASC["distance"].size == np.ceil(dist_nmi.max() / cell_dist)
