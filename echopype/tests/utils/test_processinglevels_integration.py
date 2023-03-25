418733a981e5ecd75a024c165f3569188aa4929c
Wu-Jung Lee <leewujung@gmail.com>
2023-03-24 18:20:26 -0700

    fix typo in test_raw_to_mvbs

--- echopype/tests/utils/test_processinglevels_integration.py
+++ echopype/tests/utils/test_processinglevels_integration.py
@@ -101,7 +101,7 @@ def test_raw_to_mvbs(
         freqAB = list(out_ds.frequency_nominal.values[:2])
         freqdiff_da = ep.mask.frequency_differencing(source_Sv=out_ds, freqAB=freqAB, operator=">", diff=5)
 
-        # Apply maks to multi-channel Sv
+        # Apply mask to multi-channel Sv
         return ep.mask.apply_mask(source_ds=out_ds, var_name="Sv", mask=freqdiff_da)
 
     # On Sv w/o noise removal
