443dacfd7ff93ae917324ef7b2463cbb4b67433d
Emilio Mayorga <emiliomayorga@gmail.com>
2023-07-21 19:29:17 -0700

    Remove deprecated preprocess subpackage [all tests ci] (#1082)
    * Remove deprecated preprocess subpackage
    
    * docs: replace references to preprocess with clean and commongrid
    
    * docs: add attention box about removal of preprocess subpackage

--- echopype/preprocess/__init__.py
+++
@@ -1,8 +0,0 @@
-from .api import compute_MVBS, compute_MVBS_index_binning, estimate_noise, remove_noise
-
-__all__ = [
-    "estimate_noise",
-    "remove_noise",
-    "compute_MVBS",
-    "compute_MVBS_index_binning",
-]
