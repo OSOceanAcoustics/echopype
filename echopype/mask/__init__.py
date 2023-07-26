e4bdb3b8ca35f9c22320ee59e733d2d0e0c362e5
Mihai Boldeanu <mishu_boldy@yahoo.com>
2023-07-26 13:44:11 +0300

    added the feature described in issue 1

--- echopype/mask/__init__.py
+++ echopype/mask/__init__.py
@@ -1,3 +1,3 @@
-from .api import apply_mask, frequency_differencing
+from .api import apply_mask, frequency_differencing, get_attenuation_mask
 
-__all__ = ["frequency_differencing", "apply_mask"]
+__all__ = ["frequency_differencing", "apply_mask","get_attenuation_mask"]
