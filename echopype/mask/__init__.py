8a9b984bd329196ef0b0c8609cd7ec6f438db1ad
Mihai Boldeanu <mishu_boldy@yahoo.com>
2023-08-01 12:32:01 +0300

    Code for seabed masking

--- echopype/mask/__init__.py
+++ echopype/mask/__init__.py
@@ -1,6 +1,5 @@
 from .api import apply_mask, frequency_differencing
-from .mask_attenuated_signal import get_attenuation_mask
-from .mask_transient_noise import get_transient_noise_mask
+from .api import get_seabed_mask
 from .mask_seabed import get_seabed_mask
 
-__all__ = ["frequency_differencing", "apply_mask","get_attenuation_mask","get_transient_noise_mask","get_seabed_mask"]
+__all__ = ["frequency_differencing", "apply_mask", "get_seabed_mask"]
