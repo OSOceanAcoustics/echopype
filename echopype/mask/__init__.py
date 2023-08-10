47605b783377ea706b8d9617f6caee7744910477
Andrei Rusu <andrei.rusu@beatfactor.net>
2023-08-10 15:13:35 +0200

    renamed utils

--- echopype/mask/__init__.py
+++ echopype/mask/__init__.py
@@ -1,5 +1,4 @@
 from .api import apply_mask, frequency_differencing
 from .mask_attenuated_signal import get_attenuation_mask
 
-__all__ = ["frequency_differencing", "apply_mask","get_attenuation_mask"]
-
+__all__ = ["frequency_differencing", "apply_mask", "get_attenuation_mask"]
