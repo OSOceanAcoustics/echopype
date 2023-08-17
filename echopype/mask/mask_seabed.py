689787f4a83251c27e7c03b3af3d4df7a4645042
Ruxandra Valcu <ruxandra.valcu@gmail.com>
2023-08-17 15:11:01 +0300

    Update echopype/mask/mask_seabed.py
    Co-authored-by: Raluca Simedroni <92971445+simedroniraluca@users.noreply.github.com>

--- echopype/mask/mask_seabed.py
+++ echopype/mask/mask_seabed.py
@@ -13,6 +13,39 @@ from skimage.morphology import erosion,dilation, square
 from skimage.measure import label
 
 from ..utils.mask_transformation import log, lin
+"""
+Algorithms for masking seabed.
+  These methods are based on:
+  
+    Copyright (c) 2020 Echopy
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+
+    __authors__ = ['Alejandro Ariza'   # wrote maxSv(), deltaSv(), blackwell(), 
+                                       # blackwell_mod(), aliased2seabed(), 
+                                       # seabed2aliased(), ariza(), experimental()
+  __authors__ = ['Mihai Boldeanu' 
+                  # adapted the mask seabed algorithms from the Echopy library 
+                  and implemented them for use with the Echopype library.
+                ]
+"""
 
 def get_seabed_mask(
     source_Sv: Union[xr.Dataset, str, pathlib.Path],
