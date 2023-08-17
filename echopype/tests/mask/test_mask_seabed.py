e70a8b6084f44ffc9356d08a5c75c63050bd7ee6
Ruxandra Valcu <ruxandra.valcu@gmail.com>
2023-08-17 12:31:57 +0300

    Update echopype/tests/mask/test_mask_seabed.py
    Co-authored-by: Raluca Simedroni <92971445+simedroniraluca@users.noreply.github.com>

--- echopype/tests/mask/test_mask_seabed.py
+++ echopype/tests/mask/test_mask_seabed.py
@@ -9,7 +9,7 @@ import xarray as xr
 
 
 """
-In order to run `test_mask_impulse_noise.py` with pytest, follow the steps below:
+In order to run `test_mask_seabed.py` with pytest, follow the steps below:
     1. Locate the `test_data` directory in the current project.
     2. Download the necessary test file `JR179-D20080410-T150637.raw` from the provided link.
         Link: [ ftp://ftp.bas.ac.uk/rapidkrill/ ]
