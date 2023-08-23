9131ca2de639f56bb758265c012ccdb6445f62ae
Raluca Simedroni <92971445+simedroniraluca@users.noreply.github.com>
2023-08-23 08:59:16 +0300

    Run black and isort on noise mask files (except api.py)

--- echopype/mask/__init__.py
+++ echopype/mask/__init__.py
@@ -5,4 +5,12 @@ from .mask_range import get_range_mask
 from .mask_seabed import get_seabed_mask
 from .mask_transient_noise import get_transient_noise_mask
 
-__all__ = ["frequency_differencing", "apply_mask", "get_attenuation_mask", "get_impulse_noise_mask", "get_range_mask", "get_seabed_mask", "get_transient_noise_mask"]
+__all__ = [
+    "frequency_differencing",
+    "apply_mask",
+    "get_attenuation_mask",
+    "get_impulse_noise_mask",
+    "get_range_mask",
+    "get_seabed_mask",
+    "get_transient_noise_mask",
+]
