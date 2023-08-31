828ab6d24ccd7fa30044722f44f6447a10adc926
Landung 'Don' Setiawan <landungs@uw.edu>
2023-08-31 11:05:28 -0700

    test: Extract api test and add markings
    Extracted fixtures to conftest.py for commongrid.
    Additionally, clean up unused functions and mark
    tests b/w unit and integration. Added a new test module
    called 'test_api.py' for 'commongrid.api'.

---
+++ echopype/tests/commongrid/conftest.py
@@ -0,0 +1,219 @@
+import pytest
+
+import xarray as xr
+import numpy as np
+import pandas as pd
+
+
+@pytest.fixture
+def random_number_generator():
+    """Random number generator for tests"""
+    return np.random.default_rng()
+
+@pytest.fixture(
+    params=[
+        (
+            ("EK60", "ncei-wcsd", "Summer2017-D20170719-T211347.raw"),
+            "EK60",
+            None,
+            {},
+        ),
+        (
+            ("EK80_NEW", "echopype-test-D20211004-T235930.raw"),
+            "EK80",
+            None,
+            {'waveform_mode': 'BB', 'encode_mode': 'complex'},
+        ),
+        (
+            ("EK80_NEW", "D20211004-T233354.raw"),
+            "EK80",
+            None,
+            {'waveform_mode': 'CW', 'encode_mode': 'power'},
+        ),
+        (
+            ("EK80_NEW", "D20211004-T233115.raw"),
+            "EK80",
+            None,
+            {'waveform_mode': 'CW', 'encode_mode': 'complex'},
+        ),
+        (("ES70", "D20151202-T020259.raw"), "ES70", None, {}),
+        (("AZFP", "17082117.01A"), "AZFP", ("AZFP", "17041823.XML"), {}),
+        (
+            ("AD2CP", "raw", "090", "rawtest.090.00001.ad2cp"),
+            "AD2CP",
+            None,
+            {},
+        ),
+    ],
+    ids=[
+        "ek60_cw_power",
+        "ek80_bb_complex",
+        "ek80_cw_power",
+        "ek80_cw_complex",
+        "es70",
+        "azfp",
+        "ad2cp",
+    ],
+)
+def test_data_samples(request, test_path):
+    (
+        filepath,
+        sonar_model,
+        azfp_xml_path,
+        range_kwargs,
+    ) = request.param
+    if sonar_model.lower() in ['es70', 'ad2cp']:
+        pytest.xfail(
+            reason="Not supported at the moment",
+        )
+    path_model, *paths = filepath
+    filepath = test_path[path_model].joinpath(*paths)
+
+    if azfp_xml_path is not None:
+        path_model, *paths = azfp_xml_path
+        azfp_xml_path = test_path[path_model].joinpath(*paths)
+    return (
+        filepath,
+        sonar_model,
+        azfp_xml_path,
+        range_kwargs,
+    )
+    
+@pytest.fixture
+def regular_data_params():
+    return {
+        "channel_len": 4,
+        "depth_len": 4000,
+        "ping_time_len": 100,
+        "ping_time_jitter_max_ms": 0,
+    }
+
+
+@pytest.fixture
+def ds_Sv_er_regular(regular_data_params, random_number_generator):
+    return _gen_Sv_er_regular(
+        **regular_data_params,
+        random_number_generator=random_number_generator,
+    )
+
+
+@pytest.fixture
+def ds_Sv_er_irregular(random_number_generator):
+    depth_interval = [0.5, 0.32, 0.13]
+    depth_ping_time_len = [100, 300, 200]
+    ping_time_len = 600
+    ping_time_interval = "0.3S"
+    return _gen_Sv_er_irregular(
+        depth_interval=depth_interval,
+        depth_ping_time_len=depth_ping_time_len,
+        ping_time_len=ping_time_len,
+        ping_time_interval=ping_time_interval,
+        ping_time_jitter_max_ms=0,
+        random_number_generator=random_number_generator,
+    )
+
+
+# Helper functions to generate mock Sv dataset
+def _gen_ping_time(ping_time_len, ping_time_interval, ping_time_jitter_max_ms=0):
+    ping_time = pd.date_range("2018-07-01", periods=ping_time_len, freq=ping_time_interval)
+    if ping_time_jitter_max_ms != 0:  # if to add jitter
+        jitter = (
+            np.random.randint(ping_time_jitter_max_ms, size=ping_time_len) / 1000
+        )  # convert to seconds
+        ping_time = pd.to_datetime(ping_time.astype(int) / 1e9 + jitter, unit="s")
+    return ping_time
+
+
+def _gen_Sv_er_regular(
+    channel_len=2,
+    depth_len=100,
+    depth_interval=0.5,
+    ping_time_len=600,
+    ping_time_interval="0.3S",
+    ping_time_jitter_max_ms=0,
+    random_number_generator=None,
+):
+    """
+    Generate a Sv dataset with uniform echo_range across all ping_time.
+
+    ping_time_jitter_max_ms controlled jitter in milliseconds in ping_time.
+    """
+
+    if random_number_generator is None:
+        random_number_generator = np.random.default_rng()
+
+    # regular echo_range
+    echo_range = np.array([[np.arange(depth_len)] * ping_time_len] * channel_len) * depth_interval
+
+    # generate dataset
+    ds_Sv = xr.Dataset(
+        data_vars={
+            "Sv": (
+                ["channel", "ping_time", "range_sample"],
+                random_number_generator.random(size=(channel_len, ping_time_len, depth_len)),
+            ),
+            "echo_range": (["channel", "ping_time", "range_sample"], echo_range),
+            "frequency_nominal": (["channel"], np.arange(channel_len)),
+        },
+        coords={
+            "channel": [f"ch_{ch}" for ch in range(channel_len)],
+            "ping_time": _gen_ping_time(ping_time_len, ping_time_interval, ping_time_jitter_max_ms),
+            "range_sample": np.arange(depth_len),
+        },
+    )
+
+    return ds_Sv
+
+
+def _gen_Sv_er_irregular(
+    channel_len=2,
+    depth_len=100,
+    depth_interval=[0.5, 0.32, 0.13],
+    depth_ping_time_len=[100, 300, 200],
+    ping_time_len=600,
+    ping_time_interval="0.3S",
+    ping_time_jitter_max_ms=0,
+    random_number_generator=None,
+):
+    """
+    Generate a Sv dataset with uniform echo_range across all ping_time.
+
+    ping_time_jitter_max_ms controlled jitter in milliseconds in ping_time.
+    """
+    if random_number_generator is None:
+        random_number_generator = np.random.default_rng()
+
+    # check input
+    if len(depth_interval) != len(depth_ping_time_len):
+        raise ValueError("The number of depth_interval and depth_ping_time_len must be equal!")
+
+    if ping_time_len != np.array(depth_ping_time_len).sum():
+        raise ValueError("The number of total pings does not match!")
+
+    # irregular echo_range
+    echo_range_list = []
+    for d, dp in zip(depth_interval, depth_ping_time_len):
+        echo_range_list.append(np.array([[np.arange(depth_len)] * dp] * channel_len) * d)
+    echo_range = np.hstack(echo_range_list)
+
+    # generate dataset
+    ds_Sv = xr.Dataset(
+        data_vars={
+            "Sv": (
+                ["channel", "ping_time", "range_sample"],
+                random_number_generator.random(size=(channel_len, ping_time_len, depth_len)),
+            ),
+            "echo_range": (["channel", "ping_time", "range_sample"], echo_range),
+            "frequency_nominal": (["channel"], np.arange(channel_len)),
+        },
+        coords={
+            "channel": [f"ch_{ch}" for ch in range(channel_len)],
+            "ping_time": _gen_ping_time(ping_time_len, ping_time_interval, ping_time_jitter_max_ms),
+            "range_sample": np.arange(depth_len),
+        },
+    )
+
+    return ds_Sv
+
+
+# End helper functions
---
+++ echopype/tests/commongrid/test_api.py
@@ -0,0 +1,142 @@
+import pytest
+import echopype as ep
+import numpy as np
+import pandas as pd
+
+# NASC Tests
+@pytest.mark.integration
+@pytest.mark.skip(reason="NASC is not implemented yet")
+def test_compute_NASC(test_data_samples):
+    pass
+
+# MVBS Tests
+@pytest.mark.integration
+def test_compute_MVBS_index_binning(ds_Sv_er_regular, regular_data_params):
+    """Test compute_MVBS_index_binning on mock data"""
+
+    ping_num = 3  # number of pings to average over
+    range_sample_num = 7  # number of range_samples to average over
+    nchan = regular_data_params["channel_len"]
+    npings = regular_data_params["ping_time_len"]
+    nrange_samples = regular_data_params["depth_len"]
+
+    # Binned MVBS test
+    ds_MVBS = ep.commongrid.compute_MVBS_index_binning(
+        ds_Sv_er_regular, range_sample_num=range_sample_num, ping_num=ping_num
+    )
+
+    # Shape test
+    data_binned_shape = np.ceil(
+        (nchan, npings / ping_num, nrange_samples / range_sample_num)
+    ).astype(int)
+    assert np.all(ds_MVBS.Sv.shape == data_binned_shape)
+
+    # Expected values compute
+    # average should be done in linear domain
+    da_sv = 10 ** (ds_Sv_er_regular["Sv"] / 10)
+    expected = 10 * np.log10(
+        da_sv
+        .coarsen(ping_time=ping_num, range_sample=range_sample_num, boundary="pad")
+        .mean(skipna=True)
+    )
+
+    # Test all values in MVBS
+    assert np.array_equal(ds_MVBS.Sv.data, expected.data)
+    
+@pytest.mark.unit
+@pytest.mark.parametrize("range_var", ["my_range", "echo_range", "depth"])
+def test_compute_MVBS_invalid_range_var(ds_Sv_er_regular, range_var):
+    """Test compute MVBS range_var on mock data"""
+
+    if range_var == "my_range":
+        with pytest.raises(
+            ValueError,
+            match="range_var must be one of 'echo_range' or 'depth'."
+        ):
+            ep.commongrid.compute_MVBS(ds_Sv_er_regular, range_var=range_var)
+    elif range_var == "depth":
+        with pytest.raises(
+            ValueError,
+            match=f"range_var '{range_var}' does not exist in the input dataset."
+        ):
+            ep.commongrid.compute_MVBS(ds_Sv_er_regular, range_var=range_var)
+    else:
+        pass
+
+@pytest.mark.integration
+def test_compute_MVBS(test_data_samples):
+    """
+    Test running through from open_raw to compute_MVBS.
+    """
+    (
+        filepath,
+        sonar_model,
+        azfp_xml_path,
+        range_kwargs,
+    ) = test_data_samples
+    ed = ep.open_raw(filepath, sonar_model, azfp_xml_path)
+    if ed.sonar_model.lower() == 'azfp':
+        avg_temperature = ed["Environment"]['temperature'].values.mean()
+        env_params = {
+            'temperature': avg_temperature,
+            'salinity': 27.9,
+            'pressure': 59,
+        }
+        range_kwargs['env_params'] = env_params
+        if 'azfp_cal_type' in range_kwargs:
+            range_kwargs.pop('azfp_cal_type')
+    Sv = ep.calibrate.compute_Sv(ed, **range_kwargs)
+    ping_time_bin = "20S"
+    ds_MVBS = ep.commongrid.compute_MVBS(Sv, ping_time_bin=ping_time_bin)
+    assert ds_MVBS is not None
+    
+    # Test to see if ping_time was resampled correctly
+    expected_ping_time = (
+        Sv["ping_time"]
+        .resample(ping_time=ping_time_bin, skipna=True)
+        .asfreq()
+        .indexes["ping_time"]
+    )
+    assert np.array_equal(ds_MVBS.ping_time.data, expected_ping_time.values)
+
+@pytest.mark.integration
+@pytest.mark.parametrize(
+    ("er_type"),
+    [
+        ("regular"),
+        ("irregular"),
+    ],
+)
+def test_compute_MVBS_er_output(request, er_type):
+    """Tests the output of compute_MVBS on regular and irregular data."""
+    # set jitter=0 to get predictable number of ping within each echo_range groups
+    if er_type == "regular":
+        ds_Sv = request.getfixturevalue("ds_Sv_er_regular")
+    else:
+        ds_Sv = request.getfixturevalue("ds_Sv_er_irregular")
+    
+    ds_MVBS = ep.commongrid.compute_MVBS(ds_Sv, range_meter_bin=5, ping_time_bin="10S")
+
+    if er_type == "regular":
+        expected_len = (
+            ds_Sv["channel"].size,  # channel
+            np.ceil(np.diff(ds_Sv["ping_time"][[0, -1]].astype(int))/1e9 / 10),  # ping_time
+            np.ceil(ds_Sv["echo_range"].max()/5),  # depth
+        )
+        assert ds_MVBS["Sv"].shape == expected_len
+    else:
+        assert (
+            ds_MVBS["Sv"]
+            .isel(ping_time=slice(None, 3))
+            .dropna(dim="echo_range").shape
+        ) == (2, 3, 10)  # full array, no NaN
+        assert (
+            ds_MVBS["Sv"]
+            .isel(ping_time=slice(3, 12))
+            .dropna(dim="echo_range").shape
+        ) == (2, 9, 7)  # bottom bins contain NaN
+        assert (
+            ds_MVBS["Sv"]
+            .isel(ping_time=slice(12, None))
+            .dropna(dim="echo_range").shape
+        ) == (2, 6, 3)  # bottom bins contain NaN
