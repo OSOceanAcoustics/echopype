9b7c81865ca94168f0009524dcf8daa8506fc25f
Landung 'Don' Setiawan <landungs@uw.edu>
2023-10-26 09:44:45 -0700

    test: Improve test for parser rectangularize data

--- echopype/tests/convert/conftest.py
+++ echopype/tests/convert/conftest.py
@@ -1,18 +1,40 @@
 import pytest
 
-from collections import defaultdict
-
-import numpy as np
-import pandas as pd
-
 from echopype.testing import _gen_ping_data_dict_power_angle, _gen_ping_data_dict_complex
 
-@pytest.fixture
-def mock_ping_data_dict_power_angle():
-    # TODO: Parameterize this fixture
-    return _gen_ping_data_dict_power_angle()
 
 @pytest.fixture
-def mock_ping_data_dict_complex():
-    # TODO: Parameterize this fixture
-    return _gen_ping_data_dict_complex()
+def irregular_ch_range_sample_len():
+    return [[10, 20, 100], [130], [20, 100, 10]]
+
+
+@pytest.fixture
+def irregular_ch_range_sample_ping_time_len():
+    return [[20, 100, 20], [120, 10, 5], [50, 20, 20]]
+
+
+@pytest.fixture(params=["regular", "irregular"])
+def mock_ping_data_dict_power_angle(
+    request, irregular_ch_range_sample_len, irregular_ch_range_sample_ping_time_len
+):
+    if request.param == "regular":
+        return request.param, _gen_ping_data_dict_power_angle()
+    elif request.param == "irregular":
+        return request.param, _gen_ping_data_dict_power_angle(
+            ch_range_sample_len=irregular_ch_range_sample_len,
+            ch_range_sample_ping_time_len=irregular_ch_range_sample_ping_time_len,
+            has_angle=[True, False, True],
+        )
+
+
+@pytest.fixture(params=["regular", "irregular"])
+def mock_ping_data_dict_complex(
+    request, irregular_ch_range_sample_len, irregular_ch_range_sample_ping_time_len
+):
+    if request.param == "regular":
+        return request.param, _gen_ping_data_dict_complex()
+    elif request.param == "irregular":
+        return request.param, _gen_ping_data_dict_complex(
+            ch_range_sample_len=irregular_ch_range_sample_len,
+            ch_range_sample_ping_time_len=irregular_ch_range_sample_ping_time_len
+        )
--- echopype/tests/convert/test_parse_base.py
+++ echopype/tests/convert/test_parse_base.py
@@ -1,13 +1,15 @@
+import copy
 import dask.array
 import numpy as np
 
 import pytest
-from echopype.convert.parse_base import FILENAME_DATETIME_EK60, ParseBase, ParseEK
+from echopype.convert.parse_base import FILENAME_DATETIME_EK60, ParseBase, ParseEK, INDEX2POWER
+
 
 class TestParseBase:
-    file="./my_file.raw"
-    storage_options={}
-    sonar_model="EK60"
+    file = "./my_file.raw"
+    storage_options = {}
+    sonar_model = "EK60"
     data_types = ["power", "angle", "complex"]
     raw_types = ["receive", "transmit"]
 
@@ -17,7 +19,7 @@ class TestParseBase:
             storage_options=self.storage_options,
             sonar_model=self.sonar_model,
         )
-        
+
         assert isinstance(parser, ParseBase)
         assert parser.source_file == self.file
         assert parser.sonar_model == self.sonar_model
@@ -26,8 +28,7 @@ class TestParseBase:
         assert parser.raw_types == self.raw_types
         assert parser.timestamp_pattern is None
         assert parser.ping_time == []
-        
-    
+
     def test__print_status(self):
         parser = ParseBase(
             file=self.file,
@@ -36,12 +37,24 @@ class TestParseBase:
         )
         assert parser._print_status() is None
 
+
 class TestParseEK:
     file = "./my_file.raw"
     storage_options = {}
     params = "ALL"
     data_types = ["power", "angle", "complex"]
     raw_types = ["receive", "transmit"]
+
+    def _get_parser(self, sonar_model, ping_data_dict):
+        parser = ParseEK(
+            file=self.file,
+            params=self.params,
+            storage_options=self.storage_options,
+            sonar_model=sonar_model,
+        )
+        parser.ping_data_dict = copy.deepcopy(ping_data_dict)
+        parser.ping_time = parser.ping_data_dict["timestamp"]
+        return parser
 
     @pytest.fixture
     def mock_ek60_parser_obj(self, mocker, mock_ping_data_dict_power_angle):
@@ -51,10 +64,10 @@ class TestParseEK:
             storage_options=self.storage_options,
             sonar_model="EK60",
         )
-        parser.ping_data_dict = mock_ping_data_dict_power_angle
-        parser.ping_time = mock_ping_data_dict_power_angle["timestamp"]
+        parser.ping_data_dict = copy.deepcopy(mock_ping_data_dict_power_angle)
+        parser.ping_time = parser.ping_data_dict["timestamp"]
         return parser
-    
+
     @pytest.fixture
     def mock_ek80_parser_obj(self, mocker, mock_ping_data_dict_complex):
         parser = ParseEK(
@@ -63,8 +76,8 @@ class TestParseEK:
             storage_options=self.storage_options,
             sonar_model="EK80",
         )
-        parser.ping_data_dict = mock_ping_data_dict_complex
-        parser.ping_time = mock_ping_data_dict_complex["timestamp"]
+        parser.ping_data_dict = copy.deepcopy(mock_ping_data_dict_complex)
+        parser.ping_time = parser.ping_data_dict["timestamp"]
         return parser
 
     @pytest.mark.parametrize("sonar_model", ["EK60", "EK80"])
@@ -75,7 +88,7 @@ class TestParseEK:
             storage_options=self.storage_options,
             sonar_model=sonar_model,
         )
-        
+
         assert isinstance(parser, ParseEK)
         assert parser.source_file == self.file
         assert parser.sonar_model == sonar_model
@@ -83,67 +96,117 @@ class TestParseEK:
         assert parser.data_types == self.data_types
         assert parser.raw_types == self.raw_types
         assert parser.timestamp_pattern == FILENAME_DATETIME_EK60
-    
+
     @pytest.mark.skip(reason="Test not implemented at this time.")
     def test_parse_raw(self):
         pass
-    
-    @pytest.mark.parametrize("sonar_model", ["EK60", "EK80"])
-    @pytest.mark.parametrize(
-        ["use_swap", "expected_type"],
-        [
-            (True, dask.array.Array),
-            (False, np.ndarray)
-        ]
-    )
-    def test_rectangularize_data(self, mocker, request, sonar_model, use_swap, expected_type):
-        """
-        parser.rectangularize_data(
-            dest_path=destination_path,
-            dest_storage_options=destination_storage_options,
-            max_chunk_size=max_chunk_size,
-        )
-        """
-        if sonar_model == "EK60":
-            parser = request.getfixturevalue("mock_ek60_parser_obj")
-            
-            for data_type in ["power", "angle"]:
-                assert data_type in parser.ping_data_dict
-            assert "complex" not in parser.ping_data_dict
-        elif sonar_model == "EK80":
-            parser = request.getfixturevalue("mock_ek80_parser_obj")
-            assert "complex" in parser.ping_data_dict
-        else:
-            pytest.skip(reason=f"Not implement for {sonar_model}")
-        
+
+    def _setup_rectangularize_data(self, mocker, sonar_model, use_swap, data_fixture):
+        sonar_model = sonar_model
+        fixture_param, orig_data_dict = data_fixture
+        parser = self._get_parser(sonar_model, orig_data_dict)
+
         # Patch use swap to either True or False
         # based on the test fixture parameterization
         mocker.patch(
-            "echopype.convert.parse_base.ParseEK._ParseEK__should_use_swap", 
-            return_value=use_swap
+            "echopype.convert.parse_base.ParseEK._ParseEK__should_use_swap", return_value=use_swap
         )
-        
+
         parser.rectangularize_data(
             dest_path="auto",
             dest_storage_options=None,
             max_chunk_size="100MB",
         )
+        return parser, orig_data_dict, fixture_param, mocker
+
+    @pytest.mark.parametrize(
+        ["use_swap", "expected_type"], [(True, dask.array.Array), (False, np.ndarray)]
+    )
+    @pytest.mark.parametrize("data_type", ["power", "angle"])
+    def test_rectangularize_data_EK60(
+        self, mocker, request, use_swap, expected_type, data_type, mock_ping_data_dict_power_angle
+    ):
+        sonar_model = "EK60"
+        parser, orig_data_dict, fixture_param, mocker = self._setup_rectangularize_data(
+            mocker, sonar_model, use_swap, mock_ping_data_dict_power_angle
+        )
+
+        for ch, arr in parser.ping_data_dict[data_type].items():
+            if arr is not None:
+                # For cases that angle data is not available
+                assert isinstance(arr, expected_type)
+                if use_swap:
+                    # Load the dask array into memory
+                    arr = arr.compute()
+
+                if fixture_param == "regular":
+                    orig_arr = np.array(orig_data_dict[data_type][ch])
+                    if data_type == "power":
+                        # Do power conversion to ensure same data result
+                        orig_arr = orig_arr.astype("float32") * INDEX2POWER
+                    # Check if arrays are equal
+                    assert np.array_equal(orig_arr, arr)
+                elif fixture_param == "irregular":
+                    # Iterate through each range sample
+                    for i in range(len(arr) - 1):
+                        mask = ~np.isnan(arr[i])
+                        if data_type == "power":
+                            # Filter out all NaNs in array
+                            darr = arr[i][mask]
+                            if len(darr) > 0:
+                                orig_arr = orig_data_dict[data_type][ch][i]
+                                # Do power conversion to ensure same data result
+                                orig_arr = orig_arr.astype("float32") * INDEX2POWER
+                                assert np.array_equal(darr, orig_arr)
+                        elif data_type == "angle":
+                            # Filter out all Nans in array
+                            # Create a 1D mask to filter out NaNs when both
+                            # angle data is available
+                            dmask = np.alltrue(mask, axis=1)
+                            darr = arr[i][dmask]
+                            if len(darr) > 0:
+                                orig_arr = orig_data_dict[data_type][ch][i]
+                                assert np.array_equal(darr, orig_arr)
+
+    @pytest.mark.parametrize(
+        ["use_swap", "expected_type"], [(True, dask.array.Array), (False, np.ndarray)]
+    )
+    @pytest.mark.parametrize("complex_part", ["real", "imag"])
+    def test_rectangularize_data_EK80(
+        self, mocker, request, use_swap, expected_type, complex_part, mock_ping_data_dict_complex
+    ):
+        sonar_model = "EK80"
+        complex_keys = {
+            "real": np.real,
+            "imag": np.imag,
+        }
+        parser, orig_data_dict, fixture_param, mocker = self._setup_rectangularize_data(
+            mocker, sonar_model, use_swap, mock_ping_data_dict_complex
+        )
         
-        # Check for expected data types
-        if sonar_model == "EK60":
-            for data_type in ["power", "angle"]:
-                for val in parser.ping_data_dict[data_type].values():
-                    assert isinstance(val, expected_type)
-        else:
-            complex_keys = ["real", "imag"]
-            for key in complex_keys:
-                for val in parser.ping_data_dict["complex"].values():
-                    assert key in val
-                    assert isinstance(val[key], expected_type)
-                    
-        # TODO: Check for data shapes
-        # TODO: Check for actual data values
+        for ch, arr_dct in parser.ping_data_dict["complex"].items():
+            assert complex_part in arr_dct
             
+            # Check if array is of expected type
+            arr = arr_dct[complex_part]
+            assert isinstance(arr, expected_type)
             
-    
-    
+            if use_swap:
+                # Load the dask array into memory
+                arr = arr.compute()
+            
+            if fixture_param == "regular":
+                # Check if arrays are equal
+                orig_arr = np.array(orig_data_dict["complex"][ch])
+                orig_arr = complex_keys[complex_part](orig_arr)
+                assert np.array_equal(arr, orig_arr)
+            elif fixture_param == "irregular":
+                # Iterate through each range sample
+                for i in range(len(arr) - 1):
+                    mask = ~np.isnan(arr[i])
+                    # Filter out all NaNs in array
+                    darr = arr[i][mask]
+                    if len(darr) > 0:
+                        orig_arr = orig_data_dict["complex"][ch][i]
+                        orig_arr = complex_keys[complex_part](orig_arr)
+                        assert np.array_equal(darr, orig_arr)
