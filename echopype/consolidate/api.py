cd23ffcc8fe7168a09d908d04de9ff76177af102
Emilio Mayorga <emiliomayorga@gmail.com>
2023-11-06 18:31:33 -0800

    Update echopype/consolidate/api.py
    Co-authored-by: Don Setiawan <landungs@uw.edu>

--- echopype/consolidate/api.py
+++ echopype/consolidate/api.py
@@ -52,7 +52,7 @@ def swap_dims_channel_frequency(ds: xr.Dataset) -> xr.Dataset:
 
 def add_depth(
     ds: xr.Dataset,
-    echodata: EchoData = None,
+    echodata: Optional[EchoData] = None,
     echodata_strict=False,
     depth_offset: float = 0,
     tilt: float = 0,
