59388c77a3eaf7610a85a2b9df521ff93501cf7d
Wu-Jung Lee <leewujung@gmail.com>
2024-04-24 08:17:38 -0700

    add deprecation warning at epviz import

--- echopype/visualize/__init__.py
+++ echopype/visualize/__init__.py
@@ -3,7 +3,19 @@ Visualization module to quickly plot raw, Sv, and MVBS dataset.
 
 **NOTE: To use this subpackage. `Matplotlib` and `cmocean` package must be installed.**
 """
+import warnings
 from .api import create_echogram
 from . import cm
 
 __all__ = ["create_echogram", "cm"]
+
+warnings.warn(
+    (
+        "`echopype.visualization` is deprecated and will be removed in an upcoming release. \n"
+        "Use Echoshader for visualization functions instead. \n"
+        "Repository: https://github.com/OSOceanAcoustics/echoshader \n"
+        "Echogram plotting: https://echoshader.readthedocs.io/en/latest/version_0.1.0/echogram_examples.html"  # noqa
+    ),
+    DeprecationWarning,
+    2,
+)
