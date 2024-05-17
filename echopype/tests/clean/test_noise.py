8cda3f469bd56fab2ee71b7f34a227b6a2e1d49c
ctuguinay <cmtuguinay2000@gmail.com>
2024-05-17 17:24:21 +0000

    move xfail to decorator

--- echopype/tests/clean/test_noise.py
+++ echopype/tests/clean/test_noise.py
@@ -500,11 +500,14 @@ def test_remove_background_noise():
         == 6
     )
 
+
+@pytest.mark.xfail(
+    reason=f"Tests for remove_background_noise have not been implemented " +
+            "when no sound absorption is provided!"
+)
 def test_remove_background_noise_no_sound_absorption():
     """
     Tests remove_background_noise on toy data that does
     not have sound absorption as a variable.
     """
-
-    pytest.xfail(f"Tests for remove_background_noise have not been implemented" +
-                 " when no sound absorption is provided!")
+    assert False
