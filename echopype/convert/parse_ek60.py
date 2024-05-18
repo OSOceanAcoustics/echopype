06a501dbe6a6bcd72a9836e4b66d2042a180bb61
ctuguinay <cmtuguinay2000@gmail.com>
2024-05-18 05:20:20 +0000

    set default args for parse ek 60 and parse ek 80

--- echopype/convert/parse_ek60.py
+++ echopype/convert/parse_ek60.py
@@ -4,5 +4,7 @@ from .parse_base import ParseEK
 class ParseEK60(ParseEK):
     """Class for converting data from Simrad EK60 echosounders."""
 
-    def __init__(self, file, file_meta, bot_file, idx_file, storage_options={}, sonar_model="EK60"):
+    def __init__(
+        self, file, file_meta, bot_file="", idx_file="", storage_options={}, sonar_model="EK60"
+    ):
         super().__init__(file, bot_file, idx_file, storage_options, sonar_model)
--- echopype/convert/parse_ek80.py
+++ echopype/convert/parse_ek80.py
@@ -4,6 +4,8 @@ from .parse_base import ParseEK
 class ParseEK80(ParseEK):
     """Class for converting data from Simrad EK80 echosounders."""
 
-    def __init__(self, file, file_meta, bot_file, idx_file, storage_options={}, sonar_model="EK80"):
+    def __init__(
+        self, file, file_meta, bot_file="", idx_file="", storage_options={}, sonar_model="EK80"
+    ):
         super().__init__(file, bot_file, idx_file, storage_options, sonar_model)
         self.environment = {}  # dictionary to store environment data
