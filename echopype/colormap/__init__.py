8f496a3288ff45d9fb1216a82fc814377b4272b3
Wu-Jung Lee <leewujung@gmail.com>
2024-06-15 09:24:51 -0700

    remove visualize subpkg, add colormap subpkg

---
+++ echopype/colormap/__init__.py
@@ -0,0 +1,8 @@
+"""
+Visualization module to quickly plot raw, Sv, and MVBS dataset.
+
+**NOTE: To use this subpackage. `Matplotlib` and `cmocean` package must be installed.**
+"""
+from . import cm
+
+__all__ = ["cm"]
