4a52328ada05897fa1f633a8f72e7ce561951291
Wu-Jung Lee <leewujung@gmail.com>
2024-06-15 09:54:26 -0700

    update docstring for colormap.__init__.py

--- echopype/colormap/__init__.py
+++ echopype/colormap/__init__.py
@@ -1,5 +1,5 @@
 """
-Visualization module to quickly plot raw, Sv, and MVBS dataset.
+Colormaps for plotting echogram.
 
 **NOTE: To use this subpackage. `Matplotlib` and `cmocean` package must be installed.**
 """
