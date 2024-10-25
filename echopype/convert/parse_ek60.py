94ec11066d70f6990161778dc1cf1dd4757bb3ab
Michael C Ryan <spacetime.engineer@gmail.com>
2024-10-25 15:33:18 -0400

    Added sonar model checkers for EK60, EK80
    Future releases of echopype may accommodate cases where the user is not required to provide sonar model numbers. We have here a simple checker to do that.
    
    -Added model checkers for EK60 and EK80 sonar models.

--- echopype/convert/parse_ek60.py
+++ echopype/convert/parse_ek60.py
@@ -1,5 +1,6 @@
 from .parse_base import ParseEK
-
+from .utils.ek_raw_io import RawSimradFile
+import numpy as np
 
 class ParseEK60(ParseEK):
     """Class for converting data from Simrad EK60 echosounders."""
@@ -14,3 +15,17 @@ class ParseEK60(ParseEK):
         **kwargs,
     ):
         super().__init__(file, bot_file, idx_file, storage_options, sonar_model)
+        
+def _is_EK60(raw_file, storage_options):
+    """Parse raw data to check if it is from Simrad EK80 echosounder."""
+    with RawSimradFile(raw_file, "r", storage_options=storage_options) as fid:
+        config_datagram = fid.read(1)
+        config_datagram["timestamp"] = np.datetime64(
+            config_datagram["timestamp"].replace(tzinfo=None), "[ns]"
+        )
+
+        # Only EK80 files have configuration in self.config_datagram
+        if config_datagram["sounder_name"] == "ER60" or config_datagram["sounder_name"] == "EK60":
+            return True
+        else:
+            return False
--- echopype/convert/parse_ek80.py
+++ echopype/convert/parse_ek80.py
@@ -1,5 +1,6 @@
 from .parse_base import ParseEK
-
+from .utils.ek_raw_io import RawSimradFile
+import numpy as np
 
 class ParseEK80(ParseEK):
     """Class for converting data from Simrad EK80 echosounders."""
@@ -15,3 +16,17 @@ class ParseEK80(ParseEK):
     ):
         super().__init__(file, bot_file, idx_file, storage_options, sonar_model)
         self.environment = {}  # dictionary to store environment data
+
+def is_EK80(raw_file, storage_options):
+    """Parse raw data to check if it is from Simrad EK80 echosounder."""
+    with RawSimradFile(raw_file, "r", storage_options=storage_options) as fid:
+        config_datagram = fid.read(1)
+        config_datagram["timestamp"] = np.datetime64(
+            config_datagram["timestamp"].replace(tzinfo=None), "[ns]"
+        )
+
+        # Only EK80 files have configuration in self.config_datagram
+        if "configuration" in config_datagram:
+            return True
+        else:
+            return False
