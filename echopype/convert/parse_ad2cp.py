9929523aab0ecfd8369300548ffb7f73d7ac1cbd
Michael C Ryan <spacetime.engineer@gmail.com>
2024-11-01 10:36:15 -0400

    Needed some fixes for the merge.
    Sorry about that.

--- echopype/convert/parse_ad2cp.py
+++ echopype/convert/parse_ad2cp.py
@@ -8,6 +8,7 @@ from typing_extensions import Literal
 from .parse_base import ParseBase
 
 
+
 @unique
 class BurstAverageDataRecordVersion(Enum):
     """
--- echopype/tests/convert/test_convert_ad2cp.py
+++ echopype/tests/convert/test_convert_ad2cp.py
@@ -4,7 +4,8 @@ This module test conversion of two sets of .ad2cp files.
 Files under "normal" contain default data variables,
 whereas files under "raw" additionally contain the IQ samples.
 """
-
+import glob
+from echopype.convert import is_AD2CP
 
 import xarray as xr
 import numpy as np
