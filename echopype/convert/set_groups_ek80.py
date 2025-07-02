1423f460f54f1de2fd89295eb1e7d729803ed5a6
ctuguinay <cmtuguinay2000@gmail.com>
2025-07-02 13:28:35 -0700

    squeeze dimensions then expand

--- echopype/convert/set_groups_ek80.py
+++ echopype/convert/set_groups_ek80.py
@@ -1476,12 +1476,14 @@ class SetGroupsEK80(SetGroupsBase):
                             attrs,
                         )
                     else:
+                        if data.ndim == 3:
+                            data = data.squeeze()
                         dims = ["channel", "filter_time"]
                         attrs = {"long_name": f"{attrs_dict[filter_type]} {attrs_dict[key]}"}
                         coeffs_xr_data[f"{filter_type}_{key}"] = (
                             dims,
                             # TODO Review if this is needed. Better to modify upstream
-                            data.squeeze() if data.ndim == 3 else data,
+                            np.atleast_2d(data),
                             attrs,
                         )
 
