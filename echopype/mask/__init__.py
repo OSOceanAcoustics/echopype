f0d60eaf9adbc043279afc6a7a30955f34fff8ae
ctuguinay <cmtuguinay2000@gmail.com>
2025-11-06 13:09:08 -0800

    fix merge issues

--- echopype/mask/__init__.py
+++ echopype/mask/__init__.py
@@ -1,6 +1,3 @@
-from .api import apply_mask, frequency_differencing, regrid_mask
+from .api import apply_mask, detect_seafloor, detect_shoal, frequency_differencing, regrid_mask
 
-__all__ = ["frequency_differencing", "apply_mask", "regrid_mask"]
-from .api import apply_mask, detect_seafloor, detect_shoal, frequency_differencing
-
-__all__ = ["frequency_differencing", "apply_mask", "detect_seafloor", "detect_shoal"]
+__all__ = ["frequency_differencing", "apply_mask", "detect_seafloor", "detect_shoal", "regrid_mask"]
