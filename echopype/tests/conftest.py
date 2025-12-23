b90755cd9a1dccb1a563a46c00fc788f734c2a00
Wu-Jung Lee <leewujung@gmail.com>
2025-12-22 16:08:27 -0800

    update sha

--- echopype/tests/conftest.py
+++ echopype/tests/conftest.py
@@ -27,7 +27,7 @@ if os.getenv("USE_POOCH") == "True":
     # v0.11.1a1 checksums (GitHub release assets)
     registry = {
         "ad2cp.zip": "sha256:78c634c7345991177b267c4cbb31f391990d2629b7f4a546da20d5126978b98a",
-        "azfp.zip": "sha256:99180d461d3e040e5904151bbfa0b11da90082323564339a47234f809bf54519",
+        "azfp.zip": "sha256:43976d3e6763c19278790bf39923b6827143ea3024c5ba5f46126addecf235de",
         "azfp6.zip": "sha256:98228329333064fb4b44d3044296c79d58ac22f6d81f7f22cf770bacf0e882fd",
         "ea640.zip": "sha256:49f70bd6f2355cb3c4c7a5b31fc00f7ae8c8a9ae888f0df1efe759032f9580df",
         "ecs.zip": "sha256:dcc312baa1e9da4488f33bef625b1f86c8a92e3262e34fc90ccd0a4f90d1e313",
