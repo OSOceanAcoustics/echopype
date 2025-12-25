8b46763653d323b4929afca713a6b04b1f4b7e0c
Lloyd Izard <76954858+LOCEANlloydizard@users.noreply.github.com>
2025-12-25 19:28:58 +0100

    Update test_commongrid_api.py
    Closes #1590

--- echopype/tests/commongrid/test_commongrid_api.py
+++ echopype/tests/commongrid/test_commongrid_api.py
@@ -332,9 +332,10 @@ def test_compute_MVBS_range_output(request, er_type):
     ds_MVBS = ep.commongrid.compute_MVBS(ds_Sv, range_bin="5m", ping_time_bin="10s")
 
     if er_type == "regular":
+        dt_ns = np.diff(ds_Sv["ping_time"][[0, -1]].values)[0]
         expected_len = (
             ds_Sv["channel"].size,  # channel
-            int(np.ceil(int(np.diff(ds_Sv["ping_time"][[0, -1]])) / 1e9 / 10)),  # ping_time
+            int(np.ceil(int(dt_ns) / 1e9 / 10)),  # ping_time
             int(np.ceil(ds_Sv["echo_range"].max() / 5)),  # depth
         )
         assert ds_MVBS["Sv"].shape == expected_len
