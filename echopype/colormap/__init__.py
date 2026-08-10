07e35d9fcf0dabc4c5be9a741c1845a9af0894a5
Gavin Macaulay <gavin@macaulay.co.nz>
2026-08-11 08:14:48 +1200

    Update colormap docstring (cmocean is not needed to use echopype colormaps) [skip ci]

--- echopype/colormap/__init__.py
+++ echopype/colormap/__init__.py
@@ -1,7 +1,7 @@
 """
 Colormaps for plotting echograms.
 
-To use this subpackage the `Matplotlib` and `cmocean` packages must be installed.
+To use this subpackage the `Matplotlib` package must be installed.
 
 Importing this package adds echogram-specific colormaps to Matplotlib's existing colormaps. These
 always start with `ep` and come in pairs (the colormap and a reversed version with a name
@@ -10,7 +10,6 @@ ending in `_r`). The list of colormaps added can be found with this code snippet
 >>> import echopype.colormap
 >>> from matplotlib import colormaps
 >>> cmaps = [name for name in colormaps if name.startswith('ep')]
-
 """
 
 from . import cm
